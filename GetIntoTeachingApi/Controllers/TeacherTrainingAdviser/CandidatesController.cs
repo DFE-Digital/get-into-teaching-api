4b3da1a20a29575785ea99523652d22a2bfb5a0d
Ross Oliver <ross.oliver36@gmail.com>
2020-05-04 09:27:21 +0100

    Stub TTA candidate rquests
    The Teacher Training Adviser (TTA) service will enable candidates to sign up so
    that they can speak to an adviser. The information we need about a candidate in
    order to sign up to this service will be different from other services (such as
    Schools Experience). This commit stubs out a controller with TTA-specific
    candidate actions.
    
    A client will be able to request an existing TTA candidate with a valid access
    token (PIN code) or upsert a candidate (which will register them for the TTA
    service).

---
+++ GetIntoTeachingApi/Controllers/TeacherTrainingAdviser/CandidatesController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Swashbuckle.AspNetCore.Annotations;
+using System.Collections.Generic;
+
+namespace GetIntoTeachingApi.Controllers.TeacherTrainingAdviser
+{
+    [Route("api/teacher_training_adviser/candidates")]
+    [ApiController]
+    public class CandidatesController : ControllerBase
+    {
+        private readonly ILogger<CandidatesController> _logger;
+
+        public CandidatesController(ILogger<CandidatesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        [SwaggerOperation(
+            Summary = "Upserts a candidate for the Teacher Training Adviser service.",
+            OperationId = "UpsertTeacherTrainingAdviserCandidate",
+            Tags = new[] { "Teacher Training Adviser" }
+        )]
+        [ProducesResponseType(typeof(IDictionary<string, string>), 400)]
+        public IActionResult Upsert([FromBody, SwaggerRequestBody("Candidate to upsert for the Teacher Training Adviser service.", Required = true)] Object candidate) // TODO:
+        {
+            // TODO:
+            return Ok(new Object());
+        }
+
+        [HttpGet]
+        [Route("{accessToken}")]
+        [SwaggerOperation(
+            Summary = "Retrieves an existing candidate for the Teacher Training Adviser service.",
+            Description = @"
+Retrieves an existing candidate for the Teacher Training Adviser service. The `accessToken` is obtained from a 
+`POST /candidates/access_tokens` request and must be sent along with the shared secret in the `Authorization` header.",
+            OperationId = "GetExistingTeacherTrainingAdviserCandidate",
+            Tags = new[] { "Teacher Training Adviser" }
+        )]
+        public IActionResult Get(
+            [FromRoute, SwaggerParameter("Access token (PIN code).", Required = true)] string accessToken, 
+            [FromHeader(Name = "Authorization"), SwaggerParameter("Bearer <sharedSecret>.", Required = true)] string sharedSecret
+        )
+        {
+            // TODO:
+            return Ok(new Object());
+        }
+    }
+}
