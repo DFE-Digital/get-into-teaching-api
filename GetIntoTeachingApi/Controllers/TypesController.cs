eb5e3c4203f313127c35f13d247a784abc071ac6
Ross Oliver <ross.oliver36@gmail.com>
2020-05-04 09:11:37 +0100

    Stub type requests
    A candidate has several attributes that will be presented in forms as select
    controls containing many possible values. Instead of hard-coding these values
    in the client, we are exposing `/api/types` endpoints that can return the
    available options. Initially to support the Get into Teaching TTA service we
    will need `countries` and `teaching subjects`. This commit stubs out the
    controller and actions for these types.

---
+++ GetIntoTeachingApi/Controllers/TypesController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace GetIntoTeachingApi.Controllers
+{
+    [Route("api/types")]
+    [ApiController]
+    public class TypesController : ControllerBase
+    {
+        private readonly ILogger<TypesController> _logger;
+
+        public TypesController(ILogger<TypesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("countries")]
+        [SwaggerOperation(
+            Summary = "Retrieves the list of countries.",
+            OperationId = "GetCountryTypes",
+            Tags = new[] { "Types" }
+        )]
+        public IActionResult GetCountries()
+        {
+            // TODO:
+            return Ok(new[] { new Object() });
+        }
+
+        [HttpGet]
+        [Route("teaching_subjects")]
+        [SwaggerOperation(
+            Summary = "Retrieves the list of teaching subjects.",
+            OperationId = "GetTeachingSubjects",
+            Tags = new[] { "Types" }
+        )]
+        public IActionResult GetTeachingSubjects()
+        {
+            // TODO:
+            return Ok(new[] { new Object() });
+        }
+    }
+}
