dce4f2a6931327932c56a939da33b9ac38f99b5b
Ross Oliver <ross.oliver36@gmail.com>
2020-05-04 09:09:51 +0100

    Stub latest privacy policy request
    When a candidate registers for a service (e.g. Teacher Training Adviser or
    Schools Experience) we require that they accept a privacy policy. This commit
    stubs out an API endpoint that will enable a client to request to latest
    privacy policy to show the candidate.

---
+++ GetIntoTeachingApi/Controllers/PrivacyPoliciesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Swashbuckle.AspNetCore.Annotations;
+using System;
+
+namespace GetIntoTeachingApi.Controllers
+{
+    [Route("api/privacy_policies")]
+    [ApiController]
+    public class PrivacyPoliciesController : ControllerBase
+    {
+        private readonly ILogger<PrivacyPoliciesController> _logger;
+
+        public PrivacyPoliciesController(ILogger<PrivacyPoliciesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("latest")]
+        [SwaggerOperation(
+            Summary = "Retrieves the latest privacy policy.",
+            OperationId = "GetLatestPrivacyPolicy",
+            Tags = new[] { "Privacy Policies" }
+        )]
+        public IActionResult GetLatest()
+        {
+            // TODO:
+            return Ok(new Object());
+        }
+    }
+}
