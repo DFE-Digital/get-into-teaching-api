9b930d2a2dbe001c721bd4dfdc2568fa8e274d9d
Ross Oliver <ross.oliver36@gmail.com>
2020-05-04 09:05:59 +0100

    Stub mailing list subscription request
    The Get into Teaching website will have a mailing list that users are able to
    sign up to; when this happens we will flag the consent in Dynamics365.
    
    This commit stubs out a controller action for this function as well as
    introducing a `CandidateIdentification` model and associated validations. The
    model encapsulates the information we require for identification purposes when
    subscribing a user to a mailing list, or adding them as an attendee to an event
    (this will come later).

---
+++ GetIntoTeachingApiTests/Controllers/MailingListControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using Xunit;
+using GetIntoTeachingApi.Controllers;
+using GetIntoTeachingApi.Models;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Microsoft.AspNetCore.Mvc;
+using FluentAssertions;
+
+namespace GetIntoTeachingApiTests.Controllers
+{
+    public class MailingListControllerTests
+    {
+        [Fact]
+        public void CreateCandidateAccessToken_InvalidRequest_RespondsWithValidationErrors()
+        {
+            var member = new CandidateIdentification { FirstName = null };
+            var mockLogger = new Mock<ILogger<MailingListController>>();
+            var controller = new MailingListController(mockLogger.Object);
+            controller.ModelState.AddModelError("FirstName", "First name must be specified.");
+
+            var response = controller.AddMember(member);
+
+            var badRequest = response.Should().BeOfType<BadRequestObjectResult>().Subject;
+            var errors = badRequest.Value.Should().BeOfType<SerializableError>().Subject;
+            errors.Should().ContainKey("FirstName").WhichValue.Should().BeOfType<string[]>().Which.Should().Contain("First name must be specified.");
+        }
+    }
+}
