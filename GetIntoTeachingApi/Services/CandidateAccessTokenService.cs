b303f144f2ed53af7c3af851a49ed8ccfb1746ce
Ross Oliver <ross.oliver36@gmail.com>
2020-05-04 14:47:17 +0100

    Add service to generate candidate access tokens
    We need to be able to generate access tokens that can be exchanged for existing
    candidate information. This commit introduces a token generation service based
    on the Timed One Time Password (TOTP) RFC 6238.
    
    A new token is generated ever 30 second 'step' (as per the RFC) and the service
    is currently configured so that tokens remain valid for a step windows size of
    2; this means that the current step, plus 2 steps (1 minute) prior and 2 steps
    future will be valid. Essentially, it results in the candidate having roughtly
    1 minute (plus at most another 30 seconds) to receive the email and enter the
    code. We can customise this in the future by increasing the window size to
    allow more time (the step size should remain at 30 seconds as per the RFD).
    
    An environment variable `TOTP_SECRET_KEY' must be set; this is used for the
    TOTP generation. We use the candidate email address in addition to the
    `TOTP_SECRET_KEY` as the TOTP secret to ensure that candidates with different
    emails get different tokens generated when inside the same time step.

---
+++ GetIntoTeachingApi/Services/CandidateAccessTokenService.cs
@@ -0,0 +1,60 @@
+﻿using GetIntoTeachingApi.Models;
+using OtpNet;
+using System;
+using System.Text;
+
+namespace GetIntoTeachingApi.Services
+{
+    public class CandidateAccessTokenService : ICandidateAccessTokenService
+    {
+        // The amount of time a user has to verify their access token is:
+        // (VerificationWindow * StepsInSeconds) + Remaining Seconds in Current Step
+        public static readonly int VerificationWindow = 2;
+        public static readonly int StepInSeconds = 30;
+        private static readonly int Length = 6;
+
+        public string GenerateToken(string email)
+        {
+           var  totp = CreateTotp(email);
+            return totp.ComputeTotp();
+        }
+
+        public bool IsValid(CandidateAccessTokenChallenge challenge)
+        {
+            return IsValid(challenge, DateTime.UtcNow);
+        }
+
+        public bool IsValid(CandidateAccessTokenChallenge challenge, DateTime timestamp)
+        {
+            if (!challenge.HasToken())
+            {
+                return false;
+            }
+
+            var totp = CreateTotp(challenge.Email);
+
+            long timeWindowUsed;
+            return totp.VerifyTotp(
+                timestamp,
+                challenge.Token,
+                out timeWindowUsed,
+                new VerificationWindow(previous: VerificationWindow, future: VerificationWindow)
+            );
+        }
+
+        private Totp CreateTotp(string email)
+        {
+            return new Totp(CompoundSharedSecretBytes(email), totpSize: Length, step: StepInSeconds);
+        }
+
+        private byte[] CompoundSharedSecretBytes(string email)
+        {
+            return Encoding.ASCII.GetBytes(email + TotpSecretKey());
+        }
+
+        private string TotpSecretKey()
+        {
+            return Environment.GetEnvironmentVariable("TOTP_SECRET_KEY");
+        }
+    }
+}
