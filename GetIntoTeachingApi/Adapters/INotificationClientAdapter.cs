4374f5469fb90d24a63240ae29810dc47a9fef3e
Ross Oliver <ross.oliver36@gmail.com>
2020-05-05 15:09:20 +0100

    Include Notify package and wrap in an adapter
    We will be using the GOV.UK Notify service in order to send PIN code emails out
    to candidates. There is a C# client library that we are using, however it
    doesn't provide an interface and the methods are not virtual, which means we
    won't be able to mock them. This commit adds a simple adapter around the client
    so that we can mock out the interface in later tests.

---
+++ GetIntoTeachingApi/Adapters/INotificationClientAdapter.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GetIntoTeachingApi.Adapters
+{
+    public interface INotificationClientAdapter
+    {
+        public Task SendEmailAsync(string apiKey, string email, string templateId, Dictionary<string, dynamic> personalisation);
+    }
+}
---
+++ GetIntoTeachingApi/Adapters/NotificationClientAdapter.cs
@@ -0,0 +1,15 @@
+﻿using Notify.Client;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GetIntoTeachingApi.Adapters
+{
+    public class NotificationClientAdapter : INotificationClientAdapter
+    {
+        public Task SendEmailAsync(string apiKey, string email, string templateId, Dictionary<string, dynamic> personalisation)
+        {
+            var client = new NotificationClient(apiKey);
+            return client.SendEmailAsync(email, templateId, personalisation);
+        }
+    }
+}
