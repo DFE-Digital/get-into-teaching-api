1d87d8571cd6836bcdd88e778399806911fa4336
Ross Oliver <ross.oliver36@gmail.com>
2020-05-05 15:07:00 +0100

    Add test helper for ILogger interface
    The ILogger is difficult to test as the convenience methods, such as
    `LogWarning` and `LogDebug` are extension methods, which Moq can't deal with.
    The test helper verifies the underlying `Log` method instead.

---
+++ GetIntoTeachingApiTests/Utils/LoggerTestHelpers.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+
+namespace GetIntoTeachingApiTests.Utils
+{
+    public static class LoggerTestHelpers
+    {
+        public static Mock<ILogger<T>> VerifyWarningWasCalled<T>(this Mock<ILogger<T>> logger, string expectedMessage)
+        {
+            Func<object, Type, bool> state = (v, t) =>  v.ToString().Contains(expectedMessage);
+
+            logger.Verify(
+                mock => mock.Log(
+                    It.Is<LogLevel>(logLevel => logLevel == LogLevel.Warning),
+                    It.IsAny<EventId>(),
+                    It.Is<It.IsAnyType>((v, t) => state(v, t)),
+                    It.IsAny<Exception>(),
+                    It.Is<Func<It.IsAnyType, Exception, string>>((v, t) => true)
+                )
+            );
+
+            return logger;
+        }
+    }
+}
