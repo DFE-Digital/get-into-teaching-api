93d4e186283beebb13417a7887f4d177b88fc78a
Ross Oliver <ross.oliver36@gmail.com>
2020-05-07 10:51:12 +0100

    Memoize adapter target instance
    Instead of instantiating the target class that the adapter wraps on every call
    we instead lazy instantiate the class and store it in a dictionary, keyed by an
    identifier.

--- GetIntoTeachingApi/Adapters/OrganizationServiceContextAdapter.cs
+++ GetIntoTeachingApi/Adapters/OrganizationServiceContextAdapter.cs
@@ -1,17 +1,35 @@
 ﻿using Microsoft.PowerPlatform.Cds.Client;
 using Microsoft.Xrm.Sdk;
 using Microsoft.Xrm.Sdk.Client;
+using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace GetIntoTeachingApi.Adapters
 {
     public class OrganizationServiceContextAdapter : IOrganizationServiceContextAdapter
     {
+        private readonly IDictionary<string, OrganizationServiceContext> _contexts;
+
+        public OrganizationServiceContextAdapter()
+        {
+            _contexts = new Dictionary<string, OrganizationServiceContext>();
+        }
+
         public IQueryable<Entity> CreateQuery(string connectionString, string entityName)
         {
-            var client = new CdsServiceClient(connectionString);
-            var context = new OrganizationServiceContext(client);
-            return context.CreateQuery(entityName);
+            return Context(connectionString).CreateQuery(entityName);
+        }
+
+        private OrganizationServiceContext Context(string connectionString)
+        {
+            if (!_contexts.ContainsKey(connectionString))
+            {
+                var client = new CdsServiceClient(connectionString);
+                _contexts[connectionString] = new OrganizationServiceContext(client);
+            }
+
+            return _contexts[connectionString];
         }
     }
 }
