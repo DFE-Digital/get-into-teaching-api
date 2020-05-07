2eec1307ee2588b2eb754799541c102794f90711
Ross Oliver <ross.oliver36@gmail.com>
2020-05-07 15:39:04 +0100

    Add TestCoverage/ to .gitignore

---
+++ GetIntoTeachingApi/Profiles/PrivacyPolicyProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using GetIntoTeachingApi.Models;
+using Microsoft.Xrm.Sdk;
+
+namespace GetIntoTeachingApi.Profiles
+{
+    public class PrivacyPolicyProfile : Profile
+    {
+        public PrivacyPolicyProfile()
+        {
+            CreateMap<Entity, PrivacyPolicy>().ForMember(dest =>
+                dest.Text,
+                opt => opt.MapFrom(src => src.Attributes["dfe_details"])
+            );
+        }
+    }
+}
