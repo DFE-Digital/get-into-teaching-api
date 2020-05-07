a6d7e6233abba8c9e11e7d4358a33a40e7cab64d
Ross Oliver <ross.oliver36@gmail.com>
2020-05-07 07:33:16 +0100

    Add TypeEntity model
    `Type` is a keyword in C#, so this adds a `TypeEntity` model tha will be used
    for modelling generic 'types' that can be returned from the API (e.g.
    countries, teaching subjects, etc). A type can be anything that boils down to
    an identifier and a value.

---
+++ GetIntoTeachingApi/Models/TypeEntity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace GetIntoTeachingApi.Models
+{
+    public class TypeEntity
+    {
+        public Guid Id { get; set; }
+        public dynamic Value { get; set; }
+    }
+}
