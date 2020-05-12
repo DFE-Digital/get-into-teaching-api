45ce4940152d8053659510c91fefaa3ebee6d30a
Ross Oliver <ross.oliver36@gmail.com>
2020-05-12 13:45:15 +0100

    Update TypeEntity id to be dynamic
    We are using `TypeEntity` to map from a 'lookup' (`Entity`) object as well as a
    'pick list' (`PickListItem`) object. Both of these objects end up as options in
    a select element of a form and they can be represented with an identifier and
    value. By making the `Id` attribute of `TypeEntity` dynamic it will accept both
    a `Guid` and `int` (which are used by lookup and pick list objects,
    respectively).
    
    Also updates the auto mapper profile to support mapping from a `PickListItem`
    as well as the existing `Entity`.

--- GetIntoTeachingApi/Profiles/TypeEntityProfile.cs
+++ GetIntoTeachingApi/Profiles/TypeEntityProfile.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using GetIntoTeachingApi.Models;
 using Microsoft.Xrm.Sdk;
+using static Microsoft.PowerPlatform.Cds.Client.CdsServiceClient;
 
 namespace GetIntoTeachingApi.Profiles
 {
@@ -8,10 +9,20 @@ namespace GetIntoTeachingApi.Profiles
     {
         public TypeEntityProfile()
         {
-            CreateMap<Entity, TypeEntity>().ForMember(dest => 
+            CreateMap<Entity, TypeEntity>().ForMember(dest =>
                 dest.Value,
                 opt => opt.MapFrom(src => src.GetAttributeValue<string>("dfe_name"))
             );
+
+            CreateMap<PickListItem, TypeEntity>()
+                .ForMember(dest =>
+                    dest.Id,
+                    opt => opt.MapFrom(src => src.PickListItemId)
+                )
+                .ForMember(dest =>
+                    dest.Value,
+                    opt => opt.MapFrom(src => src.DisplayLabel)
+                );
         }
     }
 }
