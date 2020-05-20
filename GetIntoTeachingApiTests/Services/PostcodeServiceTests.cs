faa902dc0ea4d9f1fc86bdda5882687d6cfb5e88
Ross Oliver <ross.oliver36@gmail.com>
2020-05-20 11:47:32 +0100

    Add PostcodeService
    We need to be able to validate a postcode coming from a teaching event search
    request against the list of known postcodes that we can geocode.
    
    A user can also filter by events within a certain radius of a postcode, so
    there is a method to calculate the distance between two postcodes.
    
    Postcode data is stored in-memory; this causes the application to be slow when
    booting, so we will likely move this to a database in a future PR.

---
+++ GetIntoTeachingApiTests/Services/PostcodeServiceTests.cs
@@ -0,0 +1,45 @@
+﻿using FluentAssertions;
+using GetIntoTeachingApi.Services;
+using Xunit;
+
+namespace GetIntoTeachingApiTests.Services
+{
+    public class PostcodeServiceTests
+    {
+        private readonly IPostcodeService _service;
+
+        public PostcodeServiceTests()
+        {
+            _service = new PostcodeService("./Fixtures/ukpostcodes.csv");
+        }
+
+        [Theory]
+        [InlineData("KY11 9YU")]
+        [InlineData("ky11 9yu")]
+        [InlineData("ky119yu")]
+        [InlineData("k y 119 YU")]
+        [InlineData("CA4 8LE")]
+        public void IsValid_WithValidPostcode_ReturnsTrue(string postcode)
+        {
+            _service.IsValid(postcode).Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        [InlineData("KY11 9ZZ")]
+        [InlineData("KY11 9HFF")]
+        [InlineData("Non-Geographic")]
+        public void IsValid_WithInvalidPostcode_ReturnsFalse(string postcode)
+        {
+            _service.IsValid(postcode).Should().BeFalse();
+        }
+
+        [Fact]
+        public void DistanceBetween_ReturnsDistanceInMiles()
+        {
+            var distance = _service.DistanceBetween("KY11 9YU", "CA4 8LE");
+            distance.Should().BeApproximately(81, 0.5);
+        }
+    }
+}
