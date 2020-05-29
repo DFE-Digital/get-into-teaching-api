2e57364a2788cf06db398975103cf1d9251e0ed3
Ross Oliver <ross.oliver36@gmail.com>
2020-05-29 15:11:38 +0100

    Migrate postcode data into Postgres
    There are roughtly 1.7 million UK postcodes that we currently read from
    a CSV file and store in memory to do geolocation comparisons when
    searching for events by radius.
    
    This commit introduces Entity Framework, backed by Postgres and a
    `Location` model; the postcode and associated geolocation are read from
    the CSV and seeded into the database (if not yet present) when the
    application starts.
    
    The `LocationService` has been updated to query postcode information
    from the database and the in-memory store has been removed.
    
    Entity Framework is configured to use an in-memory SQLite database when
    in development.
    
    Going forward it will make sense to also put event information into
    Postgres so that we can run a query to find events within a geographical
    region, instead of querying postcode information individually for each
    event as we currently do.

--- GetIntoTeachingApi/Services/LocationService.cs
+++ GetIntoTeachingApi/Services/LocationService.cs
@@ -1,25 +1,25 @@
 ﻿using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
+using System.Linq;
 using System.Text.RegularExpressions;
 using CsvHelper;
-using GeoCoordinatePortable;
+using GetIntoTeachingApi.Database;
+using GetIntoTeachingApi.Models;
 using GetIntoTeachingApi.Utils;
 
 namespace GetIntoTeachingApi.Services
 {
     public class LocationService : ILocationService
-    {
-        private readonly IDictionary<string, PostcodeEntry> _postcodes;
+    { 
         private const double MetersToMiles = 0.000621371;
+        private readonly GetIntoTeachingDbContext _dbContext;
 
-        public LocationService(string fixture = "./Fixtures/ukpostcodes.csv")
+        public LocationService() { }
+
+        public LocationService(GetIntoTeachingDbContext dbContext)
         {
-            _postcodes = new Dictionary<string, PostcodeEntry>();
-
-            if (Env.IsDevelopment) fixture = "./Fixtures/ukpostcodes.dev.csv";
-
-            LoadPostcodesFixture(fixture);
+            _dbContext = dbContext;
         }
 
         public bool IsValid(string postcode)
@@ -27,62 +27,20 @@ namespace GetIntoTeachingApi.Services
             if (string.IsNullOrEmpty(postcode))
                 return false;
 
-            return _postcodes.ContainsKey(Sanitize(postcode));
+            return _dbContext.Locations.Any(l => l.Postcode == Sanitize(postcode));
         }
 
         public double DistanceBetween(string originPostcode, string destinationPostcode)
         {
-            var origin = _postcodes[Sanitize(originPostcode)];
-            var destination = _postcodes[Sanitize(destinationPostcode)];
+            var postcodes = new[] {Sanitize(originPostcode), Sanitize(destinationPostcode)};
+            var locations = _dbContext.Locations.Where(l => postcodes.Contains(l.Postcode)).ToList();
 
-            return origin.Coordinate.GetDistanceTo(destination.Coordinate) * MetersToMiles;
+            return locations.First().Coordinate.GetDistanceTo(locations.Last().Coordinate) * MetersToMiles;
         }
 
-        private void LoadPostcodesFixture(string path)
-        {
-            _postcodes.Clear();
-
-            using var reader = new StreamReader(path);
-            using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
-
-            csv.Read();
-            csv.ReadHeader();
-
-            while (csv.Read())
-            {
-                var postcode = csv.GetField<string>("postcode");
-                var entry = new PostcodeEntry()
-                {
-                    Latitude = csv.GetField<double?>("latitude"),
-                    Longitude = csv.GetField<double?>("longitude")
-                };
-
-                if (entry.IsNonGeographic())
-                    continue;
-
-                _postcodes.Add(Sanitize(postcode), entry);
-            }
-        }
-
-        private string Sanitize(string postcode)
+        public static string Sanitize(string postcode)
         {
             return Regex.Replace(postcode, @"\s+", "").ToLower();
-        }
-    }
-
-    internal class PostcodeEntry
-    {
-        public double? Latitude { get; set; }
-        public double? Longitude { get; set; }
-
-        public GeoCoordinate Coordinate
-        {
-            get { return IsNonGeographic() ? null : new GeoCoordinate((double) Latitude, (double) Longitude); }
-        }
-
-        public bool IsNonGeographic()
-        {
-            return Latitude == null || Longitude == null;
         }
     }
 }
--- GetIntoTeachingApiTests/Models/LocationTests.cs
+++ GetIntoTeachingApiTests/Models/LocationTests.cs
@@ -1,5 +1,6 @@
-﻿using Castle.Components.DictionaryAdapter;
+﻿using System.ComponentModel.DataAnnotations;
 using FluentAssertions;
+using GeoCoordinatePortable;
 using GetIntoTeachingApi.Models;
 using Xunit;
 
@@ -13,6 +14,29 @@ namespace GetIntoTeachingApiTests.Models
             var type = typeof(Location);
 
             type.GetProperty("Postcode").Should().BeDecoratedWith<KeyAttribute>();
+        }
+
+        [Fact]
+        public void Coordinate_ForNonGeographic_ReturnsNull()
+        {
+            new Location() { Latitude = null, Longitude = null }.Coordinate.Should().BeNull();
+        }
+
+        [Fact]
+        public void Coordinate_ForGeographic_ReturnsCorrectly()
+        {
+            var coordinate = new GeoCoordinate(1.234, 5.678);
+            new Location() { Latitude = 1.234, Longitude = 5.678 }.Coordinate.Should().BeEquivalentTo(coordinate);
+        }
+
+        [Theory]
+        [InlineData(null, null, true)]
+        [InlineData(null, 1.234, true)]
+        [InlineData(1.234, null, true)]
+        [InlineData(1.234, 5.678, false)]
+        public void IsGeographic_ReturnsCorrectly(double? lat, double? lng, bool expected)
+        {
+            new Location() {Latitude = lat, Longitude = lng}.IsNonGeographic().Should().Be(expected);
         }
     }
 }
