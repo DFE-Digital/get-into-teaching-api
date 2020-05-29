b505ac2a9ae3f04f2aaaf3303a5deda68fa6c494
Steven Fawcett <steve.fawcett@capgemini.com>
2020-05-29 13:57:29 +0100

    Added Apply

--- terraform/application.tf
+++ terraform/application.tf
@@ -1,7 +1,7 @@
 resource "cloudfoundry_app" "api_application" {
     name =  var.paas_api_application_name
     space = data.cloudfoundry_space.space.id
-    docker_image = "dfedigital/get-into-teaching-api:GITPB-149"
+    docker_image = var.paas_api_docker_image
     service_binding {
             service_instance = cloudfoundry_service_instance.postgres.id
         }
@@ -13,6 +13,8 @@ resource "cloudfoundry_app" "api_application" {
          CRM_CLIENT_SECRET = var.CRM_CLIENT_SECRET 
          CRM_SERVICE_URL   = var.CRM_SERVICE_URL  
          CRM_TENANT_ID     = var.CRM_TENANT_ID   
+         NOTIFY_API_KEY    = var.NOTIFY_API_KEY
+         TOTP_SECRET_KEY   = var.TOTP_SECRET_KEY
          SHARED_SECRET     = var.SHARED_SECRET }     
 }
 
