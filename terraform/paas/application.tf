006cac27757fc2b8da203fc5707e77ec0065238d
Steven Fawcett <steve.fawcett@capgemini.com>
2020-06-03 14:36:19 +0100

    Adding Environment Variables

--- terraform/paas/application.tf
+++ terraform/paas/application.tf
@@ -20,6 +20,8 @@ resource "cloudfoundry_app" "api_application" {
          TOTP_SECRET_KEY   = var.TOTP_SECRET_KEY
          SHARED_SECRET     = var.SHARED_SECRET
          ASPNETCORE_ENVIRONMENT = var.ASPNETCORE_ENVIRONMENT
+         DATABASE_INSTANCE_NAME = cloudfoundry_service_instance.postgres2.name
+         HANGFIRE_INSTANCE_NAME = cloudfoundry_service_instance.postgres1.name
     }    
 }
 
