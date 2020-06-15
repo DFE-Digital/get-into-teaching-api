a425bc8feee1627fcc65aae582880e59d7a867d5
Ross Oliver <ross.oliver36@gmail.com>
2020-06-15 09:24:45 +0100

    Expose Hangfire jobs to Prometheus

--- GetIntoTeachingApi/Services/MetricService.cs
+++ GetIntoTeachingApi/Services/MetricService.cs
@@ -6,7 +6,10 @@ namespace GetIntoTeachingApi.Services
     {
         private static readonly Histogram _crmSyncDuration = Metrics
             .CreateHistogram("api_crm_sync_duration_seconds", "Histogram of CRM sync durations.");
+        private static readonly Gauge _hangfireJobs = Metrics
+            .CreateGauge("api_hangfire_jobs", "Gauge number of Hangifre jobs.", "state");
 
         public Histogram CrmSyncDuration => _crmSyncDuration;
+        public Gauge HangfireJobs => _hangfireJobs;
     }
 }
---
+++ GetIntoTeachingApi/Utils/Extensions.cs
@@ -0,0 +1,21 @@
+﻿using GetIntoTeachingApi.Services;
+using Hangfire;
+using Microsoft.AspNetCore.Builder;
+using Prometheus;
+
+namespace GetIntoTeachingApi.Utils
+{
+    public static class Extensions
+    {
+        public static IApplicationBuilder UsePrometheusHangfireExporter(this IApplicationBuilder app)
+        {
+            var jobStorage = (JobStorage) app.ApplicationServices.GetService(typeof(JobStorage));
+            var metrics = (IMetricService) app.ApplicationServices.GetService(typeof(IMetricService));
+            var exporter = new HangfirePrometheusExporter(jobStorage, metrics);
+
+            Metrics.DefaultRegistry.AddBeforeCollectCallback(() => exporter.ExportHangfireStatistics());
+
+            return app;
+        }
+    }
+}
