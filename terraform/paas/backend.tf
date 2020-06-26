7c905de06d882298249598e5db5d666cfffc7bae
Steven Fawcett <steve.fawcett@capgemini.com>
2020-06-26 12:58:56 +0100

    Added messaging

--- terraform/paas/backend.tf
+++ terraform/paas/backend.tf
@@ -1,8 +1,5 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "s146t01-rg-tfstate"
-    storage_account_name = "s146t01sgtfstate"
     container_name       = "pass-tfstate"
-    key                  = "api.test.terraform.tfstate"
   }
 }
