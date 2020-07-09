3a415c36ddf8b7e3d2c82a44df42cc6d48dc3221
Ross Oliver <ross.oliver36@gmail.com>
2020-07-09 07:43:47 +0100

    Add validator for TeachingAdviserSignUpRequest
    Add basic validation to ensure underlying Candidate is valid - still needs
    fleshed out for more specific validation based on branches.

---
+++ GetIntoTeachingApi/Models/Validators/TeacherTrainingAdviserSignUpRequestValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using GetIntoTeachingApi.Services;
+
+namespace GetIntoTeachingApi.Models.Validators
+{
+    public class TeacherTrainingAdviserSignUpRequestValidator : AbstractValidator<TeacherTrainingAdviserSignUpRequest>
+    {
+        public TeacherTrainingAdviserSignUpRequestValidator(IStore store)
+        {
+            RuleFor(request => request.Candidate).SetValidator(new CandidateValidator(store));
+        }
+    }
+}
