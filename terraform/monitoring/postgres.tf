c3cfa15523588489836280d1df5142430f9d5090
Steven Fawcett <steve.fawcett@capgemini.com>
2020-07-22 13:38:15 +0100

    Added google connect and plugins

--- terraform/monitoring/postgres.tf
+++ terraform/monitoring/postgres.tf
@@ -1,13 +1,24 @@
 
+data "cloudfoundry_space" "postgres-1-monitor" {
+    name = var.postgres1["space"]
+    org_name =  var.paas_org_name
+}
+
 module "postgres-1" {
      source = "/Users/stevenfawcett/DFE/bat-platform-building-blocks/terraform/modules/postgres_exporter"
 //        source = "git::https://github.com/DFE-Digital/bat-platform-building-blocks.git//terraform/modules/postgres_exporter?ref=devops/get-into-teaching/add-postgres"
 //        source = "git::https://github.com/DFE-Digital/bat-platform-building-blocks.git//terraform/modules/postgres_exporter?ref=master"
 
-     space_id = data.cloudfoundry_space.space.id
+     install_space_id = data.cloudfoundry_space.space.id
+     monitor_space_id = data.cloudfoundry_space.postgres-1-monitor.id
      name = "${var.environment}-${var.postgres1[ "name" ]}"
      postgres-service-name = var.postgres1["service"] 
      prometheus = module.prometheus
+}
+
+data "cloudfoundry_space" "postgres-2-monitor" {
+    name = var.postgres2["space"]
+    org_name =  var.paas_org_name
 }
 
 module "postgres-2" {
@@ -15,7 +26,8 @@ module "postgres-2" {
 //        source = "git::https://github.com/DFE-Digital/bat-platform-building-blocks.git//terraform/modules/postgres_exporter?ref=devops/get-into-teaching/add-postgres"
 //        source = "git::https://github.com/DFE-Digital/bat-platform-building-blocks.git//terraform/modules/postgres_exporter?ref=master"
 
-     space_id = data.cloudfoundry_space.space.id
+     install_space_id = data.cloudfoundry_space.space.id
+     monitor_space_id = data.cloudfoundry_space.postgres-2-monitor.id
      name = "${var.environment}-${var.postgres2[ "name" ]}"
      postgres-service-name = var.postgres2["service"] 
      prometheus = module.prometheus
