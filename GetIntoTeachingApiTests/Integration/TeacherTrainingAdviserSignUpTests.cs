2338dfafa18a35e3e4c6d5fc614058a8511727e5
Ross Oliver <ross.oliver36@gmail.com>
2020-08-03 09:16:18 +0100

    WIP - integration tests with shared fixtures

---
+++ GetIntoTeachingApiTests/Integration/TeacherTrainingAdviserSignUpTests.cs
@@ -0,0 +1,54 @@
+﻿using CSScriptLib;
+using CsvHelper;
+using FluentAssertions;
+using GetIntoTeachingApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using Xunit;
+
+namespace GetIntoTeachingApiTests.Integration
+{
+    public class TeacherTrainingAdviserSignUpTests
+    {
+        [Fact]
+        public async void SignUp_WithFixtures_BehaveAsExpected()
+        {
+            using var reader = new StreamReader("./Fixtures/teacher_training_adviser_sign_ups.csv");
+            using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+
+            var rowsToSkip = new[] { "Firstname", "identity[first_name]", "" };
+            csv.Configuration.ShouldSkipRecord = row => rowsToSkip.Contains(row[2]);
+
+            await csv.ReadAsync();
+            csv.ReadHeader();
+
+            var headers = csv.Context.HeaderRecord.Where(h => !string.IsNullOrEmpty(h));
+
+            while (await csv.ReadAsync())
+            {
+                var signUp = CreateTeacherTrainingAdviserSignUp(csv, headers);
+
+            }
+        }
+
+        private TeacherTrainingAdviserSignUp CreateTeacherTrainingAdviserSignUp(CsvReader csv, IEnumerable<string> headers)
+        {
+            var signUp = new TeacherTrainingAdviserSignUp();
+
+            headers.ForEach(h =>
+            {
+                var propertyName = h.Substring(0, 1).ToUpper() + h.Substring(1);
+                var property = signUp.GetType().GetProperty(propertyName);
+
+                property.Should().NotBeNull($"Unrecognized property: {propertyName}");
+
+                property.SetValue(signUp, csv.GetField(h), null);
+            });
+
+            return signUp;
+        }
+    }
+}
