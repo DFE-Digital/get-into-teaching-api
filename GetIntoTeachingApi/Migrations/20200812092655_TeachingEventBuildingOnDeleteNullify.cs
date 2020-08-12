0a2ff8e87a8ae35d12998b18a869f445032c9e6c
Ross Oliver <ross.oliver36@gmail.com>
2020-08-12 10:27:03 +0100

    Set TeachingEventBuilding OnDelete to nullify foreign key
    Currently when a building is deleted from the CRM the sync job fails as it
    tries to delete the building with the foreign key constraint in the
    `TeachingEvent` still present. This updates the cascade operation to nullify
    the foreign key when deleting a `TeachingEventBuilding`.

---
+++ GetIntoTeachingApi/Migrations/20200812092655_TeachingEventBuildingOnDeleteNullify.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GetIntoTeachingApi.Migrations
+{
+    public partial class TeachingEventBuildingOnDeleteNullify : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TeachingEvents_TeachingEventBuildings_BuildingId",
+                table: "TeachingEvents");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TeachingEvents_TeachingEventBuildings_BuildingId",
+                table: "TeachingEvents",
+                column: "BuildingId",
+                principalTable: "TeachingEventBuildings",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TeachingEvents_TeachingEventBuildings_BuildingId",
+                table: "TeachingEvents");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TeachingEvents_TeachingEventBuildings_BuildingId",
+                table: "TeachingEvents",
+                column: "BuildingId",
+                principalTable: "TeachingEventBuildings",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
