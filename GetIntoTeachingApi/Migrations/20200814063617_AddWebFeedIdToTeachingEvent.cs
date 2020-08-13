e9480f9df99d7a81bc372de2544dba67c2bfa418
Ross Oliver <ross.oliver36@gmail.com>
2020-08-13 14:16:42 +0100

    Add WebFeedId to TeachingEvent

---
+++ GetIntoTeachingApi/Migrations/20200814063617_AddWebFeedIdToTeachingEvent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GetIntoTeachingApi.Migrations
+{
+    public partial class AddWebFeedIdToTeachingEvent : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "WebFeedId",
+                table: "TeachingEvents",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "WebFeedId",
+                table: "TeachingEvents");
+        }
+    }
+}
