e4fee07129b4b144da46e9854bbf698ab3d75181
ethax-ross <ross.oliver36@gmail.com>
2020-08-13 15:27:38 +0100

    Revert "Add WebFeedId to TeachingEvent and prevent sign ups if null"

--- GetIntoTeachingApiTests/Models/Validators/TeachingEventRegistrationValidatorTests.cs
+++ GetIntoTeachingApiTests/Models/Validators/TeachingEventRegistrationValidatorTests.cs
@@ -24,7 +24,7 @@ namespace GetIntoTeachingApiTests.Models.Validators
         [Fact]
         public void Validate_WhenValid_HasNoErrors()
         {
-            var mockTeachingEvent = new TeachingEvent { Id = Guid.NewGuid(), WebFeedId = "123" };
+            var mockTeachingEvent = new TeachingEvent { Id = Guid.NewGuid() };
             var mockPickListItem = new TypeEntity { Id = "123" };
 
             _mockStore
@@ -49,33 +49,6 @@ namespace GetIntoTeachingApiTests.Models.Validators
         public void Validate_EventIdIsInvalid_HasError()
         {
             _validator.ShouldHaveValidationErrorFor(registration => registration.EventId, Guid.NewGuid());
-        }
-
-        [Fact]
-        public void Validate_EventDoesNotAcceptOnlineRegistrations_HasError()
-        {
-            var mockTeachingEvent = new TeachingEvent { Id = Guid.NewGuid(), WebFeedId = null };
-            var mockPickListItem = new TypeEntity { Id = "123" };
-
-            _mockStore
-                .Setup(mock => mock.GetTeachingEventAsync((Guid)mockTeachingEvent.Id))
-                .ReturnsAsync(mockTeachingEvent);
-            _mockStore
-                .Setup(mock => mock.GetPickListItems("msevtmgt_eventregistration", "dfe_channelcreation"))
-                .Returns(new[] { mockPickListItem }.AsQueryable());
-
-            var registration = new TeachingEventRegistration()
-            {
-                EventId = (Guid)mockTeachingEvent.Id,
-                ChannelId = int.Parse(mockPickListItem.Id),
-            };
-
-            var result = _validator.TestValidate(registration);
-
-            result.IsValid.Should().BeFalse();
-
-            result.ShouldHaveValidationErrorFor("EventId")
-                .WithErrorMessage("Attendence cannot be registered for this event via the API (it has no WebFeedId).");
         }
 
         [Fact]
