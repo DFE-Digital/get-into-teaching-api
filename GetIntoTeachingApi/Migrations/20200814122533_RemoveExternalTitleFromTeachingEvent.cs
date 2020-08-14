b30a7ce4d10fb928b9857906720aae9b7a6f3396
Ross Oliver <ross.oliver36@gmail.com>
2020-08-14 13:19:27 +0100

    Use external event name
    An event has an internal and external event name; previously we were exposing
    both, but we don't actually need to display the internal event name anywhere on
    the website, so it makes more sense to just have one event name pointing to the
    external name attribute in Dynamics.

---
+++ GetIntoTeachingApi/Migrations/20200814122533_RemoveExternalTitleFromTeachingEvent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GetIntoTeachingApi.Migrations
+{
+    public partial class RemoveExternalTitleFromTeachingEvent : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExternalName",
+                table: "TeachingEvents");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ExternalName",
+                table: "TeachingEvents",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
