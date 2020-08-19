1e0e0d83d3a96844bb028b6edf67076364107ee6
Steven Fawcett <steve.fawcett@capgemini.com>
2020-08-19 10:36:05 +0100

    Migrate terraform from 12 to 13

--- terraform/paas/provider.tf
+++ terraform/paas/provider.tf
@@ -1,3 +1,14 @@
+terraform {
+ required_version = "~> 0.13"
+
+  required_providers {
+    cloudfoundry = {
+      source  = "terraform-registry.us-east.philips-healthsuite.com/philips-forks/cloudfoundry"
+      version = "0.12.2-202008131826"
+    }
+  }
+}
+
 provider "cloudfoundry" {
     api_url = var.api_url
     user = var.user
