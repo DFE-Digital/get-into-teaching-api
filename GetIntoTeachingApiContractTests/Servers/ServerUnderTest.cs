c1ed0cb8e231d77a1817c017f6fcc2a63a99627b
matt <mattonfoot@gmail.com>
2020-08-25 14:01:13 +0100

    initial fixture environment with TestServer and Partially Mocked CRM Server

---
+++ GetIntoTeachingApiContractTests/Servers/ServerUnderTest.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GetIntoTeachingApiContractTests.Servers
+{
+    public class ServerUnderTest<TStartup> : TestServer
+    {
+        private static readonly Assembly STARTUP_ASSEMBLY = typeof(TStartup).GetTypeInfo().Assembly;
+
+        public ServerUnderTest(string relativeTargetProjectParentDir)
+            : base(Setup(relativeTargetProjectParentDir))
+        {
+            
+        }
+        
+        private static IWebHostBuilder Setup(string relativeTargetProjectParentDir)
+        {
+            var contentRoot = GetProjectPath(relativeTargetProjectParentDir, STARTUP_ASSEMBLY);
+
+            var configurationBuilder = new ConfigurationBuilder()
+                    .SetBasePath(contentRoot)
+                    .AddJsonFile("appsettings.json")
+                ;
+
+            var webHostBuilder = new WebHostBuilder()
+                    .UseContentRoot(contentRoot)
+                    .ConfigureServices(InitializeServices)
+                    .UseConfiguration(configurationBuilder.Build())
+                    .UseEnvironment("Test")
+                    .UseStartup(typeof(TestStartup))
+                ;
+
+            // Create instance of test server
+            return webHostBuilder;
+        }
+
+        private static void InitializeServices(IServiceCollection services)
+        {
+            var manager = new ApplicationPartManager
+            {
+                ApplicationParts =
+                {
+                    new AssemblyPart(STARTUP_ASSEMBLY)
+                },
+                FeatureProviders =
+                {
+                    new ControllerFeatureProvider(),
+                    new ViewComponentFeatureProvider()
+                }
+            };
+
+            services.AddSingleton(manager);
+        }
+
+        private static string GetProjectPath(string projectRelativePath, Assembly startupAssembly)
+        {
+            var projectName = startupAssembly.GetName().Name;
+            var applicationBasePath = AppContext.BaseDirectory;
+            var directoryInfo = new DirectoryInfo(applicationBasePath);
+
+            do
+            {
+                directoryInfo = directoryInfo.Parent;
+
+                if (directoryInfo == null) continue;
+                var projectDirectoryInfo = new DirectoryInfo(Path.Combine(directoryInfo.FullName, projectRelativePath));
+
+                if (!projectDirectoryInfo.Exists) continue;
+                if (new FileInfo(Path.Combine(projectDirectoryInfo.FullName, projectName!, $"{projectName}.csproj")).Exists)
+                    return Path.Combine(projectDirectoryInfo.FullName, projectName);
+            }
+            while (directoryInfo?.Parent != null);
+
+            throw new Exception($"Project root could not be located using the application root {applicationBasePath}.");
+        }
+    }
+}
---
+++ GetIntoTeachingApiContractTests/TestStartup.cs
@@ -0,0 +1,30 @@
+﻿using GetIntoTeachingApi.Services;
+using GetIntoTeachingApiContractTests.Helpers;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GetIntoTeachingApiContractTests
+{
+    public class TestStartup : GetIntoTeachingApi.Startup
+    {
+        public TestStartup(IConfiguration configuration)
+            :base(configuration)
+        {
+        }
+
+        public override void Configure(IApplicationBuilder app, IWebHostEnvironment hostEnv)
+        {
+            base.Configure(app, hostEnv);
+            
+            using var serviceScope = app.ApplicationServices.CreateScope();
+            
+            var store = serviceScope.ServiceProvider.GetService<IStore>();
+            var crmService = serviceScope.ServiceProvider.GetService<ICrmService>();
+            
+            AsyncHelper.RunSync(() => store.SyncAsync(crmService));
+        }
+
+    }
+}
