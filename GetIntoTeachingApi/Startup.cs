34f9af577ee8875ef3edce9551a996301abe7842
matt <mattonfoot@gmail.com>
2020-09-03 15:16:57 +0100

    Facade added which will cache actual CRM Clinet Library responses or replay pre defined ones

--- GetIntoTeachingApi/Startup.cs
+++ GetIntoTeachingApi/Startup.cs
@@ -34,7 +34,7 @@ namespace GetIntoTeachingApi
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public virtual void ConfigureServices(IServiceCollection services)
         {
             var env = new Env();
 
--- GetIntoTeachingApiContractTests/Servers/ServerUnderTest.cs
+++ GetIntoTeachingApiContractTests/Servers/ServerUnderTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Reflection;
+using GetIntoTeachingApi.Adapters;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.ApplicationParts;
 using Microsoft.AspNetCore.Mvc.Controllers;
@@ -8,13 +9,25 @@ using Microsoft.AspNetCore.Mvc.ViewComponents;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Xrm.Sdk;
 
 namespace GetIntoTeachingApiContractTests.Servers
 {
-    public class ServerUnderTest<TStartup> : TestServer
+    public class ServerUnderTest : TestServer
+    {
+        public static TestOrganizationServiceAdapter CrmServiceAdapter { get; protected set; }
+
+        protected ServerUnderTest(IWebHostBuilder builder)
+            : base(builder)
+        {
+            
+        }
+    }
+    
+    public class ServerUnderTest<TStartup> : ServerUnderTest
     {
         private static readonly Assembly STARTUP_ASSEMBLY = typeof(TStartup).GetTypeInfo().Assembly;
-
+        
         public ServerUnderTest(string relativeTargetProjectParentDir)
             : base(Setup(relativeTargetProjectParentDir))
         {
@@ -32,7 +45,15 @@ namespace GetIntoTeachingApiContractTests.Servers
 
             var webHostBuilder = new WebHostBuilder()
                     .UseContentRoot(contentRoot)
-                    .ConfigureServices(InitializeServices)
+                    .ConfigureServices(InitializeServices) 
+                    .ConfigureTestServices(services =>
+                    {
+                        services.AddTransient<IOrganizationServiceAdapter>(sp =>
+                        {
+                            CrmServiceAdapter = new TestOrganizationServiceAdapter(sp.GetService<IOrganizationService>(), contentRoot);
+                            return CrmServiceAdapter;
+                        });
+                    })
                     .UseConfiguration(configurationBuilder.Build())
                     .UseEnvironment("Test")
                     .UseStartup(typeof(TestStartup))
