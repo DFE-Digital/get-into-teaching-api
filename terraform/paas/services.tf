bc5bc9361c4053825c8eb3573528eb7045c2f867
Steven Fawcett <steve.fawcett@capgemini.com>
2020-09-22 15:50:57 +0100

    Deployment Fixed

--- terraform/paas/services.tf
+++ terraform/paas/services.tf
@@ -6,15 +6,19 @@ data cloudfoundry_service redis {
     name = "redis"
 }
 
-resource "cloudfoundry_service_instance" "hangfire" {
-  name = var.paas_postgres_1_name
-  space = data.cloudfoundry_space.space.id
-  service_plan = data.cloudfoundry_service.postgres.service_plans["small-11"]
-  json_params = "{\"enable_extensions\": [\"postgis\"] }" 
+data cloudfoundry_service mysql {
+    name = "mysql"
 }
 
-resource "cloudfoundry_service_instance" "postgres2" {
-  name = var.paas_postgres_2_name
+
+resource "cloudfoundry_service_instance" "hangfire" {
+  name = var.paas_database_1_name
+  space = data.cloudfoundry_space.space.id
+  service_plan = data.cloudfoundry_service.postgres.service_plans["small-11"]
+}
+
+resource "cloudfoundry_service_instance" "crm_cache" {
+  name = var.paas_database_2_name
   space = data.cloudfoundry_space.space.id
   service_plan = data.cloudfoundry_service.postgres.service_plans["small-11"]
   json_params = "{\"enable_extensions\": [\"postgis\"] }" 
