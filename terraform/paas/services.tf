4cd7f5a542a822850b46db1d2dd5904eac5c7708
Steven Fawcett <steve.fawcett@capgemini.com>
2020-09-22 11:09:50 +0100

    Added a redis instance to API deployment, this is not for API but just because API is master deploy

--- terraform/paas/services.tf
+++ terraform/paas/services.tf
@@ -9,14 +9,14 @@ data cloudfoundry_service redis {
 resource "cloudfoundry_service_instance" "hangfire" {
   name = var.paas_postgres_1_name
   space = data.cloudfoundry_space.space.id
-  service_plan = data.cloudfoundry_service.postgres.service_plans["small-10"]
+  service_plan = data.cloudfoundry_service.postgres.service_plans["small-11"]
   json_params = "{\"enable_extensions\": [\"postgis\"] }" 
 }
 
 resource "cloudfoundry_service_instance" "postgres2" {
   name = var.paas_postgres_2_name
   space = data.cloudfoundry_space.space.id
-  service_plan = data.cloudfoundry_service.postgres.service_plans["small-10"]
+  service_plan = data.cloudfoundry_service.postgres.service_plans["small-11"]
   json_params = "{\"enable_extensions\": [\"postgis\"] }" 
 }
 
@@ -27,3 +27,9 @@ resource "cloudfoundry_user_provided_service" "logging" {
   syslog_drain_url = var.paas_logging_endpoint_port 
 }
 
+resource "cloudfoundry_service_instance" "redis" {
+  name = var.paas_redis_1_name
+  space = data.cloudfoundry_space.space.id
+  service_plan = data.cloudfoundry_service.redis.service_plans["small-ha-5_x"]
+  json_params = "{\"maxmemory_policy\": \"allkeys-lfu\" }"
+}
