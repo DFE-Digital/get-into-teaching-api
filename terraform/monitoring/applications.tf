f2b3a4af57a8b80d2ad5d07a54fc07c1c7f07fdc
Steven Fawcett <steve.fawcett@capgemini.com>
2020-09-29 17:50:22 +0100

    Internal Routes connected

---
+++ terraform/monitoring/applications.tf
@@ -0,0 +1,14 @@
+data "cloudfoundry_app" "app" {
+    name_or_id = var.app_application_name
+    space      = data.cloudfoundry_space.space.id
+}
+
+data "cloudfoundry_app" "api" {
+    name_or_id = var.api_application_name
+    space      = data.cloudfoundry_space.space.id
+}
+
+data "cloudfoundry_app" "tta" {
+    name_or_id = var.tta_application_name
+    space      = data.cloudfoundry_space.space.id
+}
--- terraform/monitoring/dev.env.tfvars
+++ terraform/monitoring/dev.env.tfvars
@@ -1,3 +1,5 @@
 paas_space="get-into-teaching"
 api_application_name="get-into-teaching-api-dev"
+app_application_name="get-into-teaching-app-dev"
+tta_application_name="get-teacher-training-adviser-service-dev"
 environment="dev"
