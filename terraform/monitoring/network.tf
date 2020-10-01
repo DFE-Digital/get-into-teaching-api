e5e2e3d9a0fa521995fb10827081b5a98d3b7f0e
Steven Fawcett <steve.fawcett@capgemini.com>
2020-10-01 13:31:34 +0100

    Picked up correct routes

--- terraform/monitoring/network.tf
+++ terraform/monitoring/network.tf
@@ -3,22 +3,19 @@ resource "cloudfoundry_network_policy" "my-policy" {
   policy {
     destination_app = data.cloudfoundry_app.app.id
     source_app      = module.prometheus.app_id
-    port            = "8080"
+    port            = var.port
   }
 
   policy {
     destination_app = data.cloudfoundry_app.tta.id
     source_app      = module.prometheus.app_id
-    port            = "8080"
+    port            = var.port
   }
 
   policy {
     destination_app = data.cloudfoundry_app.api.id
     source_app      = module.prometheus.app_id
-    port            = "8080"
+    port            = var.port
   }
 }
 
-output "policy" {
-  value = cloudfoundry_network_policy.my-policy
-}
--- terraform/monitoring/routes.tf
+++ terraform/monitoring/routes.tf
@@ -7,4 +7,20 @@ locals {
   api_endpoint = "${data.cloudfoundry_route.api_endpoint.hostname}.${data.cloudfoundry_domain.education.name}"
 }
 
+data "cloudfoundry_route" "api_internal" {
+  domain   = data.cloudfoundry_domain.internal.id
+  hostname = "${var.api_application_name}-internal"
+}
+
+
+data "cloudfoundry_route" "app_internal" {
+  domain   = data.cloudfoundry_domain.internal.id
+  hostname = "${var.app_application_name}-internal"
+}
+
+data "cloudfoundry_route" "tta_internal" {
+  domain   = data.cloudfoundry_domain.internal.id
+  hostname = "${var.tta_application_name}-internal"
+}
+
 
