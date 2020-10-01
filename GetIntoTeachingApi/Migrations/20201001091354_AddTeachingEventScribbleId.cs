62be4dc783d035fba8358ed0dc5d6ab4ccb3b2f6
Ross Oliver <ross.oliver36@gmail.com>
2020-10-01 10:14:08 +0100

    Migration to add scribbleId to TeachingEvent

---
+++ GetIntoTeachingApi/Migrations/20201001091354_AddTeachingEventScribbleId.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GetIntoTeachingApi.Migrations
+{
+    public partial class AddTeachingEventScribbleId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ScribbleId",
+                table: "TeachingEvents",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ScribbleId",
+                table: "TeachingEvents");
+        }
+    }
+}
