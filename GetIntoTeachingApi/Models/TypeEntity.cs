4c32293e1aa01feef43c8cbb05c85ab2425243f0
Ross Oliver <ross.oliver36@gmail.com>
2020-10-19 09:03:31 +0100

    Default PreferredTeachingSubjectId to Primary
    When a Candidate signs up with a `PreferredEducationPhaseId` of primary we do
    not ask them their preferred teaching subject (only secondary teachers will
    specialise in a particular teaching subject). The CRM, however, requires this
    field to be set and it should have the guid corresponding to `Primary`.

--- GetIntoTeachingApi/Models/TypeEntity.cs
+++ GetIntoTeachingApi/Models/TypeEntity.cs
@@ -11,6 +11,7 @@ namespace GetIntoTeachingApi.Models
     public class TypeEntity
     {
         public static readonly Guid UnitedKingdomCountryId = new Guid("72f5c2e6-74f9-e811-a97a-000d3a2760f2");
+        public static readonly Guid PrimaryTeachingSubjectId = new Guid("b02655a1-2afa-e811-a981-000d3a276620");
 
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public string Id { get; set; }
