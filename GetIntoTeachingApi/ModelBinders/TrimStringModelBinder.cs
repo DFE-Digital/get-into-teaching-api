7d140020c3aab9b938b5fbbc4529a2f7b03b084f
Ross Oliver <ross.oliver36@gmail.com>
2020-10-20 10:58:25 +0100

    Trim strings when binding to models
    When a HTTP request comes in to ASP.NET Web API the payload is bound to a
    request model; when this happens we hook into the operation and ensure any
    string attributes are trimmed.
    
    When we read/write JSON string attributes there aren't any instances where
    having trailing/leading white space would be useful or intentional.
    
    Accepting white space on the start/end of strings is currently causing issues
    with the postcode validator, as the regex is strict in this respect, yet it
    shouldn't matter if a user enters a postcode with white space around it.
    
    Initially I attempted doing this with a custom `JsonConverter`, but that only
    applied to POST request body payloads and not GET query parameters. By using a
    model binder we should be covered for any content-type.

---
+++ GetIntoTeachingApi/ModelBinders/TrimStringModelBinder.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace GetIntoTeachingApi.ModelBinders
+{
+    public class TrimStringModelBinder : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            if (bindingContext == null)
+            {
+                throw new ArgumentNullException(nameof(bindingContext));
+            }
+
+            var modelName = bindingContext.ModelName;
+
+            // Try to fetch the value of the argument by name
+            var valueProviderResult = bindingContext.ValueProvider.GetValue(modelName);
+
+            if (valueProviderResult == ValueProviderResult.None)
+            {
+                return Task.CompletedTask;
+            }
+
+            bindingContext.ModelState.SetModelValue(modelName, valueProviderResult);
+
+            var value = valueProviderResult.FirstValue;
+
+            bindingContext.Result = ModelBindingResult.Success(value?.Trim());
+            return Task.CompletedTask;
+        }
+    }
+}
