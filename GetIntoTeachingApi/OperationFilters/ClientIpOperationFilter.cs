97db91b258a5c2dec068fffb55151ab2f62fca53
Ross Oliver <ross.oliver36@gmail.com>
2020-10-22 10:15:50 +0100

    Accept X-Client-IP header on all requests
    Update the Swagger configuration to accept an `X-Client-IP` header on all
    requests. The auto-generated library will allow this to be specified per-API
    request, for example:
    
    ```
    result = api_instance.get_callback_booking_quotas(x_client_ip: "1.2.3.4")
    ```
    
    Going forward this IP address will be used to rate limit requests to the API,
    in addition to the API key of the client.
    
    If unspecified the requests will fall into the global client rate limiting
    bucket.
    
    Can be set to the end user IP address to allow per-user rate limiting in the
    API. This is how we will be rate limiting individual users when they request a
    PIN code email (so that they cannot abuse the GOV.UK Notify service to send
    spam).

---
+++ GetIntoTeachingApi/OperationFilters/ClientIpOperationFilter.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace GetIntoTeachingApi.OperationFilters
+{
+    public class ClientIpOperationFilter : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            var parameter = new OpenApiParameter()
+            {
+                Name = "X-Client-IP",
+                In = ParameterLocation.Header,
+                Description = "IP address of the end user or client " +
+                "application used for rate limiting. Will fall into a globally " +
+                "rate limited bucket if not specified.",
+                Required = false,
+            };
+
+            operation.Parameters.Add(parameter);
+        }
+    }
+}
