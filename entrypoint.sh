f7f3312e46227a7e53eb92850f05e56484cfa5a1
Ross Oliver <ross.oliver36@gmail.com>
2020-11-03 13:05:21 +0000

    Run migrations manually with dotnet exec
    The `dotnet ef database update` command only works when we have access to the
    source code; in CI we only have access to the published app DLL. In order to
    run the migrations we have to use `dotnet exec` directly, which is what `dotnet
    ef` calls under the hood.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,12 @@
-dotnet ef database update
+# See https://www.benday.com/2017/03/17/deploy-entity-framework-core-migrations-from-a-dll/
+EfMigrationsNamespace=GetIntoTeachingApi
+EfMigrationsDllName=GetIntoTeachingApi.dll
+EfMigrationsDllDepsJson=GetIntoTeachingApi.deps.json
+DllDir=$PWD
+EfMigrationsDllDepsJsonPath=$PWD/bin/$BuildFlavor/netcoreapp1.0/$EfMigrationsDllDepsJson
+PathToNuGetPackages=$HOME/.nuget/packages
+PathToEfDll=$PathToNuGetPackages/microsoft.entityframeworkcore.tools.dotnet/3.1.9/tools/netcoreapp1.0/ef.dll
+
+dotnet exec --depsfile ./$EfMigrationsDllDepsJson --additionalprobingpath $PathToNuGetPackages $PathToEfDll database update --assembly ./$EfMigrationsDllName --startup-assembly ./$EfMigrationsDllName --project-dir . --content-root $DllDir --data-dir $DllDir --verbose --root-namespace $EfMigrationsNamespace
+
 dotnet GetIntoTeachingApi.dll
