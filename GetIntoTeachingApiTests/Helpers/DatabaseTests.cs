93c20e36ee147accd079eac2876535c1162f52aa
Ross Oliver <ross.oliver36@gmail.com>
2020-11-16 13:27:44 +0000

    Tweaks to make development setup easier
    Default some of the env variables that are not sensitive/don't need to change.
    
    Clean up `DatabaseTests`.

--- GetIntoTeachingApiTests/Helpers/DatabaseTests.cs
+++ GetIntoTeachingApiTests/Helpers/DatabaseTests.cs
@@ -7,23 +7,15 @@ namespace GetIntoTeachingApiTests.Helpers
 {
     public abstract class DatabaseTests : IDisposable
     {
-        public GetIntoTeachingDbContext DbContext { get; }
+        public GetIntoTeachingDbContext DbContext { get; set; }
 
         protected DatabaseTests(DatabaseFixture databaseFixture)
         {
             var id = Guid.NewGuid().ToString().Replace("-", "");
             var databaseName = $"gis_test_{id}";
-            var connectionString = $"Host=localhost;Database={databaseName};Username=docker;Password=docker";
 
-            using var templateConnection = new NpgsqlConnection(databaseFixture.ConnectionString);
-            templateConnection.Open();
-
-            using var command = new NpgsqlCommand($"CREATE DATABASE {databaseName} WITH TEMPLATE {databaseFixture.TemplateDatabaseName};", templateConnection);
-            command.ExecuteNonQuery();
-                
-            var builder = new DbContextOptionsBuilder<GetIntoTeachingDbContext>();
-            DbConfiguration.ConfigPostgres(connectionString, builder);
-            DbContext = new GetIntoTeachingDbContext(builder.Options);
+            CloneTemplateDatabase(databaseFixture, databaseName);
+            SetupDbContext(databaseName);
         }
 
         public void Dispose()
@@ -36,6 +28,25 @@ namespace GetIntoTeachingApiTests.Helpers
                 // StoreTests.CheckStatusAsync_WhenUnhealthy_ReturnsError
                 // disposes of the connection prematurely as part of the test.
             }
+        }
+
+        private void SetupDbContext(string databaseName)
+        {
+            var connectionString = $"Host=localhost;Database={databaseName};Username=docker;Password=docker";
+            var builder = new DbContextOptionsBuilder<GetIntoTeachingDbContext>();
+
+            DbConfiguration.ConfigPostgres(connectionString, builder);
+
+            DbContext = new GetIntoTeachingDbContext(builder.Options);
+        }
+
+        private static void CloneTemplateDatabase(DatabaseFixture databaseFixture, string databaseName)
+        {
+            using var templateConnection = new NpgsqlConnection(databaseFixture.ConnectionString);
+            templateConnection.Open();
+
+            using var command = new NpgsqlCommand($"CREATE DATABASE {databaseName} WITH TEMPLATE {databaseFixture.TemplateDatabaseName};", templateConnection);
+            command.ExecuteNonQuery();
         }
     }
 }
