e921968fcc61fc05df28b150fcbb2614ebd72c9a
Ross Oliver <ross.oliver36@gmail.com>
2020-12-09 13:59:52 +0000

    Log request details on validation failure
    Currently when the API receives a request the appropriate request model
    validator is ran automatically before the controller action is called. The side
    effect of this is that the `LoggableAttribute` action filter is never ran for
    these requests, meaning we don't have meaningful logs around what was in the
    request payload.
    
    This commit breaks out the logic to translate an `ActionExecutingContext` and
    `ControllerActionDescriptor` into formatted, sanitized log messages to a new
    utility class `LoggableMessageComposer`.
    
    We then hook into the `PostConfigure` callback and override the
    `InvalidModelStateResponseFactory` to log out the request details before
    calling the built in factory that formats and returns a helpful 400 response..

---
+++ GetIntoTeachingApi/Utils/LoggableMessageComposer.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using GetIntoTeachingApi.Attributes;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace GetIntoTeachingApi.Utils
+{
+    public class LoggableMessageComposer
+    {
+        public static string LogMessage(string prefix, ControllerActionDescriptor descriptor, IEnumerable<string> messages)
+        {
+            var actionName = descriptor.ActionName;
+            var controllerName = descriptor.ControllerName;
+            var payload = string.Join("\n", messages.Where(m => !string.IsNullOrEmpty(m)));
+
+            return $"{prefix} {controllerName}:{actionName} - {payload}";
+        }
+
+        public static string LogMessageForObject(object obj)
+        {
+            var type = obj.GetType().GetGenericArguments().FirstOrDefault();
+
+            if (type == null)
+            {
+                type = obj.GetType();
+            }
+
+            if (type.GetCustomAttribute<LoggableAttribute>() == null)
+            {
+                return null;
+            }
+
+            return JsonConvert.SerializeObject(
+                obj,
+                Formatting.None,
+                new JsonSerializerSettings
+                {
+                    ContractResolver = new FilterSensitiveDataContractResolver(),
+                });
+        }
+
+        private class FilterSensitiveDataContractResolver : DefaultContractResolver
+        {
+            protected override JsonProperty CreateProperty(MemberInfo member, MemberSerialization memberSerialization)
+            {
+                var property = base.CreateProperty(member, memberSerialization);
+
+                property.ShouldSerialize = instance =>
+                    instance.GetType().GetCustomAttribute<LoggableAttribute>() != null &&
+                    member.GetCustomAttribute<SensitiveDataAttribute>() == null &&
+                    member.GetCustomAttribute<System.Text.Json.Serialization.JsonIgnoreAttribute>() == null;
+
+                return property;
+            }
+        }
+    }
+}
