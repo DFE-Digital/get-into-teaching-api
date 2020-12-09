f285253c17b4a5117fbc951a7c4893ab913def84
Ross Oliver <ross.oliver36@gmail.com>
2020-12-09 13:59:52 +0000

    Log request details on validation failure
    Currently when the API receives a request the appropriate request model
    validator is ran automatically before the controller action is called. The side
    effect of this is that the `LoggableAttribute` action filter is never ran for
    these requests, meaning we don't have meaningful logs around what was in the
    request payload.
    
    This commit breaks out the logic to translate an `ActionExecutingContext` and
    `ControllerActionDescriptor` into formatted, sanitized log messages to a new
    utility class `LoggableMessageComposer`.
    
    We then hook into the `PostConfigure` callback and override the
    `InvalidModelStateResponseFactory` to log out the request details before
    calling the built in factory that formats and returns a helpful 400 response..

--- GetIntoTeachingApi/Attributes/LogRequestsAttribute.cs
+++ GetIntoTeachingApi/Attributes/LogRequestsAttribute.cs
@@ -1,13 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
+using GetIntoTeachingApi.Utils;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Controllers;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
 
 namespace GetIntoTeachingApi.Attributes
 {
@@ -31,8 +29,8 @@ namespace GetIntoTeachingApi.Attributes
         {
             var descriptor = (ControllerActionDescriptor)context.ActionDescriptor;
 
-            var messages = context.ActionArguments.Select(a => LogMessageForObject(a.Value));
-            var message = LogMessage("Request", descriptor, messages);
+            var messages = context.ActionArguments.Select(a => LoggableMessageComposer.LogMessageForObject(a.Value));
+            var message = LoggableMessageComposer.LogMessage("Request", descriptor, messages);
             _logger.LogInformation(message);
         }
 
@@ -41,56 +39,9 @@ namespace GetIntoTeachingApi.Attributes
             if (context.Result is ObjectResult result)
             {
                 var descriptor = (ControllerActionDescriptor)context.ActionDescriptor;
-                var messages = new List<string> { LogMessageForObject(result.Value) };
-                var message = LogMessage("Response", descriptor, messages);
+                var messages = new List<string> { LoggableMessageComposer.LogMessageForObject(result.Value) };
+                var message = LoggableMessageComposer.LogMessage("Response", descriptor, messages);
                 _logger.LogInformation(message);
-            }
-        }
-
-        private static string LogMessage(string type, ControllerActionDescriptor descriptor, IEnumerable<string> messages)
-        {
-            var actionName = descriptor.ActionName;
-            var controllerName = descriptor.ControllerName;
-            var payload = string.Join("\n", messages.Where(m => !string.IsNullOrEmpty(m)));
-
-            return $"{type} {controllerName}:{actionName} - {payload}";
-        }
-
-        private static string LogMessageForObject(object obj)
-        {
-            var type = obj.GetType().GetGenericArguments().FirstOrDefault();
-
-            if (type == null)
-            {
-                type = obj.GetType();
-            }
-
-            if (type.GetCustomAttribute<LoggableAttribute>() == null)
-            {
-                return null;
-            }
-
-            return JsonConvert.SerializeObject(
-                obj,
-                Formatting.None,
-                new JsonSerializerSettings
-                {
-                    ContractResolver = new FilterSensitiveDataContractResolver(),
-                });
-        }
-
-        private class FilterSensitiveDataContractResolver : DefaultContractResolver
-        {
-            protected override JsonProperty CreateProperty(MemberInfo member, MemberSerialization memberSerialization)
-            {
-                var property = base.CreateProperty(member, memberSerialization);
-
-                property.ShouldSerialize = instance =>
-                    instance.GetType().GetCustomAttribute<LoggableAttribute>() != null &&
-                    member.GetCustomAttribute<SensitiveDataAttribute>() == null &&
-                    member.GetCustomAttribute<System.Text.Json.Serialization.JsonIgnoreAttribute>() == null;
-
-                return property;
             }
         }
     }
