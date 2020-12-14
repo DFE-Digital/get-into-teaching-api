d3f28a135649842158c6a13a81b4fe0e8021851e
Ross Oliver <ross.oliver36@gmail.com>
2020-12-14 11:50:48 +0000

    Add migration for PickListItem table

---
+++ GetIntoTeachingApi/Migrations/20201214101842_AddPickListItem.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GetIntoTeachingApi.Migrations
+{
+    public partial class AddPickListItem : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PickListItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false),
+                    EntityName = table.Column<string>(nullable: false),
+                    AttributeName = table.Column<string>(nullable: false),
+                    Value = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PickListItems", x => new { x.Id, x.EntityName, x.AttributeName });
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PickListItems");
+        }
+    }
+}
