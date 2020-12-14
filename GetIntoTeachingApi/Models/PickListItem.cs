0e0d0b008552a613fa5f30724c7eb4798c4ed397
Ross Oliver <ross.oliver36@gmail.com>
2020-12-14 11:50:08 +0000

    Add PickListItem model
    Adds the new `PickListItem` model that is used to represent CRM pick list
    items, which are currently `TypeEntity` models that have an integer `Id` cast
    as a string.

---
+++ GetIntoTeachingApi/Models/PickListItem.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+using GetIntoTeachingApi.Attributes;
+using Microsoft.PowerPlatform.Cds.Client;
+
+namespace GetIntoTeachingApi.Models
+{
+    [Loggable]
+    public class PickListItem
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Id { get; set; }
+        public string Value { get; set; }
+        [JsonIgnore]
+        public string EntityName { get; set; }
+        [JsonIgnore]
+        public string AttributeName { get; set; } = string.Empty;
+
+        public PickListItem()
+        {
+        }
+
+        public PickListItem(CdsServiceClient.PickListItem pickListItem, string entityName, string attributeName)
+        {
+            Id = pickListItem.PickListItemId;
+            Value = pickListItem.DisplayLabel;
+            EntityName = entityName;
+            AttributeName = attributeName;
+        }
+    }
+}
