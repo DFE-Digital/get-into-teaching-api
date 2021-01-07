ca0bfb90d8583097276c1539fce8e23dc1bb5c15
Steven <64777084+sfawcett123@users.noreply.github.com>
2021-01-07 12:54:08 +0000

    Devops/monitor/merge/prod influx (#442)
    * Import Production state
    
    * Development Monitoring Delivered in main pipeline
    
    * Keep InfluxDB Safe

--- terraform/monitoring/future/alertmanager.tf
+++
@@ -1,6 +0,0 @@
-module "alertmanager" {
-     source = "git::https://github.com/DFE-Digital/bat-platform-building-blocks.git//terraform/modules/alertmanager?ref=devops/get-into-teaching/add-postgres"
-     space_id = data.cloudfoundry_space.space.id
-     name = "${var.environment}-${var.alertmanager[ "name" ]}"
-     config = var.alertmanager[ "config" ]
-}
--- terraform/monitoring/services.tf
+++
@@ -1,4 +0,0 @@
-data "cloudfoundry_service_instance" "redis" {
-    name_or_id = var.redis_service
-    space      = data.cloudfoundry_space.application_space.id
-}
