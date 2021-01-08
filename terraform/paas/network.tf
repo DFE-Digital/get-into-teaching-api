17c53011a4765de24ceaaf0e649bfabc172af382
Steven Fawcett <steve.fawcett@capgemini.com>
2021-01-08 12:12:19 +0000

    Added Policies for APP and TTA

--- terraform/paas/network.tf
+++ terraform/paas/network.tf
@@ -1,8 +1,28 @@
+data cloudfoundry_app app_application {
+    name_or_id = var.app_application_name
+    space      = data.cloudfoundry_space.space.id
+}
+
+data cloudfoundry_app tta_application {
+    name_or_id = var.tta_application_name
+    space      = data.cloudfoundry_space.space.id
+}
+
 resource "cloudfoundry_network_policy" "api-policy" {
   count = var.monitoring
   policy {
     destination_app = cloudfoundry_app.api_application.id_bg
     source_app      = module.prometheus[0].app_id
     port            = 8080
+  }
+  policy {
+    destination_app = data.cloudfoundry_app.app_application.id
+    source_app      = module.prometheus[0].app_id
+    port            = 3000
+  }
+  policy {
+    destination_app = data.cloudfoundry_app.tta_application.id
+    source_app      = module.prometheus[0].app_id
+    port            = 3000
   }
 }
