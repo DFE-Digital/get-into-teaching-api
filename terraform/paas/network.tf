b48107f97ca7a57d88b3d604af22c3975e8d97f0
Steven <64777084+sfawcett123@users.noreply.github.com>
2021-01-08 09:50:36 +0000

    Delete network.tf

--- terraform/paas/network.tf
+++
@@ -1,7 +0,0 @@
-resource "cloudfoundry_network_policy" "api-policy" {
-  policy {
-    destination_app = cloudfoundry_app.api_application.id
-    source_app      = module.prometheus[0].app_id
-    port            = 8080
-  }
-}
