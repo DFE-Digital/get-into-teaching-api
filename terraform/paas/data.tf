b8bca4a66e0b43a6ce72326a6f4f0f87dbc746a2
Steven Fawcett <steve.fawcett@capgemini.com>
2021-02-02 13:01:52 +0000

    Initial Migratrion to Azure Secrets
    The first phase is to deliver the terraform application using the
    secrets, and make sure that works before rolling forward with the
    workflows

---
+++ terraform/paas/data.tf
@@ -0,0 +1,50 @@
+data azurerm_key_vault vault {
+  name                = var.azure_key_vault
+  resource_group_name = var.azure_resource_group
+}
+
+data azurerm_key_vault_secret application {
+  key_vault_id = data.azurerm_key_vault.vault.id
+  name         = "API-KEYS"
+}
+
+data azurerm_key_vault_secret monitoring {
+  key_vault_id = data.azurerm_key_vault.vault.id
+  name         = "MONITORING-KEYS"
+}
+
+locals {
+  application_secrets = yamldecode(data.azurerm_key_vault_secret.application.value)
+  monitoring_secrets = yamldecode(data.azurerm_key_vault_secret.monitoring.value)
+}
+
+data azurerm_key_vault_secret docker_username {
+  key_vault_id = data.azurerm_key_vault.vault.id
+  name         = "DOCKER-USERNAME"
+}
+
+data azurerm_key_vault_secret docker_password {
+  key_vault_id = data.azurerm_key_vault.vault.id
+  name         = "DOCKER-PASSWORD"
+}
+
+data azurerm_key_vault_secret paas_username {
+  key_vault_id = data.azurerm_key_vault.vault.id
+  name         = "PAAS-USERNAME"
+}
+
+data azurerm_key_vault_secret paas_password {
+  key_vault_id = data.azurerm_key_vault.vault.id
+  name         = "PAAS-PASSWORD"
+}
+
+data azurerm_key_vault_secret statuscake_username {
+  key_vault_id = data.azurerm_key_vault.vault.id
+  name         = "SC-USERNAME"
+}
+
+data azurerm_key_vault_secret statuscake_password {
+  key_vault_id = data.azurerm_key_vault.vault.id
+  name         = "SC-PASSWORD"
+}
+
