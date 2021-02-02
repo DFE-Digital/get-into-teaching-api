ae07f1d9ddffe4df0602937159acacbc19b28dae
Ross Oliver <ross.oliver36@gmail.com>
2021-02-02 10:42:04 +0000

    Enable per-client rate limit rules
    Add a custom `ResolveContributor` that looks up the API client from their API
    key and sets the `clientId` to the `ApiKeyPrefix`.
    
    Define custom rate limiting rules for `GIT` and `TTA` clients.
    
    The `ADMIN` client has been white-listed and a set of default rate limits
    emposed on potentially exploitable endpoints.

---
+++ GetIntoTeachingApi/RateLimiting/ApiClientRateLimitConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using AspNetCoreRateLimit;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+
+namespace GetIntoTeachingApi.RateLimiting
+{
+    public class ApiClientRateLimitConfiguration : RateLimitConfiguration
+    {
+        public ApiClientRateLimitConfiguration(
+            IHttpContextAccessor httpContextAccessor,
+            IOptions<IpRateLimitOptions> ipOptions,
+            IOptions<ClientRateLimitOptions> clientOptions)
+            : base(httpContextAccessor, ipOptions, clientOptions)
+        {
+        }
+
+        protected override void RegisterResolvers()
+        {
+            ClientResolvers.Add(new ApiClientResolveContributor(HttpContextAccessor, ClientRateLimitOptions.ClientIdHeader));
+        }
+    }
+}
