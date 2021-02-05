cf221c031e549bbdda650c868bc01ddd9dd7e6db
Ross Oliver <ross.oliver36@gmail.com>
2021-02-05 10:38:15 +0000

    Add magic link token generator service
    Add a service to generate secure, random 128 bit magic link tokens. It also has
    a method that will exchange a valid token for the associated `Candidate`
    record.
    
    In the unlikely event that two candidates have been assigned the same magic
    link token, the service will return `null` (ultimately forcing a re-generation
    of the token for that candidate).

---
+++ GetIntoTeachingApiTests/Services/CandidateMagicLinkTokenServiceTests.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using GetIntoTeachingApi.Models;
+using GetIntoTeachingApi.Services;
+using Moq;
+using Xunit;
+
+namespace GetIntoTeachingApiTests.Services
+{
+    public class CandidateMagicLinkTokenServiceTests
+    {
+        private readonly Mock<ICrmService> _mockCrm;
+        private readonly CandidateMagicLinkTokenService _service;
+
+        public CandidateMagicLinkTokenServiceTests()
+        {
+            _mockCrm = new Mock<ICrmService>();
+            _service = new CandidateMagicLinkTokenService(_mockCrm.Object);
+        }
+
+        [Fact]
+        public void GenerateTokens_WithCandidates_SetsTokenAndCreatedAt()
+        {
+            var candidate = new Candidate();
+
+            _service.GenerateToken(candidate);
+
+            candidate.MagicLinkToken.Should().NotBeNull();
+            candidate.MagicLinkToken.Length.Should().Be(32);
+            candidate.MagicLinkTokenCreatedAt.Should().BeCloseTo(DateTime.UtcNow);
+        }
+
+        [Fact]
+        public void GenerateTokens_WhenCalledMultipleTimes_CreatesUniqueTokens()
+        {
+            var candidate = new Candidate();
+            var tokens = new List<string>();
+
+            _service.GenerateToken(candidate);
+            tokens.Add(candidate.MagicLinkToken);
+            _service.GenerateToken(candidate);
+            tokens.Add(candidate.MagicLinkToken);
+            _service.GenerateToken(candidate);
+            tokens.Add(candidate.MagicLinkToken);
+
+            tokens.Should().OnlyHaveUniqueItems();
+        }
+
+        [Fact]
+        public void Exchange_WithValidToken_ReturnsCandidate()
+        {
+            var candidate = new Candidate();
+            var token = Guid.NewGuid().ToString();
+            _mockCrm.Setup(m => m.MatchCandidates(token)).Returns(new Candidate[] { candidate });
+
+            var result = _service.Exchange(token);
+
+            result.Should().Be(candidate);
+        }
+
+        [Fact]
+        public void Exchange_WhenTokenMatchesMultipleCandidates_ReturnsNull()
+        {
+            var candidates = new List<Candidate>() { new Candidate(), new Candidate() };
+            var token = Guid.NewGuid().ToString();
+            _mockCrm.Setup(m => m.MatchCandidates(token)).Returns(candidates);
+
+            var result = _service.Exchange(token);
+
+            result.Should().BeNull();
+        }
+
+        [Fact]
+        public void Exchange_WhenTokenDoesNotMatch_ReturnsNull()
+        {
+            var token = Guid.NewGuid().ToString();
+            _mockCrm.Setup(m => m.MatchCandidates(token)).Returns(new Candidate[0]);
+
+            var result = _service.Exchange(token);
+
+            result.Should().BeNull();
+        }
+    }
+}
