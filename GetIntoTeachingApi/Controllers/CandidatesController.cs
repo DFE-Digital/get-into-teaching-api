ae56a5409bf4eca598d66d6751d7bda23ecdc121
Ross Oliver <ross.oliver36@gmail.com>
2021-02-05 10:38:21 +0000

    Add endpoint to generate candidate magic link tokens
    Introduce a new endpoint that will enable the CRM to batch-generate magic links
    for candidates.

--- GetIntoTeachingApi/Controllers/CandidatesController.cs
+++ GetIntoTeachingApi/Controllers/CandidatesController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using GetIntoTeachingApi.Attributes;
+using GetIntoTeachingApi.Jobs;
 using GetIntoTeachingApi.Models;
 using GetIntoTeachingApi.Services;
+using Hangfire;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Swashbuckle.AspNetCore.Annotations;
@@ -15,18 +18,26 @@ namespace GetIntoTeachingApi.Controllers
     [Authorize(Roles = "Admin,GetIntoTeaching,GetAnAdviser")]
     public class CandidatesController : ControllerBase
     {
-        private readonly ICandidateAccessTokenService _tokenService;
+        private const int MaximumMagicLinkTokensPerBatch = 25;
+
+        private readonly ICandidateAccessTokenService _accessTokenService;
+        private readonly ICandidateMagicLinkTokenService _magicLinkTokenService;
         private readonly INotifyService _notifyService;
         private readonly ICrmService _crm;
+        private readonly IBackgroundJobClient _jobClient;
 
         public CandidatesController(
-            ICandidateAccessTokenService tokenService,
+            ICandidateAccessTokenService accessTokenService,
+            ICandidateMagicLinkTokenService magicLinkTokenService,
             INotifyService notifyService,
-            ICrmService crm)
+            ICrmService crm,
+            IBackgroundJobClient jobClient)
         {
-            _tokenService = tokenService;
+            _accessTokenService = accessTokenService;
+            _magicLinkTokenService = magicLinkTokenService;
             _notifyService = notifyService;
             _crm = crm;
+            _jobClient = jobClient;
         }
 
         [HttpPost]
@@ -34,9 +45,9 @@ namespace GetIntoTeachingApi.Controllers
         [SwaggerOperation(
             Summary = "Creates a candidate access token.",
             Description = @"
-Finds a candidate matching at least 3 of the provided CandidateAccessTokenRequest attributes (including email). 
-If a candidate is found, an access token (PIN code) will be sent to the candidate email address 
-that can then be used for verification.",
+                Finds a candidate matching at least 3 of the provided CandidateAccessTokenRequest attributes (including email). 
+                If a candidate is found, an access token (PIN code) will be sent to the candidate email address 
+                that can then be used for verification.",
             OperationId = "CreateCandidateAccessToken",
             Tags = new[] { "Candidates" })]
         [ProducesResponseType(204)]
@@ -55,11 +66,46 @@ that can then be used for verification.",
                 return NotFound();
             }
 
-            var token = _tokenService.GenerateToken(request, (Guid)candidate.Id);
+            var token = _accessTokenService.GenerateToken(request, (Guid)candidate.Id);
             var personalisation = new Dictionary<string, dynamic> { { "pin_code", token } };
 
             // We respond immediately/assume this will be successful.
             _notifyService.SendEmailAsync(request.Email, NotifyService.NewPinCodeEmailTemplateId, personalisation);
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        [Route("magic_link_tokens")]
+        [SwaggerOperation(
+            Summary = "Creates a token for use in a magic link.",
+            Description = @"Creates a long-lived magic link token that can be exchanged for candidate information for up to 48 hours.
+                Magic link tokens are stored against the contact in Dynamics CRM and are issued to candidates by email.",
+            OperationId = "CreateCandidateMagicLinkToken",
+            Tags = new[] { "Candidates" })]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        public IActionResult CreateMagicLinkToken([FromBody, SwaggerRequestBody("Candidate identifiers.", Required = true)] IEnumerable<Guid> candidateIds)
+        {
+            if (candidateIds.Count() > MaximumMagicLinkTokensPerBatch)
+            {
+                return BadRequest($"You can only generate {MaximumMagicLinkTokensPerBatch} magic link tokens per request.");
+            }
+
+            var candidates = _crm.GetCandidates(candidateIds);
+
+            if (candidates.Count() != candidateIds.Count())
+            {
+                var missingCandidateIds = candidateIds.Except(candidates.Select(c => (Guid)c.Id));
+
+                return BadRequest(new { message = "Candidate IDs could not be found.", missingCandidateIds });
+            }
+
+            foreach (var candidate in candidates)
+            {
+                _magicLinkTokenService.GenerateToken(candidate);
+                _jobClient.Enqueue<UpsertCandidateJob>(x => x.Run(candidate, null));
+            }
 
             return NoContent();
         }
--- GetIntoTeachingApiTests/Controllers/CandidatesControllerTests.cs
+++ GetIntoTeachingApiTests/Controllers/CandidatesControllerTests.cs
@@ -9,22 +9,36 @@ using System.Collections.Generic;
 using Microsoft.AspNetCore.Authorization;
 using GetIntoTeachingApi.Attributes;
 using System;
+using Hangfire;
+using Hangfire.Common;
+using Hangfire.States;
+using GetIntoTeachingApi.Jobs;
+using System.Linq;
 
 namespace GetIntoTeachingApiTests.Controllers
 {
     public class CandidatesControllerTests
     {
-        private readonly Mock<ICandidateAccessTokenService> _mockTokenService;
+        private readonly Mock<ICandidateAccessTokenService> _mockAccessTokenService;
+        private readonly Mock<ICandidateMagicLinkTokenService> _mockMagicLinkTokenService;
         private readonly Mock<INotifyService> _mockNotifyService;
         private readonly Mock<ICrmService> _mockCrm;
+        private readonly Mock<IBackgroundJobClient> _mockJobClient;
         private readonly CandidatesController _controller;
 
         public CandidatesControllerTests()
         {
-            _mockTokenService = new Mock<ICandidateAccessTokenService>();
+            _mockAccessTokenService = new Mock<ICandidateAccessTokenService>();
+            _mockMagicLinkTokenService = new Mock<ICandidateMagicLinkTokenService>() { CallBase = true };
             _mockNotifyService = new Mock<INotifyService>();
+            _mockJobClient = new Mock<IBackgroundJobClient>();
             _mockCrm = new Mock<ICrmService>();
-            _controller = new CandidatesController(_mockTokenService.Object, _mockNotifyService.Object, _mockCrm.Object);
+            _controller = new CandidatesController(
+                _mockAccessTokenService.Object,
+                _mockMagicLinkTokenService.Object,
+                _mockNotifyService.Object,
+                _mockCrm.Object,
+                _mockJobClient.Object);
         }
 
         [Fact]
@@ -57,7 +71,7 @@ namespace GetIntoTeachingApiTests.Controllers
         {
             var request = new ExistingCandidateRequest { Email = "email@address.com", FirstName = "John", LastName = "Doe" };
             var candidate = new Candidate { Id = Guid.NewGuid(), Email = request.Email, FirstName = request.FirstName, LastName = request.LastName };
-            _mockTokenService.Setup(mock => mock.GenerateToken(request, (Guid)candidate.Id)).Returns("123456");
+            _mockAccessTokenService.Setup(mock => mock.GenerateToken(request, (Guid)candidate.Id)).Returns("123456");
             _mockCrm.Setup(mock => mock.MatchCandidate(request)).Returns(candidate);
 
             var response = _controller.CreateAccessToken(request);
@@ -86,6 +100,53 @@ namespace GetIntoTeachingApiTests.Controllers
                 mock.SendEmailAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<Dictionary<string, dynamic>>()),
                 Times.Never()
             );
+        }
+
+        [Fact]
+        public void CreateMagicLinkToken_TooManyCandidates_RespondsWithBadRequest()
+        {
+            var candidateIds = new Guid[26];
+
+            var response = _controller.CreateMagicLinkToken(candidateIds);
+
+            var badRequest = response.Should().BeOfType<BadRequestObjectResult>().Subject;
+            badRequest.Value.Should().Be("You can only generate 25 magic link tokens per request.");
+        }
+
+        [Fact]
+        public void CreateMagicLinkToken_MissingCandidateId_RespondsWithBadRequest()
+        {
+            var foundCandidateId = Guid.NewGuid();
+            var missingCandidateId = Guid.NewGuid();
+            var candidates = new List<Candidate>() { new Candidate() { Id = foundCandidateId } };
+            var candidate = candidates.First();
+            var candidateIds = new Guid[] { foundCandidateId, missingCandidateId };
+            _mockCrm.Setup(m => m.GetCandidates(candidateIds)).Returns(candidates);
+
+            var response = _controller.CreateMagicLinkToken(candidateIds);
+
+            var badRequest = response.Should().BeOfType<BadRequestObjectResult>().Subject;
+            badRequest.Value.Should().BeEquivalentTo(new { message = "Candidate IDs could not be found.", missingCandidateIds = new Guid[] { missingCandidateId } });
+        }
+
+        [Fact]
+        public void CreateMagicLinkToken_WithValidCandidateIds_GeneratesMagicLinkTokensAndUpdatesCandidate()
+        {
+            var candidates = new List<Candidate>() { new Candidate() { Id = Guid.NewGuid() } };
+            var candidate = candidates.First();
+            var candidateIds = candidates.Select(c => (Guid)c.Id);
+            _mockCrm.Setup(m => m.GetCandidates(candidateIds)).Returns(candidates);
+
+            var response = _controller.CreateMagicLinkToken(candidateIds);
+
+            response.Should().BeOfType<NoContentResult>();
+
+            _mockMagicLinkTokenService.Verify(m => m.GenerateToken(candidate));
+
+            _mockJobClient.Verify(x => x.Create(
+                It.Is<Job>(job => job.Type == typeof(UpsertCandidateJob) && job.Method.Name == "Run" &&
+                candidate == (Candidate)job.Args[0]),
+                It.IsAny<EnqueuedState>()));
         }
     }
 }
