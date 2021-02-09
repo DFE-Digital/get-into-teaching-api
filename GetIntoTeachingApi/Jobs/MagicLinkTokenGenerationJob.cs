8576e303abfa85536fa58844483737ef32dd2911
Ross Oliver <ross.oliver36@gmail.com>
2021-02-09 13:28:32 +0000

    Add job to generate magic link tokens
    The job runs every minute and processes 1,000 candidates (we should be able to
    do a lot more than this if needed, but it's a good starting point). Once a
    token has been generated the candidate magic link token status is updated to
    'generated' and the updated record is sent back to the CRM.

---
+++ GetIntoTeachingApi/Jobs/MagicLinkTokenGenerationJob.cs
@@ -0,0 +1,58 @@
+﻿using System.Linq;
+using GetIntoTeachingApi.Services;
+using GetIntoTeachingApi.Utils;
+using Hangfire;
+using Microsoft.Extensions.Logging;
+using Prometheus;
+
+namespace GetIntoTeachingApi.Jobs
+{
+    public class MagicLinkTokenGenerationJob : BaseJob
+    {
+        private const int BatchSize = 1000;
+        private readonly ICrmService _crm;
+        private readonly IBackgroundJobClient _jobClient;
+        private readonly ICandidateMagicLinkTokenService _magicLinkTokenService;
+        private readonly ILogger<MagicLinkTokenGenerationJob> _logger;
+        private readonly IMetricService _metrics;
+
+        public MagicLinkTokenGenerationJob(
+            IEnv env,
+            IBackgroundJobClient jobClient,
+            ICandidateMagicLinkTokenService magicLinkTokenService,
+            ICrmService crm,
+            ILogger<MagicLinkTokenGenerationJob> logger,
+            IMetricService metrics)
+            : base(env)
+        {
+            _jobClient = jobClient;
+            _magicLinkTokenService = magicLinkTokenService;
+            _crm = crm;
+            _logger = logger;
+            _metrics = metrics;
+        }
+
+        [DisableConcurrentExecution(timeoutInSeconds: 10 * 30)]
+        public void Run()
+        {
+            using (_metrics.MagicLinkTokenGenerationDuration.NewTimer())
+            {
+                _logger.LogInformation("MagicLinkTokenGenerationJob - Started");
+                GenerateTokens();
+                _logger.LogInformation("MagicLinkTokenGenerationJob - Succeeded");
+            }
+        }
+
+        private void GenerateTokens()
+        {
+            var candidates = _crm.GetCandidatesPendingMagicLinkTokenGeneration(BatchSize);
+            _logger.LogInformation($"MagicLinkTokenGenerationJob - Processing ({candidates.Count()})");
+
+            foreach (var candidate in candidates)
+            {
+                _magicLinkTokenService.GenerateToken(candidate);
+                _jobClient.Enqueue<UpsertCandidateJob>(x => x.Run(candidate, null));
+            }
+        }
+    }
+}
