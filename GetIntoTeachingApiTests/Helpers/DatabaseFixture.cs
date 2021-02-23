c7690eab9195347a18a23ae11e0757912874403d
Ross Oliver <ross.oliver36@gmail.com>
2021-02-23 09:11:50 +0000

    Support isolated integration tests against a postgres DB
    We already have the `DatabaseFixture` class to support unit testing against an
    isolated postgres DB (running in Docker). This commit updates the `Environment`
    so that any integration tests using `DatabaseFixture` will also hit an isolated
    test database in docker.

--- GetIntoTeachingApiTests/Helpers/DatabaseFixture.cs
+++ GetIntoTeachingApiTests/Helpers/DatabaseFixture.cs
@@ -1,4 +1,5 @@
-﻿using GetIntoTeachingApi.Database;
+﻿using System;
+using GetIntoTeachingApi.Database;
 using Microsoft.EntityFrameworkCore;
 
 namespace GetIntoTeachingApiTests.Helpers
@@ -13,6 +14,13 @@ namespace GetIntoTeachingApiTests.Helpers
         {
             TemplateDatabaseName = $"gis_test";
             ConnectionString = $"Host=localhost;Database={TemplateDatabaseName};Username=docker;Password=docker";
+
+            // Set environment for integration tests using the database.
+            Environment.SetEnvironmentVariable("DATABASE_INSTANCE_NAME", TemplateDatabaseName);
+            Environment.SetEnvironmentVariable("VCAP_SERVICES",
+                $"{{\"postgres\": [{{\"instance_name\": \"{TemplateDatabaseName}\",\"credentials\": {{\"host\": \"localhost\"," +
+                $"\"name\": \"{TemplateDatabaseName}\",\"username\": \"docker\",\"password\": \"docker\",\"port\": \"5432\"}}}}]," +
+                $"\"redis\": [{{\"credentials\": {{\"host\": \"0.0.0.0\",\"port\": \"6379\",\"password\": \"docker\",\"tls_enabled\": false}}}}]}}");
 
             var builder = new DbContextOptionsBuilder<GetIntoTeachingDbContext>();
             DbConfiguration.ConfigPostgres(ConnectionString, builder);
