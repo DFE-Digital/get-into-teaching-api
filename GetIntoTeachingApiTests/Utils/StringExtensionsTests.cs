aa9ce95f32d96c0f39f7983ef63b5f0a7b80e06b
Ross Oliver <ross.oliver36@gmail.com>
2021-03-04 14:39:57 +0000

    Add StringExtensions for formatting postcodes
    The extension will take a valid postcode and ensure it is formatted correctly
    (upper case and with a space before the inward part of the postcode).

---
+++ GetIntoTeachingApiTests/Utils/StringExtensionsTests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using GetIntoTeachingApi.Utils;
+using Xunit;
+
+namespace GetIntoTeachingApiTests.Utils
+{
+    public class StringExtensionsTests
+    {
+        [Theory]
+        [InlineData("KY11 9YU", "KY11 9YU")]
+        [InlineData("KY119YU", "KY11 9YU")]
+        [InlineData("ca48le", "CA4 8LE")]
+        [InlineData("ca4 8LE", "CA4 8LE")]
+        [InlineData("  ca4   8L E", "CA4 8LE")]
+        [InlineData("invalid", null)]
+        [InlineData(null, null)]
+        public void AsFormattedPostcode_ReturnsCorrectly(string input, string expected)
+        {
+            input.AsFormattedPostcode().Should().Be(expected);
+        }
+    }
+}
