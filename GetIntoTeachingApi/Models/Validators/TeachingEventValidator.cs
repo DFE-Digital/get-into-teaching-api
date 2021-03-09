be27cd51f9fb10e2afa2134ae6a57cdc5f14ed8d
Joseph Kempster <jkempster34@gmail.com>
2021-03-09 12:15:03 +0000

    Add teaching event validator

---
+++ GetIntoTeachingApi/Models/Validators/TeachingEventValidator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using FluentValidation;
+
+namespace GetIntoTeachingApi.Models.Validators
+{
+    public class TeachingEventValidator : AbstractValidator<TeachingEvent>
+    {
+
+        public TeachingEventValidator()
+        {
+            RuleFor(teachingEvent => teachingEvent.Name).NotEmpty();
+            RuleFor(teachingEvent => teachingEvent.Summary).NotEmpty();
+            RuleFor(teachingEvent => teachingEvent.Description).NotEmpty();
+            RuleFor(teachingEvent => teachingEvent.ProviderContactEmail).EmailAddress();
+            RuleFor(teachingEvent => teachingEvent.StartAt).GreaterThan(DateTime.UtcNow);
+            RuleFor(teachingEvent => teachingEvent.EndAt).GreaterThanOrEqualTo(rule => rule.StartAt);
+        }
+    }
+}
