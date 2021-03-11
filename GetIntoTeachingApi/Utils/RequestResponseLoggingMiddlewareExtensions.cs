6c2d11206998ffdb240603d73983879b5e80c6cb
Ross Oliver <ross.oliver36@gmail.com>
2021-03-11 13:40:48 +0000

    Log out request/response payloads
    Add middleware that intercepts request/response payloads, redacts sensitive
    data and them logs the request information.
    
    So as not to upset other middleware in the chain the request/response body
    streams are copied and rewound. `RecyclableMemoryStreamManager` is used for
    performance/to avoid memory leaks.
    
    Query parameters are assumed to be safe/not sensitive.

---
+++ GetIntoTeachingApi/Utils/RequestResponseLoggingMiddlewareExtensions.cs
@@ -0,0 +1,13 @@
+﻿using GetIntoTeachingApi.Middleware;
+using Microsoft.AspNetCore.Builder;
+
+namespace GetIntoTeachingApi.Utils
+{
+    public static class RequestResponseLoggingMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseRequestResponseLogging(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<RequestResponseLoggingMiddleware>();
+        }
+    }
+}
