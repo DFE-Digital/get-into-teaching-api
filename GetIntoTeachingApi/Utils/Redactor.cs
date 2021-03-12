e54242c3b3efd22098a1fcf2b1b127d29bead5db
Ross Oliver <ross.oliver36@gmail.com>
2021-03-12 10:25:40 +0000

    Add utilty class to redact sensitive data from JSON
    We want to be able to log out the request/response bodies and Hangfire job
    paylaods to aid in debugging when something goes wrong. While doing this we
    need to ensure no personally identifiable information is sent to our logstash.
    
    Add a `Redactor` utility class that redacts sensitive data (identified by
    payload keys) from JSON.
    
    Update README to explain why we have to use Newtonsoft.Json for this.

---
+++ GetIntoTeachingApi/Utils/Redactor.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using MoreLinq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace GetIntoTeachingApi.Utils
+{
+    public static class Redactor
+    {
+        private static readonly string _redactedValue = "******";
+        private static readonly IEnumerable<string> _sensitivePropertyNames = new string[]
+        {
+            "password",
+            "email",
+            "fullName",
+            "firstName",
+            "lastName",
+            "telephone",
+            "dateOfBirth",
+            "teacherId",
+            "addressLine1",
+            "addressLine2",
+        };
+
+        public static string RedactJson(string json)
+        {
+            try
+            {
+                var rootToken = JToken.Parse(json);
+                var jsonPath = $"$..['{string.Join("','", _sensitivePropertyNames)}']";
+
+                rootToken
+                    .SelectTokens(jsonPath)
+                    .ForEach(t => t.Replace(new JValue(_redactedValue)));
+
+                return JsonConvert.SerializeObject(rootToken);
+            }
+            catch (JsonReaderException)
+            {
+                return string.Empty;
+            }
+        }
+    }
+}
---
+++ GetIntoTeachingApiTests/Utils/RedactorTests.cs
@@ -0,0 +1,58 @@
+﻿using System.Text.Json;
+using FluentAssertions;
+using GetIntoTeachingApi.Utils;
+using Xunit;
+
+namespace GetIntoTeachingApiTests.Utils
+{
+    public class RedactorTests
+    {
+        [Fact]
+        public void RedactJson_WithValidJson_RedactsSensitiveData()
+        {
+            string json = JsonSerializer.Serialize(
+                new
+                {
+                    password = "abc123",
+                    firstName = "Ross",
+                    nested = new
+                    {
+                        addressLine1 = "6 main street",
+                        password = "abc123",
+                    },
+                    email = new string[] { "first", "second" },
+                    addressPostcode = "TE7 1NG",
+                }
+            );
+
+            string redactedJson = JsonSerializer.Serialize(
+                new
+                {
+                    password = "******",
+                    firstName = "******",
+                    nested = new
+                    {
+                        addressLine1 = "******",
+                        password = "******",
+                    },
+                    email = "******",
+                    addressPostcode = "TE7 1NG",
+                }
+            );
+
+            var result = Redactor.RedactJson(json);
+
+            result.Should().Be(redactedJson);
+        }
+
+        [Fact]
+        public void RedactJson_WithInvalidJson_ReturnsEmpty()
+        {
+            var invalidJson = "my password is 123456";
+
+            var result = Redactor.RedactJson(invalidJson);
+
+            result.Should().Be(string.Empty);
+        }
+    }
+}
