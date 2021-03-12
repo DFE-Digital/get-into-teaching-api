7f33df7ed6c41c3d29cd51c8e4b3f35411c38191
Steven Fawcett <steve.fawcett@capgemini.com>
2021-03-12 10:08:39 +0000

    Common Delivery Method Used

--- terraform/paas/services.tf
+++ terraform/paas/services.tf
@@ -6,13 +6,6 @@ data cloudfoundry_service redis {
   name = "redis"
 }
 
-data cloudfoundry_service mysql {
-  name = "mysql"
-}
-
-data cloudfoundry_service influxdb {
-  name = "influxdb"
-}
 
 resource cloudfoundry_service_instance postgres_common {
   name         = var.paas_database_common_name
@@ -33,18 +26,5 @@ resource cloudfoundry_service_instance redis {
   space        = data.cloudfoundry_space.space.id
   service_plan = data.cloudfoundry_service.redis.service_plans[var.redis_1_plan]
   json_params  = "{\"maxmemory_policy\": \"allkeys-lfu\" }"
-}
-
-resource cloudfoundry_service_instance influxdb {
-  count        = var.influx
-  name         = "influxdb-${local.monitoring_org_name}"
-  space        = data.cloudfoundry_space.monitor.id
-  service_plan = data.cloudfoundry_service.influxdb.service_plans[var.influxdb_1_plan]
-}
-
-resource cloudfoundry_service_key influxdb-key {
-  count            = var.influx
-  name             = "influxdb-prometheus-key"
-  service_instance = cloudfoundry_service_instance.influxdb[0].id
 }
 
