91a05e63cf4bd175976e147b23a378a42b18478d
Ross Oliver <ross.oliver36@gmail.com>
2021-03-17 14:21:43 +0000

    Add AppSettings model
    We want to be able to flag the CRM as unavailable when it has been
    intentionally taken offline by the CRM team (for an update, for example).
    
    Storing this in Postgres felt like overkill, so there is now a simple
    `AppSettings` model that is backed by Redis. It has a single writable attribute
    to set how long we want to pause the CRM integration for (this is a `DateTime`
    so that we can auto-resume the integration and avoid potential user-error).
    
    The idea is that the CRM will call an endpoint in the API and flag itself as
    being unavailable before an update is carried out. We can then prevent any
    calls to the CRM, which would otherwise result in errors.

---
+++ GetIntoTeachingApi/Models/AppSettings.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+using GetIntoTeachingApi.Services;
+
+namespace GetIntoTeachingApi.Models
+{
+    public class AppSettings : IAppSettings
+    {
+        private const string DateFormat = "O";
+        private const string CrmOfflineUntilKey = "app_settings.crm_offline_until";
+        private readonly IRedisService _redis;
+
+        public DateTime? CrmIntegrationPausedUntil
+        {
+            get
+            {
+                var dateStr = _redis.Database.StringGet(CrmOfflineUntilKey);
+
+                if (dateStr.IsNullOrEmpty)
+                {
+                    return null;
+                }
+
+                return DateTime.ParseExact(
+                    dateStr,
+                    DateFormat,
+                    CultureInfo.InvariantCulture,
+                    DateTimeStyles.RoundtripKind);
+            }
+
+            set
+            {
+                var dateStr = value?.ToString(DateFormat);
+
+                _redis.Database.StringSet(CrmOfflineUntilKey, dateStr);
+            }
+        }
+
+        public bool IsCrmIntegrationPaused => CrmIntegrationPausedUntil > DateTime.UtcNow;
+
+        public AppSettings(IRedisService redis)
+        {
+            _redis = redis;
+        }
+    }
+}
---
+++ GetIntoTeachingApiTests/Models/AppSettingsTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using FluentAssertions;
+using GetIntoTeachingApi.Models;
+using GetIntoTeachingApi.Services;
+using Moq;
+using StackExchange.Redis;
+using Xunit;
+
+namespace GetIntoTeachingApiTests.Models
+{
+    public class AppSettingsTests
+    {
+        private readonly AppSettings _settings;
+        private readonly Mock<IDatabase> _database;
+
+        public AppSettingsTests()
+        {
+            _database = new Mock<IDatabase>();
+            var redis = new Mock<IRedisService>();
+            redis.Setup(m => m.Database).Returns(_database.Object);
+
+            _settings = new AppSettings(redis.Object);
+        }
+
+        [Fact]
+        public void CrmIntegrationPausedUntil_SetAndGetWithDate_WorkCorrectly()
+        {
+            var date = DateTime.UtcNow;
+            var dateStr = date.ToString("O");
+            _database.Setup(m => m.StringSet("app_settings.crm_offline_until", dateStr, null, When.Always, CommandFlags.None));
+            _database.Setup(m => m.StringGet("app_settings.crm_offline_until", CommandFlags.None)).Returns(dateStr);
+
+            _settings.CrmIntegrationPausedUntil = date;
+            _settings.CrmIntegrationPausedUntil.Should().Be(date);
+        }
+
+        [Fact]
+        public void CrmIntegrationPausedUntil_SetAndGetWithNull_WorkCorrectly()
+        {
+            _database.Setup(m => m.StringSet("app_settings.crm_offline_until", null as string, null, When.Always, CommandFlags.None));
+            _database.Setup(m => m.StringGet("app_settings.crm_offline_until", CommandFlags.None)).Returns(null as string);
+
+            _settings.CrmIntegrationPausedUntil = null;
+            _settings.CrmIntegrationPausedUntil.Should().BeNull();
+        }
+
+        [Fact]
+        public void IsCrmIntegrationPaused_WhenCrmOfflineUntilIsNull_ReturnsFalse()
+        {
+            _database.Setup(m => m.StringGet("app_settings.crm_offline_until", CommandFlags.None)).Returns<string>(null);
+
+            _settings.IsCrmIntegrationPaused.Should().BeFalse();
+        }
+
+        [Fact]
+        public void IsCrmIntegrationPaused_WhenCrmOfflineUntilIsAFutureDate_ReturnsTrue()
+        {
+            var dateStr = DateTime.UtcNow.AddDays(1).ToString("O");
+            _database.Setup(m => m.StringGet("app_settings.crm_offline_until", CommandFlags.None)).Returns(dateStr);
+
+            _settings.IsCrmIntegrationPaused.Should().BeTrue();
+        }
+
+        [Fact]
+        public void IsCrmIntegrationPaused_WhenCrmOfflineUntilIsAPastDate_ReturnsFalse()
+        {
+            var dateStr = DateTime.UtcNow.AddDays(-1).ToString("O");
+            _database.Setup(m => m.StringGet("app_settings.crm_offline_until", CommandFlags.None)).Returns(dateStr);
+
+            _settings.IsCrmIntegrationPaused.Should().BeFalse();
+        }
+    }
+}
