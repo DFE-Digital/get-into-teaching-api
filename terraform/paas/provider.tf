e4e8b771b9e90432ea863c122c51b51d77a05c20
Steven Fawcett <steve.fawcett@capgemini.com>
2021-03-26 17:32:47 +0000

    Upgrade Terraform
    The version of terraform is now version 14.9 it is time to switch
    to this version, as we also want to make changes in underlying modules

--- terraform/paas/provider.tf
+++ terraform/paas/provider.tf
@@ -14,7 +14,6 @@ locals {
 }
 
 provider "azurerm" {
-  version                    = ">= 2.0"
   skip_provider_registration = true
   features {}
   subscription_id = local.azure_credentials.subscriptionId
@@ -24,7 +23,7 @@ provider "azurerm" {
 }
 
 terraform {
-  required_version = ">= 0.13.4"
+  required_version = "~> 0.14"
 
   required_providers {
     cloudfoundry = {
