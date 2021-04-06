8d417fb481c08b51c2f768ce1676476af61bc01a
Ross Oliver <ross.oliver36@gmail.com>
2021-04-06 11:04:27 +0100

    Fix TeachingEvent not linking to Buildings
    The teaching events were not being linked to the correct building as the
    `TeachingEvent.BuildingId` field was not being mapped from the CRM entity (this
    is then used to establish the relationship before persisting the event).
    
    Update to map the `BuildingId` so it gets populated correctly and the building
    gets linked.

--- GetIntoTeachingApiTests/Models/TeachingEventTests.cs
+++ GetIntoTeachingApiTests/Models/TeachingEventTests.cs
@@ -36,6 +36,8 @@ namespace GetIntoTeachingApiTests.Models
             type.GetProperty("StartAt").Should().BeDecoratedWith<EntityFieldAttribute>(a => a.Name == "msevtmgt_eventstartdate");
             type.GetProperty("EndAt").Should().BeDecoratedWith<EntityFieldAttribute>(a => a.Name == "msevtmgt_eventenddate");
             type.GetProperty("ProvidersList").Should().BeDecoratedWith<EntityFieldAttribute>(a => a.Name == "dfe_providerslist");
+            type.GetProperty("BuildingId").Should().BeDecoratedWith<EntityFieldAttribute>(
+                a => a.Name == "msevtmgt_building" && a.Type == typeof(EntityReference));
 
             type.GetProperty("Building").Should().BeDecoratedWith<EntityRelationshipAttribute>(
                 a => a.Name == "msevtmgt_event_building" && a.Type == typeof(TeachingEventBuilding));
