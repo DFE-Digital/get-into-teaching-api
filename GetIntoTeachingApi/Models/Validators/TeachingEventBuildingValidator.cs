aa584bc618756a6dd30bbbf3f6aa9d8fbc717480
Joseph Kempster <jkempster34@gmail.com>
2021-04-21 10:04:32 +0100

    Add Teaching Event Building validator

---
+++ GetIntoTeachingApi/Models/Validators/TeachingEventBuildingValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using GetIntoTeachingApi.Validators;
+
+namespace GetIntoTeachingApi.Models.Validators
+{
+    public class TeachingEventBuildingValidator : AbstractValidator<TeachingEventBuilding>
+    {
+        public TeachingEventBuildingValidator()
+        {
+            RuleFor(building => building.Venue).NotEmpty();
+            RuleFor(building => building.AddressPostcode).SetValidator(new PostcodeValidator());
+        }
+    }
+}
