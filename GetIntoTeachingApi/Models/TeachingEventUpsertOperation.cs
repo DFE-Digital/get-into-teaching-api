6a9a5b191facd32f66c8d6073c74296ebb0ca354
Ross Oliver <ross.oliver36@gmail.com>
2021-04-22 10:51:46 +0100

    Only validate ReadableId on upsert action
    Validating the `ReadableId` during the sync is expensive (as we have to query
    the CRM for each event to ensure uniqueness). Instead, we can assume that the
    events coming back from the CRM have a unique `ReadableId` and we only need to
    perform the validation when we are adding or updating an event via the app/API.

---
+++ GetIntoTeachingApi/Models/TeachingEventUpsertOperation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GetIntoTeachingApi.Models
+{
+    public class TeachingEventUpsertOperation
+    {
+        public Guid? Id { get; set; }
+        public string ReadableId { get; set; }
+
+        public TeachingEventUpsertOperation(TeachingEvent teachingEvent)
+        {
+            Id = teachingEvent.Id;
+            ReadableId = teachingEvent.ReadableId;
+        }
+
+        public TeachingEventUpsertOperation()
+        {
+        }
+    }
+}
