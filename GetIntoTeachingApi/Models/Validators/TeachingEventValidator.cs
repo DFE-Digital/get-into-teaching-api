0bea2bf52c5e6d5053958fdf655e093608076637
Ross Oliver <ross.oliver36@gmail.com>
2021-04-22 10:08:42 +0100

    Remove unique readable id validation
    We're seeing errors in the rolling environment where we think the validator
    gets stuck in a loop and results in a stack overflow, causing the instance to
    run out of memory.
    
    Reverting this validation check for now until we can debug it further offline.

--- GetIntoTeachingApi/Models/Validators/TeachingEventValidator.cs
+++ GetIntoTeachingApi/Models/Validators/TeachingEventValidator.cs
@@ -1,45 +1,16 @@
-﻿using System;
-using FluentValidation;
+﻿using FluentValidation;
 using FluentValidation.Validators;
-using GetIntoTeachingApi.Services;
 
 namespace GetIntoTeachingApi.Models.Validators
 {
     public class TeachingEventValidator : AbstractValidator<TeachingEvent>
     {
-        private readonly ICrmService _crm;
-        private readonly IStore _store;
-
-        public TeachingEventValidator(ICrmService crm, IStore store)
+        public TeachingEventValidator()
         {
-            _crm = crm;
-            _store = store;
-
-            RuleFor(teachingEvent => teachingEvent.ReadableId)
-                .NotEmpty()
-                .Must(BeUnique)
-                .When(teachingEvent => ReadableIdHasChanged(teachingEvent))
-                .WithMessage("Must be unique");
+            RuleFor(teachingEvent => teachingEvent.ReadableId).NotEmpty();
             RuleFor(teachingEvent => teachingEvent.Name).NotEmpty();
             RuleFor(teachingEvent => teachingEvent.ProviderContactEmail).EmailAddress(EmailValidationMode.AspNetCoreCompatible).MaximumLength(100);
             RuleFor(teachingEvent => teachingEvent.EndAt).GreaterThanOrEqualTo(rule => rule.StartAt);
-        }
-
-        private bool ReadableIdHasChanged(TeachingEvent teachingEvent)
-        {
-            // Always changes on creation.
-            if (teachingEvent.Id == null)
-            {
-                return true;
-            }
-
-            // Best effort check - Checking the CRM is much slower
-            return !_store.TeachingEventExistsWithReadableId(teachingEvent.Id.Value, teachingEvent.ReadableId);
-        }
-
-        private bool BeUnique(string readableId)
-        {
-            return _crm.GetTeachingEvent(readableId) == null;
         }
     }
 }
