4773b40d6e2a29b557179c726e54cec4eb7241a0
Ross Oliver <ross.oliver36@gmail.com>
2021-04-28 14:01:14 +0100

    Support compact logging of verbose requests/responses
    Currently we enable request/response logging for all API endpoints, however
    some of these endpoints return quite a lot of not necessarily all that useful
    information when it comes to debugging issues. Its mainly the POST/PUT
    request/response payloads that we want to log out (but not exclusively - there
    are some GET request payloads like the healthcheck that are useful).
    
    Add support for configuring the `RequestResponseLoggingMiddleware` with a set
    of `CompactLoggingPattern`s - any request/response that matches a comapct regex
    pattern will not have its payload included in the log.

---
+++ GetIntoTeachingApi/Middleware/RequestResponseLoggingConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+
+namespace GetIntoTeachingApi.Middleware
+{
+    public class RequestResponseLoggingConfiguration : IRequestResponseLoggingConfiguration
+    {
+        public Regex[] CompactLoggingPatterns
+        {
+            get
+            {
+                var options = RegexOptions.Compiled | RegexOptions.IgnoreCase;
+
+                return new Regex[]
+                {
+                    new Regex(@"^GET /api/callback_booking_quotas", options),
+                    new Regex(@"^GET /api/lookup_items", options),
+                    new Regex(@"^GET /api/pick_list_items", options),
+                    new Regex(@"^GET /api/privacy_policies", options),
+                    new Regex(@"^GET /api/teaching_event_buildings", options),
+                    new Regex(@"^GET /api/teaching_events", options),
+                };
+            }
+        }
+    }
+}
