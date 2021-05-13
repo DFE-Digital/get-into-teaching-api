d35c27e360dcdb1a5d58cbdd594af35d9f44df7f
Ross Oliver <ross.oliver36@gmail.com>
2021-05-13 12:45:45 +0100

    Support matchback Schools Experience requests
    Add a new `SchoolExperienceSignUp` and associated validator to model the main
    transaction payload for signing up to the Schools Experience service.
    
    Update rate limiting to ensure the sign up endpoint can only receive at most
    250 requests/min.

---
+++ GetIntoTeachingApi/Controllers/SchoolsExperience/CandidatesController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using GetIntoTeachingApi.Jobs;
+using GetIntoTeachingApi.Models;
+using GetIntoTeachingApi.Services;
+using GetIntoTeachingApi.Utils;
+using Hangfire;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace GetIntoTeachingApi.Controllers.SchoolsExperience
+{
+    [Route("api/schools_experience/candidates")]
+    [ApiController]
+    [Authorize(Roles = "Admin,SchoolsExperience")]
+    public class CandidatesController : ControllerBase
+    {
+        private readonly ICandidateAccessTokenService _tokenService;
+        private readonly ICrmService _crm;
+        private readonly IBackgroundJobClient _jobClient;
+        private readonly IDateTimeProvider _dateTime;
+
+        public CandidatesController(
+            ICandidateAccessTokenService tokenService,
+            ICrmService crm,
+            IBackgroundJobClient jobClient,
+            IDateTimeProvider dateTime)
+        {
+            _crm = crm;
+            _tokenService = tokenService;
+            _jobClient = jobClient;
+            _dateTime = dateTime;
+        }
+
+        [HttpPost]
+        [SwaggerOperation(
+            Summary = "Sign up a candidate for the Schools Experience service.",
+            Description = "Validation errors may be present on the `SchoolsExperienceSignUp` object as " +
+                          "well as the hidden `Candidate` model that is mapped to; property names are " +
+                          "consistent, so you should check for inclusion of the field in the key " +
+                          "when linking an error message back to a property on the request model. For " +
+                          "example, an error on `DegreeSubject` can return under the keys " +
+                          "`Candidate.Qualifications[0].DegreeSubject` and `DegreeSubject`.",
+            OperationId = "SignUpSchoolsExperienceCandidate",
+            Tags = new[] { "Schools Experience" })]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(typeof(IDictionary<string, string>), StatusCodes.Status400BadRequest)]
+        public IActionResult SignUp(
+            [FromBody, SwaggerRequestBody("Candidate to sign up for the Schools Experience service.", Required = true)] SchoolsExperienceSignUp request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            // This is the only way we can mock/freeze the current date/time
+            // in contract tests (there's no other way to inject it into this class).
+            request.DateTimeProvider = _dateTime;
+            string json = request.Candidate.SerializeChangeTracked();
+            _jobClient.Enqueue<UpsertCandidateJob>((x) => x.Run(json, null));
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        [Route("exchange_access_token/{accessToken}")]
+        [SwaggerOperation(
+            Summary = "Retrieves a pre-populated SchoolsExperienceSignUp for the candidate.",
+            Description = @"
+                Retrieves a pre-populated SchoolsExperienceSignUp for the candidate. The `accessToken` is obtained from a 
+                `POST /candidates/access_tokens` request (you must also ensure the `ExistingCandidateRequest` payload you 
+                exchanged for your token matches the request payload here).",
+            OperationId = "ExchangeAccessTokenForSchoolsExperienceSignUp",
+            Tags = new[] { "Schools Experience" })]
+        [ProducesResponseType(typeof(SchoolsExperienceSignUp), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult ExchangeAccessToken(
+            [FromRoute, SwaggerParameter("Access token (PIN code).", Required = true)] string accessToken,
+            [FromBody, SwaggerRequestBody("Candidate access token request (must match an existing candidate).", Required = true)] ExistingCandidateRequest request)
+        {
+            var candidate = _crm.MatchCandidate(request);
+
+            if (candidate == null || !_tokenService.IsValid(accessToken, request, (Guid)candidate.Id))
+            {
+                return Unauthorized();
+            }
+
+            return Ok(new SchoolsExperienceSignUp(candidate));
+        }
+    }
+}
---
+++ GetIntoTeachingApiTests/Models/SchoolsExperienceSignUpTests.cs
@@ -0,0 +1,145 @@
+﻿using FluentAssertions;
+using GetIntoTeachingApi.Models;
+using System;
+using Xunit;
+
+namespace GetIntoTeachingApiTests.Models
+{
+    public class SchoolsExperienceSignUpTests
+    {
+        [Fact]
+        public void Constructor_WithCandidate_MapsCorrectly()
+        {
+            var candidate = new Candidate()
+            {
+                Id = Guid.NewGuid(),
+                PreferredTeachingSubjectId = Guid.NewGuid(),
+                SecondaryPreferredTeachingSubjectId = Guid.NewGuid(),
+                CountryId = Guid.NewGuid(),
+                Email = "email@address.com",
+                SecondaryEmail = "email2@address.com",
+                FirstName = "John",
+                LastName = "Doe",
+                DateOfBirth = DateTime.UtcNow,
+                AddressLine1 = "Address 1",
+                AddressLine2 = "Address 2",
+                AddressLine3 = "Address 3",
+                AddressCity = "City",
+                AddressStateOrProvince = "County",
+                AddressPostcode = "KY11 9YU",
+                AddressTelephone = "123456789",
+                Telephone = "234567890",
+                SecondaryTelephone = "345678901",
+                MobileTelephone = "456789012",
+                HasDbsCertificate = true,
+                DbsCertificateIssuedAt = DateTime.UtcNow,
+            };
+
+            var response = new SchoolsExperienceSignUp(candidate);
+
+            response.CandidateId.Should().Be(candidate.Id);
+            response.PreferredTeachingSubjectId.Should().Be(candidate.PreferredTeachingSubjectId);
+            response.SecondaryPreferredTeachingSubjectId.Should().Be(candidate.SecondaryPreferredTeachingSubjectId);
+            response.CountryId.Should().Be(candidate.CountryId);
+            response.Email.Should().Be(candidate.Email);
+            response.SecondaryEmail.Should().Be(candidate.SecondaryEmail);
+            response.FirstName.Should().Be(candidate.FirstName);
+            response.LastName.Should().Be(candidate.LastName);
+            response.AddressTelephone.Should().Be(candidate.AddressTelephone);
+            response.AddressLine1.Should().Be(candidate.AddressLine1);
+            response.AddressLine2.Should().Be(candidate.AddressLine2);
+            response.AddressLine3.Should().Be(candidate.AddressLine3);
+            response.AddressCity.Should().Be(candidate.AddressCity);
+            response.AddressStateOrProvince.Should().Be(candidate.AddressStateOrProvince);
+            response.AddressPostcode.Should().Be(candidate.AddressPostcode);
+            response.AddressTelephone.Should().Be(candidate.AddressTelephone);
+            response.Telephone.Should().Be(candidate.Telephone);
+            response.SecondaryTelephone.Should().Be(candidate.SecondaryTelephone);
+            response.MobileTelephone.Should().Be(candidate.MobileTelephone);
+            response.HasDbsCertificate.Should().Be(candidate.HasDbsCertificate);
+            response.DbsCertificateIssuedAt.Should().Be(candidate.DbsCertificateIssuedAt);
+        }
+
+        [Fact]
+        public void Candidate_MapsCorrectly()
+        {
+            var request = new SchoolsExperienceSignUp()
+            {
+                CandidateId = Guid.NewGuid(),
+                PreferredTeachingSubjectId = Guid.NewGuid(),
+                SecondaryPreferredTeachingSubjectId = Guid.NewGuid(),
+                CountryId = Guid.NewGuid(),
+                AcceptedPolicyId = Guid.NewGuid(),
+                Email = "email@address.com",
+                SecondaryEmail = "email2@address.com",
+                FirstName = "John",
+                LastName = "Doe",
+                DateOfBirth = DateTime.UtcNow,
+                AddressLine1 = "Address 1",
+                AddressLine2 = "Address 2",
+                AddressLine3 = "Address 3",
+                AddressCity = "City",
+                AddressStateOrProvince = "County",
+                AddressPostcode = "KY11 9YU",
+                AddressTelephone = "123456789",
+                Telephone = "234567890",
+                SecondaryTelephone = "345678901",
+                MobileTelephone = "456789012",
+                HasDbsCertificate = true,
+                DbsCertificateIssuedAt = DateTime.UtcNow,
+            };
+
+            var candidate = request.Candidate;
+
+            candidate.Id.Should().Equals(request.CandidateId);
+            candidate.PreferredTeachingSubjectId.Should().Equals(request.PreferredTeachingSubjectId);
+            candidate.SecondaryPreferredTeachingSubjectId.Should().Equals(request.SecondaryPreferredTeachingSubjectId);
+            candidate.CountryId.Should().Equals(request.CountryId);
+            candidate.Email.Should().Be(request.Email);
+            candidate.SecondaryEmail.Should().Be(request.SecondaryEmail);
+            candidate.FirstName.Should().Be(request.FirstName);
+            candidate.LastName.Should().Be(request.LastName);
+            candidate.DateOfBirth.Should().Be(request.DateOfBirth);
+            candidate.AddressLine1.Should().Be(request.AddressLine1);
+            candidate.AddressLine2.Should().Be(request.AddressLine2);
+            candidate.AddressLine3.Should().Be(request.AddressLine3);
+            candidate.AddressCity.Should().Be(request.AddressCity);
+            candidate.AddressStateOrProvince.Should().Be(request.AddressStateOrProvince);
+            candidate.AddressPostcode.Should().Be(request.AddressPostcode);
+            candidate.AddressTelephone.Should().Be(request.AddressTelephone);
+            candidate.Telephone.Should().Be(request.Telephone);
+            candidate.SecondaryTelephone.Should().Be(request.SecondaryTelephone);
+            candidate.MobileTelephone.Should().Be(request.MobileTelephone);
+            candidate.HasDbsCertificate.Should().Be(request.HasDbsCertificate);
+            candidate.DbsCertificateIssuedAt.Should().Be(request.DbsCertificateIssuedAt);
+
+            candidate.PrivacyPolicy.AcceptedPolicyId.Should().Be((Guid)request.AcceptedPolicyId);
+            candidate.PrivacyPolicy.AcceptedAt.Should().BeCloseTo(DateTime.UtcNow);
+        }
+
+        [Fact]
+        public void Candidate_ChannelIdWhenCandidateIdIsNull_IsSchoolsExperience()
+        {
+            var request = new SchoolsExperienceSignUp() { CandidateId = null };
+
+            request.Candidate.ChannelId.Should().Be((int)Candidate.Channel.SchoolsExperience);
+        }
+
+        [Fact]
+        public void Candidate_ChannelIdWhenCandidateIdIsNotNull_IsNotChanged()
+        {
+            var request = new SchoolsExperienceSignUp() { CandidateId = Guid.NewGuid() };
+
+            request.Candidate.ChannelId.Should().BeNull();
+            request.Candidate.ChangedPropertyNames.Should().NotContain("ChannelId");
+        }
+
+        [Fact]
+        public void Candidate_AddressPostcode_IsFormatted()
+        {
+            var request = new SchoolsExperienceSignUp() { AddressPostcode = "ky119yu" };
+
+            request.Candidate.AddressPostcode.Should().Be("KY11 9YU");
+        }
+    }
+}
