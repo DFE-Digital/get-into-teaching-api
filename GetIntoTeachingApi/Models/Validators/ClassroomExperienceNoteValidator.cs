258a56f8742a6f6705f7e6f5a1572891fd252870
Ross Oliver <ross.oliver36@gmail.com>
2021-05-21 13:02:17 +0100

    Validate against array of strings instead of regex
    The swagger-codegen is creating an invalid Regex in the client, so swapping
    this validator out to check an array of explicit strings instead.

--- GetIntoTeachingApi/Models/Validators/ClassroomExperienceNoteValidator.cs
+++ GetIntoTeachingApi/Models/Validators/ClassroomExperienceNoteValidator.cs
@@ -1,15 +1,23 @@
-﻿using FluentValidation;
+﻿using System.Linq;
+using FluentValidation;
 
 namespace GetIntoTeachingApi.Models.Validators
 {
     public class ClassroomExperienceNoteValidator : AbstractValidator<ClassroomExperienceNote>
     {
-        private static readonly string ActionRegex =
-            "\\A(REQUEST|ACCEPTED|ATTENDED|DID NOT ATTEND|CANCELLED BY (SCHOOL|CANDIDATE))\\z";
+        private static readonly string[] _validActions = new string[]
+        {
+            "REQUEST",
+            "ACCEPTED",
+            "ATTENDED",
+            "DID NOT ATTEND",
+            "CANCELLED BY SCHOOL",
+            "CANCELLED BY CANDIDATE",
+        };
 
         public ClassroomExperienceNoteValidator()
         {
-            RuleFor(request => request.Action).NotEmpty().Matches(ActionRegex);
+            RuleFor(request => request.Action).NotEmpty().Must(a => _validActions.Contains(a));
             RuleFor(request => request.SchoolUrn).NotEmpty().MaximumLength(6);
             RuleFor(request => request.SchoolName).NotEmpty();
             RuleFor(request => request.RecordedAt).NotNull();
