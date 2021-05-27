43e9fec8364540b02775e4234fb2df3342fc5019
Ross Oliver <ross.oliver36@gmail.com>
2021-05-27 10:36:07 +0100

    Add support for comma-separated query string values
    By default ASP.NET Core expects `FromQuery` parameters of a collectiont type to
    be expressed in the format:
    
    ```
    GET /items?key=value1&key=value2
    ```
    
    Swagger UI and SwaggerCodegen, however, create code that results in the query
    parameters in the format:
    
    ```
    GET /items?key=value1,value2
    ```
    
    As it doesn't appear possible to customise Swagger UI/SwaggerCodegen in this
    respect, this commit instead updates ASP.NET Core with a `CommaSeparated`
    attribute that can be applied to controller parameters in order to accept the
    comma-separated value format.
    
    It does this via a mixture of custom attributs, value providers and a
    convention - all of which are closed to mocking and difficult to test, so I've
    covered the bits of code that I can.

---
+++ GetIntoTeachingApi/Utils/CommaSeparatedQueryStringValueProvider.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.Extensions.Primitives;
+
+namespace GetIntoTeachingApi.Utils
+{
+    public class CommaSeparatedQueryStringValueProvider : QueryStringValueProvider
+    {
+        private readonly string _key;
+        private readonly string _separator;
+
+        public CommaSeparatedQueryStringValueProvider(IQueryCollection values, string separator)
+            : this(null, values, separator)
+        {
+            _separator = separator;
+        }
+
+        public CommaSeparatedQueryStringValueProvider(string key, IQueryCollection values, string separator)
+            : base(BindingSource.Query, values, CultureInfo.InvariantCulture)
+        {
+            _key = key;
+            _separator = separator;
+        }
+
+        public override ValueProviderResult GetValue(string key)
+        {
+            var result = base.GetValue(key);
+
+            if (_key != key)
+            {
+                return result;
+            }
+
+            if (result != ValueProviderResult.None && result.Values.Any(x => x.Contains(_separator)))
+            {
+                var splitValues = new StringValues(result.Values
+                    .SelectMany(x => x.Split(new[] { _separator }, StringSplitOptions.None)).ToArray());
+                return new ValueProviderResult(splitValues, result.Culture);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ GetIntoTeachingApiTests/Utils/CommaSeparatedQueryStringValueProviderFactoryTests.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using GetIntoTeachingApi.Utils;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Xunit;
+
+namespace GetIntoTeachingApiTests.Utils
+{
+    public class CommaSeparatedQueryStringValueProviderFactoryTests
+    {
+        [Fact]
+        public void CreateValueProviderAsync_InsertsCommaSepratedQueryStrinValueProvider()
+        {
+            var factory = new CommaSeparatedQueryStringValueProviderFactory("key", ",");
+            var actionContext = new ActionContext();
+            var httpContext = new DefaultHttpContext();
+            httpContext.Request.QueryString = new QueryString("?key=value");
+            actionContext.HttpContext = httpContext;
+            var context = new ValueProviderFactoryContext(actionContext);
+
+            factory.CreateValueProviderAsync(context);
+
+            var expectedProvider = new CommaSeparatedQueryStringValueProvider("key",
+                context.ActionContext.HttpContext.Request.Query, ",");
+            context.ValueProviders[0].Should().BeEquivalentTo(expectedProvider);
+        }
+    }
+}
