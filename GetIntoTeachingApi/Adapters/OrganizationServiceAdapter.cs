7b151208b29631451437719a84eae542a8de1582
Ross Oliver <ross.oliver36@gmail.com>
2021-06-01 13:29:45 +0100

    Update CdsServiceClient
    The CdsServiceClient has had a major version bump (we were a major version
    behind anyway) and the core classes have been renamed to fall inline with the
    Microsoft PowerPlatform Dataversebranding.
    
    Functionality-wise the interfaces remain largely consistent, just a couple of
    minor updates to the configuration of the client and the method to retrieve the
    current user (performed in the healthcheck).
    
    I plan on pushing this to dev and doing some full end-to-end testing with each
    of the clients to ensure everything still works correctly.

--- GetIntoTeachingApi/Adapters/OrganizationServiceAdapter.cs
+++ GetIntoTeachingApi/Adapters/OrganizationServiceAdapter.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using GetIntoTeachingApi.Models;
-using Microsoft.PowerPlatform.Cds.Client;
+using Microsoft.PowerPlatform.Dataverse.Client;
 using Microsoft.Xrm.Sdk;
 using Microsoft.Xrm.Sdk.Client;
 using Microsoft.Xrm.Sdk.Query;
@@ -11,18 +11,18 @@ namespace GetIntoTeachingApi.Adapters
 {
     public class OrganizationServiceAdapter : IOrganizationServiceAdapter
     {
-        private readonly CdsServiceClient _client;
+        private readonly ServiceClient _client;
 
         public OrganizationServiceAdapter(IOrganizationService client)
         {
-            _client = (CdsServiceClient)client;
+            _client = (ServiceClient)client;
         }
 
         public string CheckStatus()
         {
             try
             {
-                _client.GetMyCdsUserId();
+                _client.GetMyUserId();
             }
             catch (Exception e)
             {
@@ -65,7 +65,7 @@ namespace GetIntoTeachingApi.Adapters
             return result;
         }
 
-        public IEnumerable<CdsServiceClient.PickListItem> GetPickListItemsForAttribute(
+        public IEnumerable<ServiceClient.PickListItem> GetPickListItemsForAttribute(
             string entityName,
             string attributeName)
         {
