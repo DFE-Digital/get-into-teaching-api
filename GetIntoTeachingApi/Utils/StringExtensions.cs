a92e5fe40e91ff3e89ed192c56cd9228a7a4739c
Ross Oliver <ross.oliver36@gmail.com>
2021-06-08 13:46:35 +0100

    Strip exit code before serving existing sign ups
    If a candidate already exists in the system its likely that we will have
    transformed their telephone number into a format that is dialiable by TPUK's
    telephone system. This means it will be prefixed with a `00` exit code. As we
    don't want the user to have to put in the exit code manually we expect their
    country dial-in code at the start of the telephone instead. We need to
    therefore strip the `00` exit code from any responses that are subsequently
    used to pre-fill a form.
    
    Reworks the telephone sanitization to pull it into a string extension so that
    we can more easily reuse it elsewhere.

--- GetIntoTeachingApi/Utils/StringExtensions.cs
+++ GetIntoTeachingApi/Utils/StringExtensions.cs
@@ -1,4 +1,5 @@
-﻿using GetIntoTeachingApi.Models;
+﻿using System.Text.RegularExpressions;
+using GetIntoTeachingApi.Models;
 
 namespace GetIntoTeachingApi.Utils
 {
@@ -14,6 +15,33 @@ namespace GetIntoTeachingApi.Utils
             }
 
             return str.ToUpper().Insert(str.Length - Location.InwardPostcodeLength, " ");
+        }
+
+        public static string StripExitCode(this string str)
+        {
+            if (str == null)
+            {
+                return null;
+            }
+
+            return Regex.Replace(str, "^00", string.Empty);
+        }
+
+        public static string AsFormattedTelephone(this string str, bool international)
+        {
+            if (international && str != null)
+            {
+                // Remove non-digit characters.
+                str = Regex.Replace(str, "[^0-9]", string.Empty);
+
+                // Prefix the 00 exit code.
+                str = $"00{str}";
+
+                // Replace UK dial-in code with a 0.
+                str = Regex.Replace(str, "^00440?", "0");
+            }
+
+            return str;
         }
 
         public static string NullIfEmptyOrWhitespace(this string str)
