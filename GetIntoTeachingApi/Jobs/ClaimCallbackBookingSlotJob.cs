459be816169106e6c474f5180c3232ff940d7c9f
Ross Oliver <ross.oliver36@gmail.com>
2021-06-14 09:54:51 +0100

    Add job to claim a callback booking quota slot
    When someone books a callback we need to ensure the callback booking slot they
    select has its available slots decremented. Currently we do this in the
    `UpsertCandidateJob`, but this commit adds a new job to perform this function.

---
+++ GetIntoTeachingApi/Jobs/ClaimCallbackBookingSlotJob.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using GetIntoTeachingApi.Adapters;
+using GetIntoTeachingApi.Models;
+using GetIntoTeachingApi.Services;
+using GetIntoTeachingApi.Utils;
+using Hangfire.Server;
+using Microsoft.Extensions.Logging;
+
+namespace GetIntoTeachingApi.Jobs
+{
+    public class ClaimCallbackBookingSlotJob : BaseJob
+    {
+        private readonly IPerformContextAdapter _contextAdapter;
+        private readonly IMetricService _metrics;
+        private readonly IAppSettings _appSettings;
+        private readonly ICrmService _crm;
+        private readonly ILogger<ClaimCallbackBookingSlotJob> _logger;
+
+        public ClaimCallbackBookingSlotJob(
+            IEnv env,
+            IPerformContextAdapter contextAdapter,
+            ICrmService crm,
+            IMetricService metrics,
+            ILogger<ClaimCallbackBookingSlotJob> logger,
+            IAppSettings appSettings)
+            : base(env)
+        {
+            _contextAdapter = contextAdapter;
+            _metrics = metrics;
+            _logger = logger;
+            _appSettings = appSettings;
+            _crm = crm;
+        }
+
+        public void Run(DateTime scheduledAt, PerformContext context)
+        {
+            if (_appSettings.IsCrmIntegrationPaused)
+            {
+                throw new InvalidOperationException("ClaimCallbackBookingSlotJob - Aborting (CRM integration paused).");
+            }
+
+            _logger.LogInformation($"ClaimCallbackBookingSlotJob - Started ({AttemptInfo(context, _contextAdapter)})");
+            _logger.LogInformation($"ClaimCallbackBookingSlotJob - Payload {scheduledAt}");
+
+            if (IsLastAttempt(context, _contextAdapter))
+            {
+                // Not critical, they'll still get a call but it may be at a different time
+                // so we can just let the job expire.
+                _logger.LogInformation($"ClaimCallbackBookingSlotJob - Deleted");
+            }
+            else
+            {
+                var quota = _crm.GetCallbackBookingQuota(scheduledAt);
+
+                if (quota != null && quota.IsAvailable)
+                {
+                    quota.NumberOfBookings += 1;
+                    _crm.Save(quota);
+                }
+
+                _logger.LogInformation($"ClaimCallbackBookingSlotJob - Succeeded - {scheduledAt}");
+            }
+
+            var duration = (DateTime.UtcNow - _contextAdapter.GetJobCreatedAt(context)).TotalSeconds;
+            _metrics.HangfireJobQueueDuration.WithLabels(new[] { $"ClaimCallbackBookingSlotJob" }).Observe(duration);
+        }
+    }
+}
