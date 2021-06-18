b7aabf5908c1aa1dc73f1ddff4173c39c8ba9194
Ross Oliver <ross.oliver36@gmail.com>
2021-06-18 10:34:45 +0100

    Add validator for request model
    Add a validator for the new GetIntoTeachingCallback request model. Every field
    is required and callbacks can only be scheduled for future dates.

---
+++ GetIntoTeachingApi/Models/Validators/GetIntoTeachingCallbackValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using GetIntoTeachingApi.Services;
+
+namespace GetIntoTeachingApi.Models.Validators
+{
+    public class GetIntoTeachingCallbackValidator : AbstractValidator<GetIntoTeachingCallback>
+    {
+        public GetIntoTeachingCallbackValidator(IStore store, IDateTimeProvider dateTime)
+        {
+            RuleFor(request => request.AcceptedPolicyId).NotNull();
+
+            RuleFor(request => request.Email).NotEmpty();
+            RuleFor(request => request.FirstName).NotEmpty();
+            RuleFor(request => request.LastName).NotEmpty();
+            RuleFor(request => request.AddressTelephone).NotNull();
+            RuleFor(request => request.PhoneCallScheduledAt)
+                .NotNull()
+                .GreaterThan(candidate => dateTime.UtcNow)
+                    .WithMessage("Can only be scheduled for future dates.");
+
+            RuleFor(request => request.Candidate).SetValidator(new CandidateValidator(store, dateTime));
+        }
+    }
+}
