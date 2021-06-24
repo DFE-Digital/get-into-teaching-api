26d677bbf9561fd00234cc1362b53d7607564035
Ross Oliver <ross.oliver36@gmail.com>
2021-06-24 14:05:07 +0100

    Add job to sync a find/apply candidate with the CRM
    Add a job that takes a Find & Apply candidate model and matches it against a
    `contact` in the CRM. If there is a match the `FindApplyId` is set and the
    model is persisted back to the CRM.

---
+++ GetIntoTeachingApi/Jobs/FindApplyCandidateSyncJob.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using GetIntoTeachingApi.Models.FindApply;
+using GetIntoTeachingApi.Services;
+using GetIntoTeachingApi.Utils;
+using Microsoft.Extensions.Logging;
+
+namespace GetIntoTeachingApi.Jobs
+{
+    public class FindApplyCandidateSyncJob : BaseJob
+    {
+        private readonly ILogger<FindApplyCandidateSyncJob> _logger;
+        private readonly ICrmService _crm;
+        private readonly Models.IAppSettings _appSettings;
+
+        public FindApplyCandidateSyncJob(
+            IEnv env,
+            ILogger<FindApplyCandidateSyncJob> logger,
+            ICrmService crm,
+            Models.IAppSettings appSettings)
+            : base(env)
+        {
+            _logger = logger;
+            _crm = crm;
+            _appSettings = appSettings;
+        }
+
+        public void Run(Candidate candidate)
+        {
+            if (_appSettings.IsCrmIntegrationPaused)
+            {
+                throw new InvalidOperationException("FindApplyCandidateSyncJob - Aborting (CRM integration paused).");
+            }
+
+            _logger.LogInformation($"FindApplyCandidateSyncJob - Started - {candidate.Id}");
+            SyncCandidate(candidate);
+            _logger.LogInformation($"FindApplyCandidateSyncJob - Succeeded - {candidate.Id}");
+        }
+
+        public void SyncCandidate(Candidate candidate)
+        {
+            var match = _crm.MatchCandidate(candidate.Attributes.Email);
+
+            if (match != null)
+            {
+                _logger.LogInformation($"FindApplyCandidateSyncJob - Hit - {candidate.Id}");
+                match.FindApplyId = candidate.Id;
+                _crm.Save(match);
+            }
+            else
+            {
+                _logger.LogInformation($"FindApplyCandidateSyncJob - Miss - {candidate.Id}");
+            }
+        }
+    }
+}
