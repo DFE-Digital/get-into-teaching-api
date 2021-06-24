440c548a87e5c0bc02a22a5d0c25bf2c5e2d0c99
Ross Oliver <ross.oliver36@gmail.com>
2021-06-24 14:02:29 +0100

    Model response from find/apply API
    Add the models that map to the response from the Find & Apply candidates API.

---
+++ GetIntoTeachingApi/Models/FindApply/CandidateAttributes.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace GetIntoTeachingApi.Models.FindApply
+{
+    public class CandidateAttributes
+    {
+        [JsonProperty("email_address")]
+        public string Email { get; set; }
+    }
+}
---
+++ GetIntoTeachingApiTests/Models/FindApply/ResponseTests.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using GetIntoTeachingApi.Models.FindApply;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace GetIntoTeachingApiTests.Models.FindApply
+{
+    public class ResponseTests
+    {
+        [Fact]
+        public void JsonAttributes()
+        {
+            var type = typeof(Response<object>);
+
+            type.GetProperty("Data").Should()
+                .BeDecoratedWith<JsonPropertyAttribute>(a => a.PropertyName == "data");
+        }
+    }
+}
