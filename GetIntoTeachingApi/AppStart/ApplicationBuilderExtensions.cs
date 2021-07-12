9e8583271546d74d2f415f0f02ac97af9b54d987
Joseph Kempster <jkempster34@gmail.com>
2021-07-12 15:58:46 +0100

    Tidy up `Startup`
    The Startup class is getting a bit unwieldy. Update to use extension/static classes to help keep things organised.

---
+++ GetIntoTeachingApi/AppStart/ApplicationBuilderExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using GetIntoTeachingApi.Auth;
+using GetIntoTeachingApi.Utils;
+using Hangfire;
+using Hangfire.Dashboard;
+using HangfireBasicAuthenticationFilter;
+using Microsoft.AspNetCore.Builder;
+
+namespace GetIntoTeachingApi.AppStart
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static void ConfigureHangfire(this IApplicationBuilder app, int? workerCount, bool addBasicAuthFilter, IEnv env)
+        {
+            var hangfireOptions = new BackgroundJobServerOptions();
+            if (workerCount.HasValue)
+            {
+                hangfireOptions.WorkerCount = workerCount.Value;
+            }
+
+            app.UseHangfireServer(hangfireOptions);
+
+            var filters = new List<IDashboardAuthorizationFilter> { new HangfireDashboardEnvironmentAuthorizationFilter(env) };
+
+            if (addBasicAuthFilter)
+            {
+                var basicAuthFilter = new HangfireCustomBasicAuthenticationFilter()
+                {
+                    User = env.HangfireUsername,
+                    Pass = env.HangfirePassword,
+                };
+
+                filters.Add(basicAuthFilter);
+            }
+
+            app.UseHangfireDashboard("/hangfire", new DashboardOptions
+            {
+                Authorization = filters,
+            });
+        }
+    }
+}
---
+++ GetIntoTeachingApi/AppStart/HangfireJobs.cs
@@ -0,0 +1,38 @@
+﻿using GetIntoTeachingApi.Jobs;
+using Hangfire;
+
+namespace GetIntoTeachingApi.AppStart
+{
+    public static class HangfireJobs
+    {
+        public static void AddCrmSyncJob()
+        {
+            const string everyFifthMinute = "*/5 * * * *";
+            RecurringJob.AddOrUpdate<CrmSyncJob>(JobConfiguration.CrmSyncJobId, (x) => x.RunAsync(), everyFifthMinute);
+        }
+
+        public static void AddLocationSyncJob()
+        {
+            RecurringJob.AddOrUpdate<LocationSyncJob>(
+                    JobConfiguration.LocationSyncJobId,
+                    (x) => x.RunAsync(LocationSyncJob.FreeMapToolsUrl),
+                    Cron.Weekly());
+        }
+
+        public static void AddMagicLinkTokenGenerationJob()
+        {
+            RecurringJob.AddOrUpdate<MagicLinkTokenGenerationJob>(
+                    JobConfiguration.MagicLinkTokenGenerationJobId,
+                    (x) => x.Run(),
+                    Cron.Hourly());
+        }
+
+        public static void AddFindApplySyncJob()
+        {
+            RecurringJob.AddOrUpdate<FindApplySyncJob>(
+                                    JobConfiguration.FindApplySyncJobId,
+                                    (x) => x.RunAsync(),
+                                    Cron.Hourly());
+        }
+    }
+}
