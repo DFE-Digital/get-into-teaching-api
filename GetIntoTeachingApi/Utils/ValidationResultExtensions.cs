12a5fd7f79d1f11061e1cc55d1649e67db1b16d6
Joseph Kempster <jkempster34@gmail.com>
2021-07-15 18:58:31 +0100

    Map related model validation errors to request model
    Validation errors on related models will appear in the format `RealtedModel.PropertyName`. It will be easier for clients to handle errors if they could instead be mapped to the request model  property.
    
    Intercept the `ValidationResult`, mapping any keys that are duplicated in any related model to the request model. Validation errors now have the key `PropertyName`.

---
+++ GetIntoTeachingApi/Utils/ValidationResultExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using FluentValidation;
+using FluentValidation.Results;
+
+namespace GetIntoTeachingApi.Utils
+{
+    public static class ValidationResultExtensions
+    {
+        public static void MapRelatedModelErrorsToRequestModel(
+            this ValidationResult result, IValidationContext validationContext, string relatedModelName)
+        {
+            var requestModel = validationContext.InstanceToValidate;
+            foreach (var error in result.Errors)
+            {
+                string[] splitPropertyName = error.PropertyName.Split('.');
+                string propertyName = splitPropertyName.Last();
+
+                bool relatedModelProperty = splitPropertyName.First() == relatedModelName;
+                bool requestModelHasProperty = requestModel
+                    .GetType()
+                    .GetProperties()
+                    .Any(property => property.Name == propertyName);
+
+                if (relatedModelProperty && requestModelHasProperty)
+                {
+                    error.PropertyName = propertyName;
+                }
+            }
+        }
+    }
+}
