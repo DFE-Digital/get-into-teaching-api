83d712bfdc3e03bb1c396963396a0cfaaaa3c4a1
Ross Oliver <ross.oliver36@gmail.com>
2021-08-17 15:36:43 +0100

    Map additional attributes on Find/Apply models
    New attributes are exposed on the Apply API; we are mapping these to the
    Find/Apply models so that we can later persist them to the CRM.

---
+++ GetIntoTeachingApi/Models/FindApply/ApplicationForm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace GetIntoTeachingApi.Models.FindApply
+{
+    public class ApplicationForm
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+        [JsonProperty("created_at")]
+        public DateTime CreatedAt { get; set; }
+        [JsonProperty("updated_at")]
+        public DateTime UpdatedAt { get; set; }
+    }
+}
--- GetIntoTeachingApiTests/Models/FindApply/CandidateAttributesTests.cs
+++ GetIntoTeachingApiTests/Models/FindApply/CandidateAttributesTests.cs
@@ -14,6 +14,16 @@ namespace GetIntoTeachingApiTests.Models.FindApply
 
             type.GetProperty("Email").Should()
                 .BeDecoratedWith<JsonPropertyAttribute>(a => a.PropertyName == "email_address");
+            type.GetProperty("CreatedAt").Should()
+                .BeDecoratedWith<JsonPropertyAttribute>(a => a.PropertyName == "created_at");
+            type.GetProperty("UpdatedAt").Should()
+                .BeDecoratedWith<JsonPropertyAttribute>(a => a.PropertyName == "updated_at");
+            type.GetProperty("ApplicationStatus").Should()
+                .BeDecoratedWith<JsonPropertyAttribute>(a => a.PropertyName == "application_status");
+            type.GetProperty("ApplicationPhase").Should()
+                .BeDecoratedWith<JsonPropertyAttribute>(a => a.PropertyName == "application_phase");
+            type.GetProperty("ApplicationForms").Should()
+                .BeDecoratedWith<JsonPropertyAttribute>(a => a.PropertyName == "application_forms");
         }
     }
 }
