cea821a5361c18cdf1f07b15daca067ff2e322c5
Ross Oliver <ross.oliver36@gmail.com>
2021-08-17 15:36:43 +0100

    Map additional attributes on Find/Apply models
    New attributes are exposed on the Apply API; we are mapping these to the
    Find/Apply models so that we can later persist them to the CRM.

--- GetIntoTeachingApi/Models/FindApply/CandidateAttributes.cs
+++ GetIntoTeachingApi/Models/FindApply/CandidateAttributes.cs
@@ -1,4 +1,6 @@
-﻿using Newtonsoft.Json;
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace GetIntoTeachingApi.Models.FindApply
 {
@@ -6,5 +8,15 @@ namespace GetIntoTeachingApi.Models.FindApply
     {
         [JsonProperty("email_address")]
         public string Email { get; set; }
+        [JsonProperty("created_at")]
+        public DateTime CreatedAt { get; set; }
+        [JsonProperty("updated_at")]
+        public DateTime? UpdatedAt { get; set; }
+        [JsonProperty("application_status")]
+        public string ApplicationStatus { get; set; }
+        [JsonProperty("application_phase")]
+        public string ApplicationPhase { get; set; }
+        [JsonProperty("application_forms")]
+        public IEnumerable<ApplicationForm> ApplicationForms { get; set; }
     }
 }
---
+++ GetIntoTeachingApiTests/Models/FindApply/ApplicationFormTests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using GetIntoTeachingApi.Models.FindApply;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace GetIntoTeachingApiTests.Models.FindApply
+{
+    public class ApplicationFormTests
+    {
+        [Fact]
+        public void JsonAttributes()
+        {
+            var type = typeof(ApplicationForm);
+
+            type.GetProperty("Id").Should()
+                .BeDecoratedWith<JsonPropertyAttribute>(a => a.PropertyName == "id");
+            type.GetProperty("CreatedAt").Should()
+                .BeDecoratedWith<JsonPropertyAttribute>(a => a.PropertyName == "created_at");
+            type.GetProperty("UpdatedAt").Should()
+                .BeDecoratedWith<JsonPropertyAttribute>(a => a.PropertyName == "updated_at");
+        }
+    }
+}
