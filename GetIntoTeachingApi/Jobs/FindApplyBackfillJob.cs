25a9d45f8ac2503b55958e76641621f432d51e91
Ross Oliver <ross.oliver36@gmail.com>
2021-10-19 15:26:04 +0100

    Add Find/Apply candidate backfill job
    We want to be able to backfill all the candidate information in Apply to the
    CRM so that we can sync across data thats been created in production up until
    now.
    
    Add a new job that hits the Apply API and pages through all candidates,
    enqueuing them as individual sync jobs.

---
+++ GetIntoTeachingApi/Jobs/FindApplyBackfillJob.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Flurl;
+using Flurl.Http;
+using GetIntoTeachingApi.Models;
+using GetIntoTeachingApi.Models.FindApply;
+using GetIntoTeachingApi.Services;
+using GetIntoTeachingApi.Utils;
+using Hangfire;
+using Microsoft.Extensions.Logging;
+using MoreLinq;
+
+namespace GetIntoTeachingApi.Jobs
+{
+    public class FindApplyBackfillJob : BaseJob
+    {
+        private readonly IBackgroundJobClient _jobClient;
+        private readonly ILogger<FindApplyBackfillJob> _logger;
+        private readonly IAppSettings _appSettings;
+
+        public FindApplyBackfillJob(
+            IEnv env,
+            IBackgroundJobClient jobClient,
+            ILogger<FindApplyBackfillJob> logger,
+            IAppSettings appSettings)
+            : base(env)
+        {
+            _jobClient = jobClient;
+            _logger = logger;
+            _appSettings = appSettings;
+        }
+
+        [DisableConcurrentExecution(timeoutInSeconds: 5)]
+        public async Task RunAsync()
+        {
+            _appSettings.IsFindApplyBackfillInProgress = true;
+            _logger.LogInformation("FindApplyBackfillJob - Started");
+            await QueueCandidateSyncJobs();
+            _logger.LogInformation("FindApplyBackfillJob - Succeeded");
+            _appSettings.IsFindApplyBackfillInProgress = false;
+        }
+
+        private async Task QueueCandidateSyncJobs()
+        {
+            var request = Env.FindApplyApiUrl
+                .AppendPathSegment("candidates")
+                .SetQueryParam("updated_since", DateTime.MinValue)
+                .WithOAuthBearerToken(Env.FindApplyApiKey);
+
+            var paginator = new PaginatorClient<Response<IEnumerable<Candidate>>>(request);
+
+            while (paginator.HasNext)
+            {
+                var response = await paginator.NextAsync();
+                _logger.LogInformation($"FindApplyBackfillJob - Syncing {response.Data.Count()} Candidates");
+                response.Data.ForEach(c => _jobClient.Enqueue<FindApplyCandidateSyncJob>(x => x.Run(c)));
+            }
+        }
+    }
+}
---
+++ GetIntoTeachingApiTests/Jobs/FindApplyBackfillJobTests.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Flurl.Http.Testing;
+using GetIntoTeachingApi.Jobs;
+using GetIntoTeachingApi.Models;
+using GetIntoTeachingApi.Models.FindApply;
+using GetIntoTeachingApi.Utils;
+using GetIntoTeachingApiTests.Helpers;
+using Hangfire;
+using Hangfire.Common;
+using Hangfire.States;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace GetIntoTeachingApiTests.Jobs
+{
+    public class FindApplyBackfillJobTests
+    {
+        private readonly Mock<ILogger<FindApplyBackfillJob>> _mockLogger;
+        private readonly Mock<IBackgroundJobClient> _mockJobClient;
+        private readonly Mock<IEnv> _mockEnv;
+        private readonly Mock<IAppSettings> _mockAppSettings;
+        private readonly FindApplyBackfillJob _job;
+
+        public FindApplyBackfillJobTests()
+        {
+            _mockLogger = new Mock<ILogger<FindApplyBackfillJob>>();
+            _mockJobClient = new Mock<IBackgroundJobClient>();
+            _mockEnv = new Mock<IEnv>();
+            _mockAppSettings = new Mock<IAppSettings>();
+
+            _mockEnv.Setup(m => m.FindApplyApiUrl).Returns("https://test.apply.api/candidates-api");
+            _mockEnv.Setup(m => m.FindApplyApiKey).Returns("1234567");
+
+            _job = new FindApplyBackfillJob(
+                _mockEnv.Object,
+                _mockJobClient.Object,
+                _mockLogger.Object,
+                _mockAppSettings.Object);
+        }
+
+        [Fact]
+        public void DisableConcurrentExecutionAttribute()
+        {
+            var type = typeof(FindApplyBackfillJob);
+
+            type.GetMethod("RunAsync").Should().BeDecoratedWith<DisableConcurrentExecutionAttribute>();
+        }
+
+        [Fact]
+        public async void RunAsync_WhenMultiplePagesAvailable_QueuesCandidateJobsForEachPage()
+        {
+            var candidates1 = new Candidate[]
+            {
+                new Candidate() { Id = "11111", Attributes = new CandidateAttributes() { Email = "email1@address.com" } },
+            };
+            var candidates2 = new Candidate[]
+            {
+                new Candidate() { Id = "11111", Attributes = new CandidateAttributes() { Email = "email1@address.com" } },
+            };
+
+            using (var httpTest = new HttpTest())
+            {
+                MockResponse(httpTest, new Response<IEnumerable<Candidate>>() { Data = candidates1 }, 1, 2);
+                MockResponse(httpTest, new Response<IEnumerable<Candidate>>() { Data = candidates2 }, 2, 2);
+                await _job.RunAsync();
+            }
+
+            _mockJobClient.Verify(x => x.Create(
+               It.Is<Job>(job => job.Type == typeof(FindApplyCandidateSyncJob) && job.Method.Name == "Run"),
+               It.IsAny<EnqueuedState>()), Times.Exactly(candidates1.Length + candidates2.Length));
+
+            _mockAppSettings.VerifySet(m => m.IsFindApplyBackfillInProgress = true, Times.Once);
+            _mockLogger.VerifyInformationWasCalled("FindApplyBackfillJob - Started");
+            _mockLogger.VerifyInformationWasCalled("FindApplyBackfillJob - Syncing 1 Candidates");
+            _mockLogger.VerifyInformationWasCalled("FindApplyBackfillJob - Syncing 1 Candidates");
+            _mockLogger.VerifyInformationWasCalled("FindApplyBackfillJob - Succeeded");
+            _mockAppSettings.VerifySet(m => m.IsFindApplyBackfillInProgress = false, Times.Once);
+        }
+
+        [Fact]
+        public async void RunAsync_WithNoCandidates_DoesNotQueueJobs()
+        {
+            using (var httpTest = new HttpTest())
+            {
+                var response = new Response<IEnumerable<Candidate>>() { Data = Array.Empty<Candidate>() };
+                MockResponse(httpTest, response);
+                await _job.RunAsync();
+            }
+
+            _mockJobClient.Verify(x => x.Create(
+               It.Is<Job>(job => job.Type == typeof(FindApplyCandidateSyncJob)),
+               It.IsAny<EnqueuedState>()), Times.Never);
+
+            _mockAppSettings.VerifySet(m => m.IsFindApplyBackfillInProgress = true, Times.Once);
+            _mockLogger.VerifyInformationWasCalled("FindApplyBackfillJob - Started");
+            _mockLogger.VerifyInformationWasCalled("FindApplyBackfillJob - Syncing 0 Candidates");
+            _mockLogger.VerifyInformationWasCalled("FindApplyBackfillJob - Succeeded");
+            _mockAppSettings.VerifySet(m => m.IsFindApplyBackfillInProgress = false, Times.Once);
+        }
+
+        private void MockResponse(HttpTest httpTest, Response<IEnumerable<Candidate>> response, int page = 1, int totalPages = 1)
+        {
+            var json = JsonConvert.SerializeObject(response);
+            var headers = new Dictionary<string, int>() { { "Total-Pages", totalPages }, { "Current-Page", page } };
+
+            httpTest
+                    .ForCallsTo($"{_mockEnv.Object.FindApplyApiUrl}/candidates")
+                    .WithVerb("GET")
+                    .WithQueryParam("page", page)
+                    .WithQueryParam("updated_since", DateTime.MinValue)
+                    .WithHeader("Authorization", $"Bearer {_mockEnv.Object.FindApplyApiKey}")
+                    .RespondWith(json, 200, headers);
+        }
+    }
+}
