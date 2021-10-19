6e3343fef29bacabdc7cda07d6575455ca254df4
Ross Oliver <ross.oliver36@gmail.com>
2021-10-19 11:17:02 +0100

    Paginate Apply API sync job
    The Apply API now supports pagination by passing a `?page=<num>` query string
    attribute. The response headers include a `Total-Pages` and `Current-Page` key
    so that we can work out if there are further records to retrieve.
    
    Create a new `PaginatorClient` that can take a `Flurl` resource and paginate
    according to the above specification. Update the `FindApplySyncJob` to use the
    new paginator class and queue all pages of candidates for syncing with the CRM.

---
+++ GetIntoTeachingApi/Services/IPaginatorClient.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace GetIntoTeachingApi.Services
+{
+    public interface IPaginatorClient<T>
+    {
+        Task<T> NextAsync();
+        bool HasNext { get; }
+    }
+}
---
+++ GetIntoTeachingApi/Services/PaginatorClient.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Flurl.Http;
+
+namespace GetIntoTeachingApi.Services
+{
+    public class PaginatorClient<T> : IPaginatorClient<T>
+    {
+        private readonly IFlurlRequest _request;
+        private bool _hasNext;
+        private int _page;
+
+        public bool HasNext
+        {
+            get
+            {
+                return _hasNext;
+            }
+        }
+
+        public PaginatorClient(IFlurlRequest request)
+        {
+            _request = request;
+            _hasNext = true;
+            _page = 1;
+        }
+
+        public async Task<T> NextAsync()
+        {
+            var response = await _request
+                .SetQueryParam("page", _page)
+                .GetAsync();
+            var headers = response.Headers;
+
+            if (!headers.Contains("Total-Pages") || !headers.Contains("Current-Page"))
+            {
+                throw new KeyNotFoundException("Expected Total-Pages and Current-Page header keys");
+            }
+
+            var totalPages = headers.FirstOrDefault("Total-Pages");
+            var currentPage = headers.FirstOrDefault("Current-Page");
+            _hasNext = currentPage != totalPages;
+
+            _page++;
+
+            return await response.GetJsonAsync<T>();
+        }
+    }
+}
