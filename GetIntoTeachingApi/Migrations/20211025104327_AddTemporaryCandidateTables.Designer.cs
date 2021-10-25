5ffc949c354343b520032bb48757e9efa117a12c
Joseph Kempster <jkempster34@gmail.com>
2021-10-25 16:25:31 +0100

    Add `Candidate` cache
    The CRM can't accept any new data during releases. The solution to this in Get into Teaching is to queue the request until the CRM is back up.
    
    However, in School Experience, the Candidate upsert cannot be queued because the candidate information is needed immediately by the Schools Experience app.
    
    Add a `Candidate` cache and a intermediate table. The cache can be used when the CRM integration is paused. We can persist a temporary ID in the intermediate table which can be used to retrieve the CRM Candidate record once the cache has been cleared.

--- GetIntoTeachingApi/Migrations/20211025104327_AddTemporaryCandidateTables.Designer.cs
+++ GetIntoTeachingApi/Migrations/20211025104327_AddTemporaryCandidateTables.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using GetIntoTeachingApi.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NetTopologySuite.Geometries;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
@@ -10,9 +11,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace GetIntoTeachingApi.Migrations
 {
     [DbContext(typeof(GetIntoTeachingDbContext))]
-    partial class GetIntoTeachingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211025104327_AddTemporaryCandidateTables")]
+    partial class AddTemporaryCandidateTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ GetIntoTeachingApi/Models/CandidateIntermediateKeys.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GetIntoTeachingApi.Models
+{
+    public class CandidateIntermediateKeys
+    {
+        [Key]
+        public int IntermediateCandidateId { get; set; }
+        public int CrmCandidateId { get; set; }
+    }
+}
