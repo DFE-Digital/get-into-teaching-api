555bdcdcbe6c3f49e0fd99a4663a1fdba295010c
Joseph Kempster <jkempster34@gmail.com>
2021-12-08 14:36:51 +0000

    Add new CandidatesSchoolExperience model
    Currently, we add a ClassroomExperienceNote when the status of a School Experience candidate changes (e.g., they make a placement request, their placement is accepted etc.). This is a single formatted text field due to some time constraints during initial development.
    
    The CRM team have created a new table - CandidateSchoolExperience - to represent this data. 
    
    Add the initial models and validators. For now, the ClassroomExperienceNote will stay in place until the new feature is verified.

---
+++ GetIntoTeachingApi/Models/Crm/CandidateSchoolExperience.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using FluentValidation;
+using GetIntoTeachingApi.Attributes;
+using GetIntoTeachingApi.Services;
+using Microsoft.Xrm.Sdk;
+
+namespace GetIntoTeachingApi.Models.Crm
+{
+    [Entity("dfe_candidateschoolexperience")]
+    public class CandidateSchoolExperience : BaseModel, IHasCandidateId
+    {
+        public enum SchoolExperienceStatus
+        {
+            Requested = 1, // default
+            Confirmed = 222750000,
+            Withdrawn = 222750001,
+            Rejected = 222750002,
+            CancelledBySchool = 222750003,
+            CancelledByCandidate = 222750004,
+            Completed = 222750005,
+        }
+
+        [EntityField("dfe_contactid", typeof(EntityReference), "contact")]
+        public Guid CandidateId { get; set; }
+        [EntityField("dfe_urn")]
+        public string SchoolUrn { get; set; }
+        [EntityField("dfe_placementduration")]
+        public int? DurationOfPlacementInDays { get; set; }
+        [EntityField("dfe_dateofschoolexperience")]
+        public DateTime? DateOfSchoolExperience { get; set; }
+        [EntityField("statuscode", typeof(OptionSetValue))]
+        public int? Status { get; set; }
+        [EntityField("dfe_teachingsubject", typeof(EntityReference), "dfe_teachingsubjectlist")]
+        public Guid? TeachingSubjectId { get; set; }
+        [EntityField("dfe_notes")]
+        public string Notes { get; set; }
+        [EntityField("dfe_where")]
+        public string SchoolName { get; set; }
+
+        public CandidateSchoolExperience()
+        {
+        }
+
+        public CandidateSchoolExperience(Entity entity, ICrmService crm, IValidatorFactory validatorFactory)
+            : base(entity, crm, validatorFactory)
+        {
+        }
+    }
+}
