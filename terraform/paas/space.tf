773c7290f66290d2e703e308cb72dcede3174761
Steven Fawcett <64777084+sfawcett123@users.noreply.github.com>
2022-02-25 13:48:00 +0000

    Improve Network Policies (#928)

--- terraform/paas/space.tf
+++ terraform/paas/space.tf
@@ -2,7 +2,8 @@ data "cloudfoundry_space" "space" {
   name     = var.paas_space
   org_name = var.paas_org_name
 }
-data "cloudfoundry_space" "monitor" {
+
+data "cloudfoundry_space" "monitoring" {
   name     = var.monitor_space
   org_name = var.paas_org_name
 }
