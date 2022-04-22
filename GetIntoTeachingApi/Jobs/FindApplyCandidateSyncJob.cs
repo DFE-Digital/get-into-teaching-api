a024acee370d425499d84634eb39ff6900586166
Ross Oliver <ross.oliver36@gmail.com>
2022-04-22 15:26:58 +0100

    Use UpsertApplicationForm job when syncing Apply API
    We have a new `UpsertApplicationFormJob` that deals with upserting either a
    v1.1 or v1.2 `ApplicationForm` and associated entities. Switching to use this
    in the `FindApplyCandidateSyncJob` simplifies the logic for updating (otherwise
    we would have a job per API version).

--- GetIntoTeachingApi/Jobs/FindApplyCandidateSyncJob.cs
+++ GetIntoTeachingApi/Jobs/FindApplyCandidateSyncJob.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
 using GetIntoTeachingApi.Models.FindApply;
 using GetIntoTeachingApi.Services;
 using GetIntoTeachingApi.Utils;
@@ -46,78 +43,22 @@ namespace GetIntoTeachingApi.Jobs
 
         public void SyncCandidate(Candidate findApplyCandidate)
         {
-            var applicationForms = findApplyCandidate.Attributes.ApplicationForms;
-            var candidate = Candidate(findApplyCandidate, applicationForms);
+            var candidate = findApplyCandidate.ToCrmModel();
+            var match = _crm.MatchCandidate(candidate.Email);
 
-            candidate.ApplicationForms = ApplicationForms(applicationForms).ToList();
+            _logger.LogInformation("FindApplyCandidateSyncJob - {Status} - {Id}", match == null ? "Miss" : "Hit", findApplyCandidate.Id);
 
-            string json = candidate.SerializeChangeTracked();
-            _jobClient.Enqueue<UpsertCandidateJob>((x) => x.Run(json, null));
-        }
-
-        private Models.Crm.Candidate Candidate(Candidate findApplyCandidate, IEnumerable<ApplicationForm> findApplyApplicationForms)
-        {
-            var match = _crm.MatchCandidate(findApplyCandidate.Attributes.Email);
-            var status = match == null ? "Miss" : "Hit";
-
-            _logger.LogInformation("FindApplyCandidateSyncJob - {Status} - {Id}", status, findApplyCandidate.Id);
-
-            // We persist a new Candidate to ensure we only write the find/apply
-            // attributes back to the CRM and not existing attributes on the match.
-            var candidate = new Models.Crm.Candidate()
+            if (match != null)
             {
-                Id = match?.Id,
-                Email = findApplyCandidate.Attributes.Email,
-                FindApplyId = findApplyCandidate.Id,
-                FindApplyCreatedAt = findApplyCandidate.Attributes.CreatedAt,
-                FindApplyUpdatedAt = findApplyCandidate.Attributes.UpdatedAt,
-            };
-
-            if (match == null)
+                candidate.Id = match.Id;
+            }
+            else
             {
                 candidate.ChannelId = (int)Models.Crm.Candidate.Channel.ApplyForTeacherTraining;
             }
 
-            var latestApplicationForm = findApplyApplicationForms?.OrderByDescending(f => f.UpdatedAt).FirstOrDefault();
-
-            if (latestApplicationForm == null)
-            {
-                candidate.FindApplyStatusId = (int)Models.Crm.ApplicationForm.Status.NeverSignedIn;
-            }
-            else
-            {
-                candidate.FindApplyStatusId = (int)Enum.Parse(typeof(Models.Crm.ApplicationForm.Status), latestApplicationForm.ApplicationStatus.ToPascalCase());
-                candidate.FindApplyPhaseId = (int)Enum.Parse(typeof(Models.Crm.ApplicationForm.Phase), latestApplicationForm.ApplicationPhase.ToPascalCase());
-            }
-
-            return candidate;
-        }
-
-        private IEnumerable<Models.Crm.ApplicationForm> ApplicationForms(IEnumerable<ApplicationForm> findApplyApplicationForms)
-        {
-            if (findApplyApplicationForms == null)
-            {
-                return Array.Empty<Models.Crm.ApplicationForm>();
-            }
-
-            return findApplyApplicationForms.Select(findApplyForm =>
-            {
-                var existingForm = _crm.GetFindApplyModels<Models.Crm.ApplicationForm>(new string[] { findApplyForm.Id.ToString(CultureInfo.CurrentCulture) }).FirstOrDefault();
-
-                var yearId = ((int)Models.Crm.ApplicationForm.RecruitmentCycleYear.Year2020) + (findApplyForm.RecruitmentCycleYear - 2020);
-
-                return new Models.Crm.ApplicationForm()
-                {
-                    Id = existingForm?.Id,
-                    FindApplyId = findApplyForm.Id.ToString(CultureInfo.CurrentCulture),
-                    CreatedAt = findApplyForm.CreatedAt,
-                    UpdatedAt = findApplyForm.UpdatedAt,
-                    SubmittedAt = findApplyForm.SubmittedAt,
-                    StatusId = (int)Enum.Parse(typeof(Models.Crm.ApplicationForm.Status), findApplyForm.ApplicationStatus.ToPascalCase()),
-                    PhaseId = (int)Enum.Parse(typeof(Models.Crm.ApplicationForm.Phase), findApplyForm.ApplicationPhase.ToPascalCase()),
-                    RecruitmentCycleYearId = yearId,
-                };
-            });
+            string json = candidate.SerializeChangeTracked();
+            _jobClient.Enqueue<UpsertCandidateJob>((x) => x.Run(json, null));
         }
     }
 }
