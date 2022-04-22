03d39b89c6d69aad8535e475955d77add4727a22
Ross Oliver <ross.oliver36@gmail.com>
2022-04-22 13:20:27 +0100

    Add CRM models for v1.2 of Apply API integration
    The Apply API integration v1.2 has additional models related to the
    `ApplicationForm` as well as a few new fields on the form iteself.
    
    Add `IHasFindApplyId` interface which will be useful later in creating generic
    methods for Find/Apply models.
    
    Update `OperationsControllerTests` to reflect additional CRM model mappings.
    
    As these models/fields are only available in the CRM dev environment we are
    adding a new feature flag to ensure they don't get accessed in environments
    that don't yet support them (we only have to do this for the attributes on the
    form model as the relationships will always be empty/not mapped in environments
    that don't support them as the Apply API does not return them).

---
+++ GetIntoTeachingApiTests/Models/Crm/ApplicationInterviewTests.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using GetIntoTeachingApi.Attributes;
+using GetIntoTeachingApi.Models.Crm;
+using Microsoft.Xrm.Sdk;
+using Xunit;
+
+namespace GetIntoTeachingApiTests.Models.Crm
+{
+    public class ApplicationInterviewTests
+    {
+        [Fact]
+        public void EntityAttributes()
+        {
+            var type = typeof(ApplicationInterview);
+
+            type.Should().BeDecoratedWith<EntityAttribute>(a => a.LogicalName == "dfe_applyinterview");
+            type.Should().BeDecoratedWith<SwaggerIgnoreAttribute>();
+
+            type.GetProperty("ApplicationChoiceId").Should().BeDecoratedWith<EntityFieldAttribute>(a => a.Name == "dfe_applyapplicationchoice"
+                && a.Type == typeof(EntityReference) && a.Reference == "dfe_applyapplicationchoice");
+
+            type.GetProperty("FindApplyId").Should().BeDecoratedWith<EntityFieldAttribute>(a => a.Name == "dfe_interviewid");
+            type.GetProperty("CreatedAt").Should().BeDecoratedWith<EntityFieldAttribute>(a => a.Name == "dfe_createdon");
+            type.GetProperty("UpdatedAt").Should().BeDecoratedWith<EntityFieldAttribute>(a => a.Name == "dfe_modifiedon");
+            type.GetProperty("ScheduledAt").Should().BeDecoratedWith<EntityFieldAttribute>(a => a.Name == "dfe_interviewscheduledat");
+            type.GetProperty("CancelledAt").Should().BeDecoratedWith<EntityFieldAttribute>(a => a.Name == "dfe_interviewcancelledat");
+        }
+
+        [Fact]
+        public void Name_IsDerivedFromFindApplyId()
+        {
+            var form = new ApplicationInterview() { FindApplyId = "123" };
+
+            form.Name.Should().Be("Application Interview 123");
+        }
+    }
+}
---
+++ GetIntoTeachingApiTests/Models/Crm/ApplicationReferenceTests.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using GetIntoTeachingApi.Attributes;
+using GetIntoTeachingApi.Models.Crm;
+using Microsoft.Xrm.Sdk;
+using Xunit;
+
+namespace GetIntoTeachingApiTests.Models.Crm
+{
+    public class ApplicationReferenceTests
+    {
+        [Fact]
+        public void EntityAttributes()
+        {
+            var type = typeof(ApplicationReference);
+
+            type.Should().BeDecoratedWith<EntityAttribute>(a => a.LogicalName == "dfe_applyreference");
+            type.Should().BeDecoratedWith<SwaggerIgnoreAttribute>();
+
+            type.GetProperty("ApplicationFormId").Should().BeDecoratedWith<EntityFieldAttribute>(a => a.Name == "dfe_applyapplicationform"
+                && a.Type == typeof(EntityReference) && a.Reference == "dfe_applyapplicationform");
+
+            type.GetProperty("FeedbackStatusId").Should().BeDecoratedWith<EntityFieldAttribute>(
+                    a => a.Name == "dfe_referencefeedbackstatus" && a.Type == typeof(OptionSetValue));
+
+            type.GetProperty("FindApplyId").Should().BeDecoratedWith<EntityFieldAttribute>(a => a.Name == "dfe_referenceid");
+            type.GetProperty("RequestedAt").Should().BeDecoratedWith<EntityFieldAttribute>(a => a.Name == "dfe_requestedat");
+            type.GetProperty("Type").Should().BeDecoratedWith<EntityFieldAttribute>(a => a.Name == "dfe_referencetype");
+        }
+
+        [Fact]
+        public void Name_IsDerivedFromFindApplyId()
+        {
+            var form = new ApplicationInterview() { FindApplyId = "123" };
+
+            form.Name.Should().Be("Application Interview 123");
+        }
+    }
+}
