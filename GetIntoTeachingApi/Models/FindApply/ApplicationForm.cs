366916e2b44138bfedcfa60a9bf0e53f38012f01
Ross Oliver <ross.oliver36@gmail.com>
2022-04-22 13:55:23 +0100

    Updated Apply models for API v1.2
    The updated Apply API (v1.2) contains additional models related to the
    `ApplicationForm`; this commit adds them and maps the attributes to the JSON
    response payload from the API.
    
    Add convenience methods on each model to map from the Apply models to the CRM
    models.

--- GetIntoTeachingApi/Models/FindApply/ApplicationForm.cs
+++ GetIntoTeachingApi/Models/FindApply/ApplicationForm.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using GetIntoTeachingApi.Utils;
 using Newtonsoft.Json;
 
 namespace GetIntoTeachingApi.Models.FindApply
@@ -19,5 +23,35 @@ namespace GetIntoTeachingApi.Models.FindApply
         public string ApplicationPhase { get; set; }
         [JsonProperty("recruitment_cycle_year")]
         public int RecruitmentCycleYear { get; set; }
+        [JsonProperty("application_choices")]
+        public ApplicationResponse<IEnumerable<ApplicationChoice>> ApplicationChoices { get; set; }
+        [JsonProperty("references")]
+        public ApplicationResponse<IEnumerable<Reference>> References { get; set; }
+        [JsonProperty("qualifications")]
+        public ApplicationResponse<IEnumerable<object>> Qualifications { get; set; }
+        [JsonProperty("personal_statement")]
+        public ApplicationResponse<IEnumerable<object>> PersonalStatement { get; set; }
+
+        public Crm.ApplicationForm ToCrmModel()
+        {
+            var yearId = ((int)Crm.ApplicationForm.RecruitmentCycleYear.Year2020) + (RecruitmentCycleYear - 2020);
+
+            return new Crm.ApplicationForm()
+            {
+                FindApplyId = Id.ToString(CultureInfo.CurrentCulture),
+                CreatedAt = CreatedAt,
+                UpdatedAt = UpdatedAt,
+                SubmittedAt = SubmittedAt,
+                StatusId = (int)Enum.Parse(typeof(Crm.ApplicationForm.Status), ApplicationStatus.ToPascalCase()),
+                PhaseId = (int)Enum.Parse(typeof(Crm.ApplicationForm.Phase), ApplicationPhase.ToPascalCase()),
+                RecruitmentCycleYearId = yearId,
+                Choices = ApplicationChoices?.Data?.Select(c => c.ToCrmModel()).ToList(),
+                References = References?.Data?.Select(c => c.ToCrmModel()).ToList(),
+                ApplicationChoicesCompleted = ApplicationChoices?.Completed,
+                ReferencesCompleted = References?.Completed,
+                PersonalStatementCompleted = PersonalStatement?.Completed,
+                QualificationsCompleted = Qualifications?.Completed,
+            };
+        }
     }
 }
---
+++ GetIntoTeachingApi/Models/FindApply/Provider.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace GetIntoTeachingApi.Models.FindApply
+{
+	public class Provider
+	{
+		[JsonProperty("name")]
+		public string Name { get; set; }
+	}
+}
