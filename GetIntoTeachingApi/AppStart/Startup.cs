3918c035048ba5edf163aac6e137c7710bf9a9ef
Ross Oliver <ross.oliver36@gmail.com>
2022-05-04 09:26:39 +0100

    Remove dot prefix from env files
    The .env files are not being copied into the Docker image; I presume its
    because they are prefixed with a dot (as other files copy correctly) so
    removing to see if that sorts it.

--- GetIntoTeachingApi/AppStart/Startup.cs
+++ GetIntoTeachingApi/AppStart/Startup.cs
@@ -31,7 +31,7 @@ namespace GetIntoTeachingApi.AppStart
             if (!_env.IsTest)
             {
                 var root = Directory.GetCurrentDirectory();
-                var envFile = Path.Combine(root, $".env.{_env.CloudFoundryEnvironmentName.ToLowerInvariant()}");
+                var envFile = Path.Combine(root, $"env.{_env.CloudFoundryEnvironmentName.ToLowerInvariant()}");
                 DotEnv.Load(options: new DotEnvOptions(ignoreExceptions: false, envFilePaths: new[] { envFile }));
             }
 
