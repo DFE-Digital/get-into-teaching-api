b7dad3646746672c317f11b8b02c57855a20dc59
Ross Oliver <ross.oliver36@gmail.com>
2023-01-31 10:59:16 +0000

    Add endpoint to retrieve existing candidate id
    When a candidate signs up for a TTA via the Apply service they will already be
    'authenticated' against the candidate email so we will not need to issue them a
    TOTP. Instead, we want to obtain a candidate id by providing matchback
    information.

---
+++ GetIntoTeachingApiTests/Models/CandidateMatchbackResponseTests.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using GetIntoTeachingApi.Models;
+using System;
+using Microsoft.Xrm.Sdk;
+using Xunit;
+using GetIntoTeachingApi.Models.Crm;
+
+namespace GetIntoTeachingApiTests.Models
+{
+    public class CandidateMatchbackResponseTests
+    {
+        private readonly CandidateMatchbackResponse _response;
+        private readonly Candidate _candidate;
+
+        public CandidateMatchbackResponseTests()
+        {
+            _candidate = new Candidate() { Id = Guid.NewGuid() };
+            _response = new CandidateMatchbackResponse(_candidate);
+        }
+
+        [Fact]
+        public void CandidateId_ReturnsCandidateId()
+        {
+            _response.CandidateId.Should().Be((Guid)_candidate.Id);
+        }
+    }
+}
