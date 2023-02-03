70018106f876fa554b0974680739e80cc4760e4c
Ross Oliver <ross.oliver36@gmail.com>
2023-02-03 13:53:39 +0000

    Add Id validators for new models
    We have a generic `LookupItemIdValidator` that ensures foreign keys are valid
    on submissions, however going forward we need separate validators for each of
    the new models that queries the corresponding tables.

---
+++ GetIntoTeachingApi/Validators/TeachingSubjectIdValodator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using FluentValidation;
+using FluentValidation.Validators;
+using GetIntoTeachingApi.Services;
+
+namespace GetIntoTeachingApi.Validators
+{
+    public class TeachingSubjectIdValidator<T> : PropertyValidator<T, Guid?>
+    {
+        private readonly IStore _store;
+
+        public TeachingSubjectIdValidator(IStore store)
+            : base()
+        {
+            _store = store;
+        }
+
+        public override bool IsValid(ValidationContext<T> context, Guid? value)
+        {
+            var exists = _store.GetTeachingSubjects().Any(i => i.Id == value);
+
+            if (exists)
+            {
+                return true;
+            }
+
+            context.MessageFormatter.AppendArgument("PropertyName", context.PropertyName);
+
+            return false;
+        }
+
+        public override string Name => "TeachingSubjectIdValidator";
+
+        protected override string GetDefaultMessageTemplate(string errorCode) => "{PropertyName} must be a valid teaching subject id.";
+    }
+}
---
+++ GetIntoTeachingApiTests/Validators/TeachingSubjectIdValidatorTests.cs
@@ -0,0 +1,49 @@
+﻿using GetIntoTeachingApi.Services;
+using GetIntoTeachingApi.Validators;
+using Moq;
+using Xunit;
+using System;
+using FluentValidation;
+using GetIntoTeachingApi.Models;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+
+namespace GetIntoTeachingApiTests.Validators
+{
+    public class TeachingSubjectIdValidatorTests
+    {
+        private readonly Mock<IStore> _mockStore;
+        private readonly TeachingSubject _item;
+        private readonly TeachingSubjectIdValidator<object> _validator;
+
+        public TeachingSubjectIdValidatorTests()
+        {
+            _mockStore = new Mock<IStore>();
+            _validator = new TeachingSubjectIdValidator<object>(_mockStore.Object);
+            _item = new TeachingSubject() { Id = Guid.NewGuid() };
+
+            _mockStore.Setup(m => m.GetTeachingSubjects()).Returns(new List<TeachingSubject>() { _item }.AsQueryable());
+        }
+
+        [Fact]
+        public void IsValid_WhenValidId_ReturnsTrue()
+        {
+            var context = new ValidationContext<object>(this);
+
+            var valid = _validator.IsValid(context, _item.Id);
+
+            valid.Should().BeTrue();
+        }
+
+        [Fact]
+        public void IsValid_WhenInvalidId_ReturnsFalse()
+        {
+            var context = new ValidationContext<object>(this);
+
+            var valid = _validator.IsValid(context, Guid.NewGuid());
+
+            valid.Should().BeFalse();
+        }
+    }
+}
