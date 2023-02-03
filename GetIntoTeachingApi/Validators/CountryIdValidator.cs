fb1a1daa29bd72083c515e7f8575d92f392c24f9
Ross Oliver <ross.oliver36@gmail.com>
2023-02-03 13:53:39 +0000

    Add Id validators for new models
    We have a generic `LookupItemIdValidator` that ensures foreign keys are valid
    on submissions, however going forward we need separate validators for each of
    the new models that queries the corresponding tables.

---
+++ GetIntoTeachingApi/Validators/CountryIdValidator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using FluentValidation;
+using FluentValidation.Validators;
+using GetIntoTeachingApi.Services;
+
+namespace GetIntoTeachingApi.Validators
+{
+    public class CountryIdValidator<T> : PropertyValidator<T, Guid?>
+    {
+        private readonly IStore _store;
+
+        public CountryIdValidator(IStore store)
+            : base()
+        {
+            _store = store;
+        }
+
+        public override bool IsValid(ValidationContext<T> context, Guid? value)
+        {
+            var exists = _store.GetCountries().Any(i => i.Id == value);
+
+            if (exists)
+            {
+                return true;
+            }
+
+            context.MessageFormatter.AppendArgument("PropertyName", context.PropertyName);
+
+            return false;
+        }
+
+        public override string Name => "CountryIdValidator";
+
+        protected override string GetDefaultMessageTemplate(string errorCode) => "{PropertyName} must be a valid country id.";
+    }
+}
