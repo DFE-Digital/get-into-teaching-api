6a0adab8653efd0287ac730d96a17df141238e71
Ross Oliver <ross.oliver36@gmail.com>
2023-02-03 13:50:11 +0000

    Add individual Country/TeachingSubject models
    We currently use a generic `LookupItem` model for both countries and teaching
    subjects. Unlike `PickListItem` these entites in the CRM can have additional
    fields assigned to them. The countries will now be returned with a country
    code, so we need the ability to add fields to our models.
    
    I originall started modelling the `Country` as a subclass of `LookupItem` but
    it quickly unravelled and I found migrating the current `LookupItem`s would be
    difficult this way. Instead, we're adding separate `Country` and
    `TeachingSubject` models. These will retain the `Id` and `Value` attributes of
    `LookupItem` so the client applications can continue to function without
    changes.

---
+++ GetIntoTeachingApi/Models/TeachingSubject.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.Xrm.Sdk;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GetIntoTeachingApi.Models
+{
+    public class TeachingSubject
+    {
+        public static readonly Guid PrimaryTeachingSubjectId = new("b02655a1-2afa-e811-a981-000d3a276620");
+
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public Guid? Id { get; set; }
+        public string Value { get; set; }
+
+        public TeachingSubject()
+        {
+        }
+
+        public TeachingSubject(Entity entity)
+        {
+            Id = entity.Id;
+            Value = entity.GetAttributeValue<string>("dfe_name");
+        }
+    }
+}
---
+++ GetIntoTeachingApiTests/Models/CountryTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using FluentAssertions;
+using GetIntoTeachingApi.Models;
+using Microsoft.Xrm.Sdk;
+using Xunit;
+
+namespace GetIntoTeachingApiTests.Models
+{
+    public class CountryTests
+    {
+        private readonly Entity _entity;
+
+        public CountryTests()
+        {
+            _entity = new Entity()
+            {
+                Id = Guid.NewGuid(),
+                LogicalName = "entityName"
+            };
+
+            _entity["dfe_name"] = "name";
+            _entity["dfe_countrykey"] = "code";
+        }
+
+        [Fact]
+        public void EntityAttributes()
+        {
+            var type = typeof(Country);
+
+            type.GetProperty("Id").Should().BeDecoratedWith<DatabaseGeneratedAttribute>(
+                a => a.DatabaseGeneratedOption == DatabaseGeneratedOption.None);
+        }
+
+        [Fact]
+        public void Constructor_WithEntity_WhenCountryCodeFeatureIsOn_SetsCountryCode()
+        {
+            var previousFeature = Environment.GetEnvironmentVariable("COUNTRY_CODES_FEATURE");
+            Environment.SetEnvironmentVariable("COUNTRY_CODES_FEATURE", "true");
+
+            var country = new Country(_entity);
+
+            country.Id.Should().Be(_entity.Id);
+            country.Value.Should().Be(_entity.GetAttributeValue<string>("dfe_name"));
+            country.IsoCode.Should().Be(_entity.GetAttributeValue<string>("dfe_countrykey"));
+
+            Environment.SetEnvironmentVariable("COUNTRY_CODES_FEATURE", previousFeature);
+        }
+
+        [Fact]
+        public void Constructor_WithEntity_WhenCountryCodeFeatureIsOff_DoesNotSetCountryCode()
+        {
+            var previousFeature = Environment.GetEnvironmentVariable("COUNTRY_CODES_FEATURE");
+            Environment.SetEnvironmentVariable("COUNTRY_CODES_FEATURE", "false");
+
+            var country = new Country(_entity);
+
+            country.Id.Should().Be(_entity.Id);
+            country.Value.Should().Be(_entity.GetAttributeValue<string>("dfe_name"));
+            country.IsoCode.Should().BeNull();
+
+            Environment.SetEnvironmentVariable("COUNTRY_CODES_FEATURE", previousFeature);
+        }
+    }
+}
