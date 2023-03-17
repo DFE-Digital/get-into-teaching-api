421aa618b10995d8d991bd73c091debb4564b34b
Ross Oliver <ross.oliver36@gmail.com>
2023-03-17 08:26:37 +0000

    Relax ExistingCandidateRequest validation
    Historically the matchback was more stringent and required a first/last name or
    DOB to match as well as email. It has since been updated to fallback to only
    matching on email address, although all existing services will also send a
    first/last name.
    
    Now that Apply offers an adviser sign up the matchback can happen without a
    first/last name and the API raises an error. Relaxing this validation will
    prevent the error and not change the underlying functionality (as we fallback
    to an email only matchback anyway).

--- GetIntoTeachingApi/Models/Validators/ExistingCandidateRequestValidator.cs
+++ GetIntoTeachingApi/Models/Validators/ExistingCandidateRequestValidator.cs
@@ -1,27 +1,13 @@
-﻿using System.Linq;
-using FluentValidation;
+﻿using FluentValidation;
 using FluentValidation.Validators;
-using GetIntoTeachingApi.Services;
 
 namespace GetIntoTeachingApi.Models.Validators
 {
     public class ExistingCandidateRequestValidator : AbstractValidator<ExistingCandidateRequest>
     {
-        public ExistingCandidateRequestValidator(IDateTimeProvider dateTime)
+        public ExistingCandidateRequestValidator()
         {
-            RuleFor(request => request.FirstName).MaximumLength(256);
-            RuleFor(request => request.LastName).MaximumLength(256);
             RuleFor(request => request.Email).NotEmpty().EmailAddress(EmailValidationMode.AspNetCoreCompatible).MaximumLength(100);
-            RuleFor(request => request.DateOfBirth).LessThan(request => dateTime.UtcNow);
-            RuleFor(request => request)
-                .Must(SpecifyTwoAdditionalRequiredAttributes)
-                .WithMessage("You must specify values for 2 additional attributes (from birthdate, firstname and lastname).");
-        }
-
-        private static bool SpecifyTwoAdditionalRequiredAttributes(ExistingCandidateRequest request)
-        {
-            var additionalRequiredAttributes = new object[] { request.DateOfBirth, request.FirstName, request.LastName };
-            return additionalRequiredAttributes.Count(attribute => attribute != null) >= 2;
         }
     }
 }
