259f2210f0354dc97fec7f2437ba834c52c818f4
Divesh Mavji <divesh987@hotmail.co.uk>
2023-06-20 14:05:33 +0100

    Deploy Redis
    Add redis resource to databases terraform

--- terraform/aks/databases.tf
+++ terraform/aks/databases.tf
@@ -1,8 +1,25 @@
+module "redis-cache" {
+  source = "./vendor/modules/aks//aks/redis"
+
+  namespace             = var.namespace
+  environment           = var.environment
+  azure_resource_prefix = var.azure_resource_prefix
+  service_short         = var.service_short
+  config_short          = var.config_short
+  service_name          = var.service_name
+
+  cluster_configuration_map = module.cluster_data.configuration_map
+
+  use_azure               = true
+  azure_enable_monitoring = var.enable_monitoring
+  azure_patch_schedule    = [{ "day_of_week": "Sunday", "start_hour_utc": 01 }]
+}
+
 module "postgres" {
   source = "./vendor/modules/aks//aks/postgres"
 
   namespace             = var.namespace
-  environment           = local.app_name_suffix
+  environment           = var.environment
   azure_resource_prefix = var.azure_resource_prefix
   service_short         = var.service_short
   config_short          = var.config_short
