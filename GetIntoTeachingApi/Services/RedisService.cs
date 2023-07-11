15a335031eef8e9aa19077fdb24d39a5f2477c03
Pavel Lisovin <pavel.lisovin@education.gov.uk>
2023-07-11 09:18:43 +0100

    Fi redis configuration in case redis connection string is used for aks

--- GetIntoTeachingApi/Services/RedisService.cs
+++ GetIntoTeachingApi/Services/RedisService.cs
@@ -4,6 +4,7 @@ using GetIntoTeachingApi.Models;
 using GetIntoTeachingApi.Redis;
 using GetIntoTeachingApi.Utils;
 using StackExchange.Redis;
+using System.Net;
 
 namespace GetIntoTeachingApi.Services
 {
@@ -11,20 +12,27 @@ namespace GetIntoTeachingApi.Services
     {
         private readonly ConnectionMultiplexer _redis;
         private readonly ConfigurationOptions _options;
+        private readonly EndPoint _endpoint;
 
         public IDatabase Database => _redis.GetDatabase();
 
         public RedisService(IEnv env)
         {
+          if (string.IsNullOrEmpty(env.RedisConnectionString)) {
             _options = RedisConfiguration.ConfigurationOptions(env);
+            _endpoint = _options.EndPoints[0];
             _redis = ConnectionMultiplexer.Connect(_options);
+          } else {
+            _endpoint = _redis.GetEndPoints()[0];
+            _redis = ConnectionMultiplexer.Connect(env.RedisConnectionString);
+          }
         }
 
         public async Task<string> CheckStatusAsync()
         {
             try
             {
-                await _redis.GetServer(_options.EndPoints[0]).PingAsync();
+                await _redis.GetServer(_endpoint).PingAsync();
                 return HealthCheckResponse.StatusOk;
             }
             catch (Exception e)
