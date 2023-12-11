83e8c1a56cc10f2dd3468a2a6ad145d42ff597e8
Colin Saliceti <colin.saliceti@ukdevops.co.uk>
2023-12-11 16:21:54 +0000

    Delete PaaS files

--- terraform/paas/space.tf
+++
@@ -1,9 +0,0 @@
-data "cloudfoundry_space" "space" {
-  name     = var.paas_space
-  org_name = var.paas_org_name
-}
-
-data "cloudfoundry_space" "monitoring" {
-  name     = var.monitor_space
-  org_name = var.paas_org_name
-}
