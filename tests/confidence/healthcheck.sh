6d8a6b139da6ec8a001ba74d8621e3145a1472fd
Colin Saliceti <colin.saliceti@ukdevops.co.uk>
2023-12-11 17:09:19 +0000

    Reintroduce smoke tests using AKS now

--- tests/confidence/healthcheck.sh
+++ tests/confidence/healthcheck.sh
@@ -4,7 +4,7 @@
 ### check the SHA against a passed in parameter
 ###
 ### Input parameters ( not validated )
-### 1 URL
+### 1 HOSTNAME
 ### 2 SHA
 ###
 ### Returns
@@ -12,16 +12,19 @@
 ### 0 on sucess
 ###
 #################################################################################################
-URL=${1}
+set -eu
+
+HOSTNAME=${1}
 EXPECTED_SHA=${2}
 
-#URL="get-into-teaching-api-dev"
-#EXPECTED_SHA="730ff0a"
+# Example:
+# HOSTNAME="getintoteachingapi-test.test.teacherservices.cloud"
+# EXPECTED_SHA="730ff0a"
 
 rval=0
-FULL_URL="https://${URL}.london.cloudapps.digital/api/operations/health_check"
-http_status=$(curl -o /dev/null -s -w "%{http_code}"  ${FULL_URL}) 
-if [ "${http_status}" != "200" ] 
+FULL_URL="https://${HOSTNAME}/api/operations/health_check"
+http_status=$(curl -o /dev/null -s -w "%{http_code}"  ${FULL_URL})
+if [ "${http_status}" != "200" ]
 then
 	echo "HTTP Status ${http_status}"
 	rval=1
@@ -30,7 +33,7 @@ else
 
         json=$(curl -s -X GET ${FULL_URL})
         status=$( echo ${json} | jq -r .status)
-        if [ "${status}" == "unhealthy" ] 
+        if [ "${status}" == "unhealthy" ]
         then
         	echo "Application Status is Unhealthy"
         	rval=1
@@ -39,7 +42,7 @@ else
         fi
 
         sha=$( echo ${json} | jq -r .gitCommitSha)
-        if [ "${sha}" != "${EXPECTED_SHA}"  ] 
+        if [ "${sha}" != "${EXPECTED_SHA}"  ]
         then
         	echo "SHA is not ${EXPECTED_SHA}"
         	rval=1
@@ -48,4 +51,3 @@ else
         fi
 fi
 exit ${rval}
-
