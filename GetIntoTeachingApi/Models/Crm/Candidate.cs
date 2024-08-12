aec4ef21da77547abd6036f14cd0678b9730a56b
Martyn Whitwell <martyn.whitwell@gmail.com>
2024-08-12 10:37:27 +0100

    Specify SwggerIgnore attribute

--- GetIntoTeachingApi/Models/Crm/Candidate.cs
+++ GetIntoTeachingApi/Models/Crm/Candidate.cs
@@ -10,7 +10,7 @@ using Swashbuckle.AspNetCore.Annotations;
 namespace GetIntoTeachingApi.Models.Crm
 {
     [Entity("contact")]
-    [SwaggerIgnore]
+    [GetIntoTeachingApi.Attributes.SwaggerIgnore]
     public class Candidate : BaseModel
     {
         public static readonly Guid AdviserBusinessUnitId = new Guid("1A61F629-F502-E911-A972-000D3A23443B");
