3c00c34d9d929b9872ffc3e8424a1f9aae027d46
spanersoraferty <s_ohegarty@yahoo.co.uk>
2025-01-07 21:34:40 +0000

    Initisl commit

---
+++ GetIntoTeaching/Infrastructure/Persistence/CandidateManagement/Common/DynamicsCrmQueryHandler.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.PowerPlatform.Dataverse.Client;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Client;
+using Microsoft.Xrm.Sdk.Query;
+using System.Collections.Immutable;
+using System.Linq.Expressions;
+using System.Runtime.Serialization;
+
+namespace GetIntoTeaching.Infrastructure.Persistence.CandidateManagement.Common
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class DynamicsCrmQueryHandler : IQueryHandler
+    {
+        private readonly ServiceClient _dataverseServiceClient;
+        private readonly OrganizationServiceContext _serviceContext;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="dataverseService">
+        /// 
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        /// 
+        /// </exception>
+        public DynamicsCrmQueryHandler(IOrganizationService dataverseService)
+        {
+            _dataverseServiceClient = dataverseService as ServiceClient ??
+                throw new ArgumentNullException(nameof(dataverseService));
+
+            _serviceContext = new OrganizationServiceContext(dataverseService);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="entityName"></param>
+        /// <param name="selector"></param>
+        /// <param name="predicate"></param>
+        /// <returns></returns>
+        public ImmutableList<IExtensibleDataObject> ReadItemsAsync(
+            string entityName,
+            Expression<Func<IExtensibleDataObject, IExtensibleDataObject>> selector,
+            Expression<Func<IExtensibleDataObject, bool>> predicate)
+        {
+            ArgumentNullException.ThrowIfNullOrEmpty(entityName);
+
+            return _serviceContext.CreateQuery(entityName)
+                .Where(predicate)
+                .Select(selector).ToImmutableList();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="query"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public ImmutableList<IExtensibleDataObject> ReadItemsAsync(IExtensibleDataObject query)
+        {
+            ArgumentNullException.ThrowIfNull(query);
+
+            QueryBase? queryBase = query as QueryBase ??
+                throw new ArgumentNullException(nameof(query));
+
+            EntityCollection entityCollection =
+                _dataverseServiceClient.RetrieveMultiple(queryBase);
+
+            return entityCollection.Entities.ToImmutableList<IExtensibleDataObject>();
+        }
+    }
+}
---
+++ GetIntoTeaching/Infrastructure/Persistence/CandidateManagement/Common/IQueryHandler.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Immutable;
+using System.Linq.Expressions;
+using System.Runtime.Serialization;
+
+namespace GetIntoTeaching.Infrastructure.Persistence.CandidateManagement.Common
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IQueryHandler
+    {
+        /// <summary>
+        /// Performs a query by creating a LINQ query for entities over the precribed CRM, utilising the
+        /// lambda expressions provisioned for the selector (projects each element of the specified
+        /// sequence into a new form), and predicate (filters a sequence of values based on the
+        /// criteria specified).
+        /// </summary>
+        /// <typeparam name="TItem">
+        /// The type of resource to return in the <see cref="ItemResponse{TItem}"/>.
+        /// </typeparam>
+        /// <param name="containerKey">
+        /// Represents a container key value in the Azure Cosmos DB service.
+        /// </param>
+        /// <param name="selector">
+        /// Lambda expression used to define what elements to project to the specified sequence.
+        /// </param>
+        /// <param name="predicate">
+        /// Lambda expression used to define the predicates on which to filter a sequence of values
+        /// based on the criteria specified.
+        /// </param>
+        /// <returns>
+        /// A configured instance of the specified generic type which encapsulates the
+        /// requested CRM entity item.
+        /// </returns>
+        ImmutableList<IExtensibleDataObject> ReadItemsAsync(
+            string entityName,
+            Expression<Func<IExtensibleDataObject, IExtensibleDataObject>> selector,
+            Expression<Func<IExtensibleDataObject, bool>> predicate);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="query"></param>
+        /// <returns></returns>
+        public ImmutableList<IExtensibleDataObject> ReadItemsAsync(IExtensibleDataObject query);
+    }
+}
