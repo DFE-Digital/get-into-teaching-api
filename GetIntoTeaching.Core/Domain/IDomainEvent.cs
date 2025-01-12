af496fb9a2934dc62337a66ddaa33d15bfaa4042
spanersoraferty <s_ohegarty@yahoo.co.uk>
2025-01-12 21:21:03 +0000

    Extended domain event contract

--- GetIntoTeaching.Core/Domain/IDomainEvent.cs
+++ GetIntoTeaching.Core/Domain/IDomainEvent.cs
@@ -1,6 +1,7 @@
 ﻿namespace GetIntoTeaching.Core.Domain
 {
-    // Marker interface for all domain events.
-    public interface IDomainEvent{
+    public interface IDomainEvent
+    {
+        DateTime DateTimeRaised { get; }
     }
 }
