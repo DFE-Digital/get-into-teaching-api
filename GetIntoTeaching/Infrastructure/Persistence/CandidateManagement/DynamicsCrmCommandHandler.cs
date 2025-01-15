cab7999a9906d2466e85977482c298ed399ea32a
Spencer O'HEGARTY <Spencer.OHEGARTY@EDUCATION.GOV.UK>
2025-01-15 17:45:55 +0000

    WIP - build out of crm command handler

---
+++ GetIntoTeaching/Infrastructure/Persistence/CandidateManagement/DynamicsCrmCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using GetIntoTeaching.Core.Infrastructure.RelationshipManagement;
+using Microsoft.PowerPlatform.Dataverse.Client;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Client;
+
+namespace GetIntoTeaching.Infrastructure.Persistence.CandidateManagement
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed class DynamicsCrmCommandHandler : CrmCommandHandler<object, object>
+    {
+        private readonly ServiceClient _crmServiceClient;
+
+        public DynamicsCrmCommandHandler(IOrganizationService crmServiceClient)
+        {
+            _crmServiceClient = (ServiceClient)crmServiceClient;
+        }
+
+        public override object ExecuteCommand(object query)
+        {
+            using (var crmServiceContext = new OrganizationServiceContext(_crmServiceClient))
+            {
+                crmServiceContext.SaveChanges(); // This performs the actual call against the CRM 
+            }
+        }
+    }
+}
