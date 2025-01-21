20abe28287abb8af2c46c241173504f53460b972
Spencer O'HEGARTY <Spencer.OHEGARTY@EDUCATION.GOV.UK>
2025-01-21 10:19:32 +0000

    Initial config setup

--- GetIntoTeachingApi/Program.cs
+++ GetIntoTeachingApi/Program.cs
@@ -1,10 +1,12 @@
 using System;
+using System.Configuration;
 using System.Threading.Tasks;
 using GetIntoTeachingApi.AppStart;
 using GetIntoTeachingApi.Database;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Serilog;
+using Serilog.Events;
 
 namespace GetIntoTeachingApi
 {
@@ -17,6 +19,11 @@ namespace GetIntoTeachingApi
 
             GetIntoTeachingDbContext.ConfigureNpgsql();
 
+
+            Log.Logger = new LoggerConfiguration()
+              .WriteTo.Sentry(o => o.Dsn = "https://77e5a366d39a433cbea90a992edab82c@o225781.ingest.us.sentry.io/5276954")
+              .CreateLogger();
+
             var webHost = CreateHostBuilder(args).Build();
 
             await webHost.RunAsync();
@@ -27,10 +34,32 @@ namespace GetIntoTeachingApi
                 .UseDefaultServiceProvider((_, options) => options.ValidateScopes = false)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseSentry();
-                    webBuilder.UseKestrel(opts => opts.AddServerHeader = false);
+                    //webBuilder.UseSentry();
+                    //webBuilder.UseKestrel(opts => opts.AddServerHeader = false);
                     webBuilder.UseStartup<Startup>();
                 })
-            .UseSerilog((ctx, config) => config.ReadFrom.Configuration(ctx.Configuration));
+            .UseSerilog((ctx, config) => config.ReadFrom.Configuration(ctx.Configuration).WriteTo.Sentry(s =>
+                {
+                    s.Dsn = "https://77e5a366d39a433cbea90a992edab82c@o225781.ingest.us.sentry.io/5276954";  // new Dsn(ConfigurationManager.AppSettings["SentryDsn"]);
+                    s.MinimumBreadcrumbLevel = LogEventLevel.Debug;
+                    s.MinimumEventLevel = LogEventLevel.Error;
+                }));
+
+
+        //.UseSerilog((_, c) =>
+        //    c.Enrich.FromLogContext()
+        //        .MinimumLevel.Debug()
+        //        .WriteTo.Console()
+        //        // Add Sentry integration with Serilog
+        //        // Two levels are used to configure it.
+        //        // One sets which log level is minimally required to keep a log message as breadcrumbs
+        //        // The other sets the minimum level for messages to be sent out as events to Sentry
+        //        .WriteTo.Sentry(s =>
+        //        {
+        //            s.MinimumBreadcrumbLevel = LogEventLevel.Debug;
+        //            s.MinimumEventLevel = LogEventLevel.Error;
+        //        }));
+
+
     }
 }
