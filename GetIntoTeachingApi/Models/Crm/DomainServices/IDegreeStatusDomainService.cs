6e2911bc9f9a853f185657c394228ee1d7e765a2
Spencer O'HEGARTY <Spencer.OHEGARTY@EDUCATION.GOV.UK>
2025-01-29 10:33:58 +0000

    Initial commit

---
+++ GetIntoTeachingApi/Models/Crm/DomainServices/IDegreeStatusDomainService.cs
@@ -0,0 +1,15 @@
+﻿namespace GetIntoTeachingApi.Models.Crm.DomainServices
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IDegreeStatusDomainService
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="graduationYear"></param>
+        /// <returns></returns>
+        int? GetInferredDegreeStatusFromGraduationYear(GraduationYear graduationYear);
+    }
+}
