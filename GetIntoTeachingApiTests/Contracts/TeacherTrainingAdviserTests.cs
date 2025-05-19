80f19935c7ba3309bdf1b7999ba78d43c23fd5f9
Martyn Whitwell <martyn.whitwell@gmail.com>
2025-05-19 16:41:31 +0100

    Revert "So/7770 pass qts value to crm for return to teaching advisor signup (#1542)"
    This reverts commit 60713fbaca254785beddcba0c76eb8a1a54ba55d.

--- GetIntoTeachingApiTests/Contracts/TeacherTrainingAdviserTests.cs
+++ GetIntoTeachingApiTests/Contracts/TeacherTrainingAdviserTests.cs
@@ -1,9 +1,10 @@
-﻿using FluentAssertions;
-using FluentAssertions.Json;
-using GetIntoTeachingApiTests.Helpers;
+﻿using System;
 using System.Net;
 using System.Net.Http;
 using System.Text;
+using FluentAssertions;
+using FluentAssertions.Json;
+using GetIntoTeachingApiTests.Helpers;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -13,20 +14,27 @@ namespace GetIntoTeachingApiTests.Contracts
     public class TeacherTrainingAdviserTests : BaseTests
     {
         public TeacherTrainingAdviserTests(DatabaseFixture databaseFixture)
-            : base(databaseFixture){
+            : base(databaseFixture)
+        {
         }
 
         [Theory]
         [ContractTestInputs("./Contracts/Input/TeacherTrainingAdviser")]
         public async Task Contract(string scenario)
         {
-            const string RequestUri = "/api/teacher_training_adviser/candidates";
-            const string RequestMediaType = "application/json";
-            StringContent content = new(ReadInput(scenario), Encoding.UTF8, RequestMediaType);
             await Setup();
-            HttpResponseMessage response = await HttpClient.PostAsync(RequestUri, content);
-            await AssertRequestMatchesSnapshot(scenario);
+
+            var response = await HttpClient.PostAsync(
+                "/api/teacher_training_adviser/candidates", ConstructBody(scenario));
+
             response.StatusCode.Should().Be(HttpStatusCode.NoContent);
+
+            await AssertRequestMatchesSnapshot(scenario);
+        }
+
+        private StringContent ConstructBody(string scenario)
+        {
+            return new StringContent(ReadInput(scenario), Encoding.UTF8, "application/json");
         }
     }
 }
