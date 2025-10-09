f6646765e75c209466fb36298ef6f7083a271475
paul senior <paul.senior@education.gov.uk>
2025-10-09 16:15:04 +0100

    feat(2128): testing a sample project specifying postgis version

--- terraform/aks/outputs.tf
+++ terraform/aks/outputs.tf
@@ -2,18 +2,22 @@ output "app_url" {
   value = module.api_application.url
 }
 
-output "postgres_db_url" {
-  value = module.postgres.url
-}
-
-output "postgres_server_docker_image" {
+output "server_docker_image" {
   value = module.postgres.server_docker_image
 }
 
-output "postgres_server_database_type" {
+output "server_database_type" {
   value = module.postgres.server_database_type
 }
 
 output "azure_extensions" {
   value = module.postgres.azure_extensions
+}
+
+output "server_postgis_version" {
+  value = module.postgres.server_postgis_version
+}
+
+output "azure_extensions_final" {
+  value = module.postgres.azure_extensions_final
 }
